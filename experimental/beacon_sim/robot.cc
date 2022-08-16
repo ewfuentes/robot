0a0b54ea8b3a4d8169d877789b6dafec5f74918b
Erick Fuentes <erick@csail.mit.edu>
2022-08-16 07:07:30 -0400

    Add Eigen as a dependency

--- experimental/beacon_sim/robot.cc
+++ experimental/beacon_sim/robot.cc
@@ -5,8 +5,7 @@
 
 namespace experimental::beacon_sim {
 void RobotState::move(const double distance_m) {
-    pos_x_m_ = pos_x_m_ + distance_m * std::cos(heading_rad_);
-    pos_y_m_ = pos_y_m_ + distance_m * std::sin(heading_rad_);
+    pos_m_ += distance_m * Eigen::Vector2d{std::cos(heading_rad_), std::sin(heading_rad_)};
 }
 
 void RobotState::turn(const double angle_rad) { heading_rad_ += angle_rad; }
--- experimental/beacon_sim/robot.hh
+++ experimental/beacon_sim/robot.hh
@@ -1,21 +1,22 @@
 
 #pragma once
 
+#include "Eigen/Core"
+
 namespace experimental::beacon_sim {
 struct RobotState {
     RobotState(const double pos_x_m, const double pos_y_m, const double heading_rad)
-        : pos_x_m_(pos_x_m), pos_y_m_(pos_y_m), heading_rad_(heading_rad) {}
+        : pos_m_{pos_x_m, pos_y_m}, heading_rad_(heading_rad) {}
 
     void move(const double distance_m);
     void turn(const double angle_rad);
 
-    const double &pos_x_m() const { return pos_x_m_; }
-    const double &pos_y_m() const { return pos_y_m_; }
+    const double &pos_x_m() const { return pos_m_(0); }
+    const double &pos_y_m() const { return pos_m_(1); }
     const double &heading_rad() const { return heading_rad_; }
 
    private:
-    double pos_x_m_;
-    double pos_y_m_;
+    Eigen::Vector2d pos_m_;
     double heading_rad_;
 };
 }  // namespace experimental::beacon_sim
