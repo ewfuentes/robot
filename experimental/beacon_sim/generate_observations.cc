27cab5a4fb9fb8b252f6fedf43f77ed841409d78
Erick Fuentes <erick@csail.mit.edu>
2022-08-17 10:16:10 -0400

    add noise to range observations

--- experimental/beacon_sim/generate_observations.cc
+++ experimental/beacon_sim/generate_observations.cc
@@ -4,23 +4,38 @@
 #include <cmath>
 
 namespace experimental::beacon_sim {
-std::optional<BeaconObservation> generate_observation(const Beacon &beacon, const RobotState &robot,
-                                                      const ObservationConfig &) {
-    const double range_m = (robot.local_from_robot().translation() - beacon.pos_in_local).norm();
+namespace {
+double compute_range_noise(const ObservationConfig &config, std::mt19937 &gen) {
+    if (!config.range_noise_std_m.has_value()) {
+        return 0.0;
+    }
 
-    const Eigen::Vector2d robot_from_beacon =
+    std::normal_distribution range_noise_m(0.0, config.range_noise_std_m.value());
+
+    return range_noise_m(gen);
+}
+}  // namespace
+std::optional<BeaconObservation> generate_observation(const Beacon &beacon, const RobotState &robot,
+                                                      std::mt19937 &gen,
+                                                      const ObservationConfig &config) {
+    const Eigen::Vector2d beacon_in_robot =
         robot.local_from_robot().inverse() * beacon.pos_in_local;
-    const double bearing_rad = std::atan2(robot_from_beacon.y(), robot_from_beacon.x());
-    return BeaconObservation{
-        .maybe_id = beacon.id, .maybe_range_m = range_m, .maybe_bearing_rad = bearing_rad};
+    const double range_m = beacon_in_robot.norm();
+
+    const double range_noise_m = compute_range_noise(config, gen);
+    const double bearing_rad = std::atan2(beacon_in_robot.y(), beacon_in_robot.x());
+    return BeaconObservation{.maybe_id = beacon.id,
+                             .maybe_range_m = range_m + range_noise_m,
+                             .maybe_bearing_rad = bearing_rad};
 }
 
 std::vector<BeaconObservation> generate_observations(const WorldMap &map, const RobotState &robot,
+                                                     std::mt19937 &gen,
                                                      const ObservationConfig &config) {
     std::vector<BeaconObservation> out;
     out.reserve(map.beacons().size());
     for (const auto &beacon : map.beacons()) {
-        const auto maybe_observation = generate_observation(beacon, robot, config);
+        const auto maybe_observation = generate_observation(beacon, robot, gen, config);
         if (maybe_observation.has_value()) {
             out.push_back(maybe_observation.value());
         }
