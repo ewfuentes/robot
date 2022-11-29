352c417c0a4fecf0744ff67db842e9d5ca3bdf0d
Erick Fuentes <fuentes.erick@gmail.com>
2022-11-28 19:38:02 -0500

    add missing dependency

--- setup.sh
+++ setup.sh
@@ -4,4 +4,5 @@ set -x # enable echo
 
 # Install clang14 and other required system packages
 sudo apt install clang-14 libxcursor-dev libxrandr-dev \
-     libxinerama-dev libxi-dev freeglut3-dev
+     libxinerama-dev libxi-dev freeglut3-dev libstdc++-12-dev \
+     gcc-11
