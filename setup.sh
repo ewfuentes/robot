855470a7f7362ed6dfb253e189fbd3e90637fa9d
Erick Fuentes <erick@csail.mit.edu>
2023-01-11 10:55:31 -0500

    back out changes

--- setup.sh
+++ setup.sh
@@ -3,10 +3,6 @@
 set -x # enable echo
 
 # Install clang14 and other required system packages
-# sudo apt install clang-14 libxcursor-dev libxrandr-dev \
-#      libxinerama-dev libxi-dev freeglut3-dev libstdc++-12-dev \
-#      gcc-11
-
-sudo apt install libxcursor-dev libxrandr-dev \
-          libxinerama-dev libxi-dev freeglut3-dev \
-          gcc-11
+sudo apt install clang-14 libxcursor-dev libxrandr-dev \
+     libxinerama-dev libxi-dev freeglut3-dev libstdc++-12-dev \
+     gcc-11
