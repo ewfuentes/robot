fdf07479b54fb51eed194d4a0632b330cf10d669
Erick Fuentes <fuentes.erick@gmail.com>
2023-01-24 17:21:58 -0500

    bind hand_distribution_code

--- experimental/pokerbots/evaluate_mass_hands.hh
+++ experimental/pokerbots/evaluate_mass_hands.hh
@@ -4,10 +4,14 @@
 #include <vector>
 
 #include "experimental/pokerbots/hand_evaluator.hh"
-namespace robot::experimental::pokerbots {
 
+namespace robot::experimental::pokerbots {
 std::vector<StrengthPotentialResult> evaluate_mass_hands(const std::vector<std::string> &hands,
                                                          const int max_additional_cards,
                                                          const int hands_per_eval);
 
-}
+std::vector<HandDistributionResult> mass_estimate_hand_distribution(const std::vector<std::string> &hands,
+                                                      const int max_additional_cards,
+                                                      const int num_board_rollouts,
+                                                      const int num_bins);
+}  // namespace robot::experimental::pokerbots
