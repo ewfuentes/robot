09fb340959ce4f6cd3e27e688b7b7d7811e3ba52
Erick Fuentes <erick@csail.mit.edu>
2023-03-09 07:10:53 -0800

    Add beacon correlations (#132)

---
+++ experimental/beacon_sim/correlated_beacons.py
@@ -0,0 +1,117 @@
+from __future__ import annotations
+
+import numpy as np
+from typing import NamedTuple
+from scipy import optimize, special
+import math
+
+
+class BeaconClique(NamedTuple):
+    """
+    This represents a set of beacons that are correlated in their presence/absence
+    """
+    # The marginal probability of a single beacon within the clique
+    p_beacon: float
+    # The probability that all beacons within the clique are absent
+    p_no_beacons: float
+
+    members: list[int]
+
+
+class BeaconPotential(NamedTuple):
+    members: list[int]
+    covariance: np.ndarray
+    bias: float
+
+    def log_prob(self, assignments: dict[int, bool]) -> float:
+        """Compute the log probability of the given configuration
+
+        Note that this function will assert if one of the members isn't given an assignment
+        """
+        input_list = []
+        for member in self.members:
+            assert member in assignments
+            input_list.append(int(assignments[member]))
+        input_array = np.array(input_list, ndmin=2)
+        return (input_array @ self.covariance @ input_array.T + self.bias)[0, 0]
+
+    def __mul__(self, other: BeaconPotential) -> BeaconPotential:
+        """Combine two independent beacon potentials"""
+        assert set(self.members).isdisjoint(
+            set(other.members)
+        ), rf"members should be disjoint but self /\ other = {set(self.members) & set(other.members)}"
+        n = len(self.members)
+        m = len(other.members)
+
+        new_covar = np.zeros((n + m, n + m))
+        new_covar[:n, :n] = self.covariance
+        new_covar[-m:, -m:] = other.covariance
+        new_bias = self.bias + other.bias
+        return BeaconPotential(
+            members=self.members + other.members, covariance=new_covar, bias=new_bias
+        )
+
+
+def create_correlated_beacons(clique: BeaconClique) -> BeaconPotential:
+    assert clique.p_beacon >= 0 and clique.p_beacon <= 1.0
+    assert clique.p_no_beacons >= 0 and clique.p_no_beacons <= 1.0
+
+    n = len(clique.members)
+
+    # We use an ising model to model the interactions between beacons. However, it can be hard to
+    # make global statements from a model that works locally. To work around this, we setup an
+    # optimization problem where the parameters are tweaked until the global conditions are
+    # satisfied.
+
+    # In an ising model, the probability of a configuration can be expressed as:
+    # P(x) = exp[x.T @ C + x + log(p_no_beacons)], where C is a symmetric matrix and x is a boolean
+    # vector where the i'th entry expresses if the i'th beacon is present or not.
+
+    # This compute the marginal probability of some arbitrary beacon being present.
+    def log_marginal_prob(phi: float, psi: float):
+        def kth_term(n: int, k: int, phi: float, psi: float) -> float:
+            # compute log(nCr(n, k) * exp[x.T @ C @ x + np.log(p_no_beacons)])
+            # n is the number of other beacons that exist and k is the number of other beacons that
+            # present. We then have k+1 diagonal terms and (k+1)*k/2 edges where both nodes are
+            # present, including the one that we are computing the probability for.
+            return np.log(math.comb(n, k)) + (k+1) * phi + (k+1) * k / 2 * psi + np.log(clique.p_no_beacons)
+        # Since one beacon is already present, we go through [0, n-1] other beacons being present
+        return special.logsumexp([kth_term(n-1, k, phi, psi) for k in range(n)])
+
+    # This computes the sum of the probability of all configurations
+    def log_total_prob(phi: float, psi: float):
+        def kth_term(n: int, k: int, phi: float, psi: float) -> float:
+            return np.log(math.comb(n, k)) + k * phi + (k-1) * k / 2 * psi + np.log(clique.p_no_beacons)
+        return special.logsumexp([kth_term(n, k, phi, psi) for k in range(n+1)])
+
+    def loss(x):
+        # The first term ensures that the marginal probability "constraint" is satisfied
+        # The second term ensures that the total probability is log(1) = 0.
+        return ((np.log(clique.p_beacon) - log_marginal_prob(x[0], x[1])) ** 2
+                + log_total_prob(x[0], x[1]) ** 2)
+
+    # I worked with the log probabilities because I thought that it would be easier to optimize,
+    # but BFGS tended to fail for reasons unknown. I found that nelder-mead, a derivative free
+    # worked well enough. These methods tend to fail when the number of decision variables gets to
+    # be too high, but we've only got two, so no trouble here.
+    result = optimize.minimize(loss, (1.0, 1.0), tol=1e-12, method='nelder-mead')
+    assert result.success, f'Failed to find distribution that matches desiderata: {result}'
+    # Our costs (beacon marginal and total probability = 1) are actually constraints. Only accept
+    # solutions if the costs are near zero.
+    assert result.fun < 1e-6, f'Can\'t satisfy total probability constraint or desired marginal probability: {result}'
+
+    phi, psi = result.x
+    off_diag = psi
+    diag = phi
+    off_diag_entries = np.ones((n, n)) - np.eye(n)
+    covar = np.eye(n) * diag + off_diag_entries * off_diag / 2
+
+    # the log probability of no beacons being present
+    bias = np.log(clique.p_no_beacons)
+
+    # Note that this distribution is normalized by construction.
+    return BeaconPotential(
+        members=clique.members,
+        covariance=covar,
+        bias=bias,
+    )
---
+++ experimental/beacon_sim/correlated_beacons_test.py
@@ -0,0 +1,151 @@
+import unittest
+import numpy as np
+
+from experimental.beacon_sim import correlated_beacons as cb
+
+
+class CorrelatedBeaconsTest(unittest.TestCase):
+    def test_independent_beacons(self):
+        # Setup
+        p_beacon = 0.75
+        p_no_beacons = (1 - p_beacon) ** 2
+        bc = cb.BeaconClique(
+            p_beacon=p_beacon,
+            p_no_beacons=p_no_beacons,
+            members=[1, 2],
+        )
+        # Action
+        beacon_pot = cb.create_correlated_beacons(bc)
+
+        p_00 = np.exp(beacon_pot.log_prob({1: False, 2: False}))
+        p_01 = np.exp(beacon_pot.log_prob({1: True, 2: False}))
+        p_10 = np.exp(beacon_pot.log_prob({1: False, 2: True}))
+        p_11 = np.exp(beacon_pot.log_prob({1: True, 2: True}))
+
+        # Verification
+        self.assertAlmostEqual(p_00, p_no_beacons, places=6)
+        self.assertAlmostEqual(p_01 + p_11, 0.75, places=6)
+        self.assertAlmostEqual(p_10 + p_11, 0.75, places=6)
+        self.assertAlmostEqual(p_01, p_10, places=6)
+        self.assertAlmostEqual(p_00 + p_01 + p_10 + p_11, 1.0, places=6)
+
+    def test_three_independent_beacons(self):
+        # Setup
+        p_beacon = 0.75
+        p_no_beacons = (1 - p_beacon) ** 3
+        bc = cb.BeaconClique(
+            p_beacon=p_beacon,
+            p_no_beacons=p_no_beacons,
+            members=[1, 2, 3],
+        )
+        # Action
+        beacon_pot = cb.create_correlated_beacons(bc)
+
+        p_000 = np.exp(beacon_pot.log_prob({1: False, 2: False, 3: False}))
+        p_010 = np.exp(beacon_pot.log_prob({1: True, 2: False, 3: False}))
+        p_100 = np.exp(beacon_pot.log_prob({1: False, 2: True, 3: False}))
+        p_110 = np.exp(beacon_pot.log_prob({1: True, 2: True, 3: False}))
+        p_001 = np.exp(beacon_pot.log_prob({1: False, 2: False, 3: True}))
+        p_011 = np.exp(beacon_pot.log_prob({1: True, 2: False, 3: True}))
+        p_101 = np.exp(beacon_pot.log_prob({1: False, 2: True, 3: True}))
+        p_111 = np.exp(beacon_pot.log_prob({1: True, 2: True, 3: True}))
+
+        # Verification
+        self.assertAlmostEqual(
+            p_000 + p_001 + p_010 + p_011 + p_100 + p_101 + p_110 + p_111, 1.0, places=6
+        )
+        self.assertAlmostEqual(p_000, p_no_beacons, places=6)
+        self.assertAlmostEqual(p_001 + p_011 + p_101 + p_111, 0.75, places=6)
+        self.assertAlmostEqual(p_010 + p_011 + p_110 + p_111, 0.75, places=6)
+        self.assertAlmostEqual(p_100 + p_101 + p_110 + p_111, 0.75, places=6)
+        self.assertAlmostEqual(p_010, p_100, places=6)
+        self.assertAlmostEqual(p_010, p_001, places=6)
+        self.assertAlmostEqual(p_111, p_beacon ** 3, places=6)
+        self.assertAlmostEqual(p_001, p_beacon * (1-p_beacon)**2, places=6)
+
+    def test_correlated_beacons(self):
+        # Setup
+        p_beacon = 0.75
+        p_no_beacons = 0.1
+        bc = cb.BeaconClique(
+            p_beacon=p_beacon,
+            p_no_beacons=p_no_beacons,
+            members=[1, 2],
+        )
+        # Action
+        beacon_pot = cb.create_correlated_beacons(bc)
+
+        p_00 = np.exp(beacon_pot.log_prob({1: False, 2: False}))
+        p_01 = np.exp(beacon_pot.log_prob({1: True, 2: False}))
+        p_10 = np.exp(beacon_pot.log_prob({1: False, 2: True}))
+        p_11 = np.exp(beacon_pot.log_prob({1: True, 2: True}))
+
+        # Verification
+        self.assertAlmostEqual(p_00, p_no_beacons, places=6)
+        self.assertAlmostEqual(p_00 + p_01 + p_10 + p_11, 1.0, places=6)
+        self.assertAlmostEqual(p_01 + p_11, p_beacon, places=6)
+        self.assertAlmostEqual(p_10 + p_11, p_beacon, places=6)
+        self.assertAlmostEqual(p_01, p_10, places=6)
+
+    def test_cannot_join_distributions_with_common_members(self):
+        # Setup
+        p_beacon = 0.75
+        p_no_beacons = (1 - p_beacon) ** 2
+        clique_1 = cb.BeaconClique(
+            p_beacon=p_beacon,
+            p_no_beacons=p_no_beacons,
+            members=[1, 2],
+        )
+        clique_2 = cb.BeaconClique(
+            p_beacon=p_beacon,
+            p_no_beacons=p_no_beacons,
+            members=[2, 3],
+        )
+
+        # Action + Verification
+        pot_1 = cb.create_correlated_beacons(clique_1)
+        pot_2 = cb.create_correlated_beacons(clique_2)
+        with self.assertRaises(AssertionError):
+            combined_pot = pot_1 * pot_2
+
+    def test_combined_distributions(self):
+        # Setup
+        # Create a clique that are positively correlated
+        p_beacon = 0.75
+        p_no_beacons = 0.1
+        clique_1 = cb.BeaconClique(
+            p_beacon=p_beacon,
+            p_no_beacons=p_no_beacons,
+            members=[1, 2],
+        )
+        # Create a clique that is negatively correlated
+        p_beacon = 0.5
+        p_no_beacons = 0.001
+        clique_2 = cb.BeaconClique(
+            p_beacon=p_beacon,
+            p_no_beacons=p_no_beacons,
+            members=[10, 11],
+        )
+
+        # Action
+        pot_1 = cb.create_correlated_beacons(clique_1)
+        pot_2 = cb.create_correlated_beacons(clique_2)
+        combined_pot = pot_1 * pot_2
+        # Verification
+        # Test an unlikely scenario, the first clique being different the second being the same
+        self.assertAlmostEqual(
+            np.exp(combined_pot.log_prob({1: True, 2: False, 10: False, 11: False})),
+            0.0,
+            places=3,
+        )
+
+        # Expect symmetry in the second clique
+        self.assertAlmostEqual(
+            np.exp(combined_pot.log_prob({1: True, 2: True, 10: True, 11: False})),
+            np.exp(combined_pot.log_prob({1: True, 2: True, 10: False, 11: True})),
+            places=6,
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
