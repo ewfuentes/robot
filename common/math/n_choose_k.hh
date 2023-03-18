a581675bc7547b2d50f059b02eff185fb6bc54c5
Erick Fuentes <erick@csail.mit.edu>
2023-03-18 11:29:32 -0400

    add n_choose_k function

---
+++ common/math/n_choose_k.hh
@@ -0,0 +1,12 @@
+
+namespace robot::math {
+constexpr int n_choose_k(const int n, const int k) {
+    int num = 1;
+    int den = 1;
+    for (int i = 1; i <= k; i++) {
+      num *= n+1 - i;
+      den *= i;
+    }
+    return num/den;
+}
+}
