4339e2af944ab4c9a2ddf06edb9917c718396f1a
Erick Fuentes <erick@csail.mit.edu>
2023-04-18 14:21:54 -0400

    wip

--- experimental/beacon_dist/model.py
+++ experimental/beacon_dist/model.py
@@ -1,12 +1,77 @@
-
 import torch
+import numpy as np
+
+from typing import NamedTuple
+
+from experimental.beacon_dist import utils
+
+
+class ReconstructorParams(NamedTuple):
+    descriptor_size: int
+    descriptor_embedding_size: int
+    position_encoding_factor: float
+
+
+def expand_descriptor(descriptor_tensor: torch.Tensor):
+    BITS_PER_BYTE = 8
+    shape = descriptor_tensor.shape
+    new_shape = *shape[:-1], shape[-1] * BITS_PER_BYTE
+    out = torch.zeros(new_shape)
+    for i in range(BITS_PER_BYTE):
+        out[:, :, i::BITS_PER_BYTE] = torch.bitwise_and(descriptor_tensor, 1 << i) > 0
+    return out
+
+
+def encode_position(x: torch.Tensor, y: torch.Tensor, output_size: int, factor: float):
+    assert output_size % 4 == 0, "Output size must be divisible by 4"
+    assert len(x.shape) == 3
+    assert len(y.shape) == 3
+    out = torch.zeros((*x.shape, output_size), dtype=torch.float32)
+    for i in range(0, output_size, 4):
+        out[:,:,:, 4 * i + 0] = torch.sin(x / factor ** (4 * i / output_size))
+        out[:,:,:, 4 * i + 1] = torch.cos(x / factor ** (4 * i / output_size))
+        out[:,:,:, 4 * i + 2] = torch.sin(y / factor ** (4 * i / output_size))
+        out[:,:,:, 4 * i + 3] = torch.cos(y / factor ** (4 * i / output_size))
+    return out
+
 
 class Reconstructor(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, params: ReconstructorParams):
         super().__init__()
 
-        self._bias = torch.nn.parameter.Parameter(torch.Tensor([1.0]))
+        # Ignore token
 
+        # descriptor encoder
+        self._descriptor_embedding = torch.nn.Linear(
+            params.descriptor_size,
+            params.descriptor_embedding_size,
+            bias=False,
+            dtype=torch.float32,
+        )
 
-    def forward(self, x):
-        return x._replace(x=x.x + self._bias)
+        # Position encoder
+
+        self._params = params
+
+    def encode_descriptors(self, descriptors: torch.Tensor):
+        expanded_descriptors = expand_descriptor(descriptors)
+        return self._descriptor_embedding(expanded_descriptors)
+
+    def position_encoding(self, x: torch.Tensor, y: torch.Tensor):
+        return encode_position(
+            x,
+            y,
+            self._params.descriptor_embedding_size,
+            self._params.position_encoding_factor,
+        )
+
+    def forward(self, x: utils.ReconstructorBatch):
+
+        # Encode the descriptors
+        embedded_descriptors = self.encode_descriptors(
+            x.descriptor
+        ) + self.position_encoding(x.x, x.y)
+
+        # Pass through transformer encoder layers
+
+        return x._replace(x=x.x)
--- experimental/beacon_dist/model_test.py
+++ experimental/beacon_dist/model_test.py
@@ -1,10 +1,65 @@
-
+import torch
 import unittest
 
+from experimental.beacon_dist import model
+from experimental.beacon_dist.test_helpers import get_test_data
+from experimental.beacon_dist.utils import Dataset, batchify
+
+
 class ModelTest(unittest.TestCase):
-    def test_construct_model(self):
-        self.assertFalse(False)
+    def test_expand_descriptor(self):
+        # Setup
+        tensor_in = torch.tensor([[[1, 2, 3, 4], [5, 6, 7, 8]]], dtype=torch.int16)
+        # fmt: off
+        expected_result = torch.tensor([[
+            [1, 0, 0, 0, 0, 0, 0, 0,  # 1
+                0, 1, 0, 0, 0, 0, 0, 0,  # 2
+                1, 1, 0, 0, 0, 0, 0, 0,  # 3
+                0, 0, 1, 0, 0, 0, 0, 0,  # 4
+                ],
+            [1, 0, 1, 0, 0, 0, 0, 0,  # 5
+                0, 1, 1, 0, 0, 0, 0, 0,  # 6
+                1, 1, 1, 0, 0, 0, 0, 0,  # 7
+                0, 0, 0, 1, 0, 0, 0, 0,  # 8
+            ]]])
+        # fmt: on
+
+        # Action
+        result = model.expand_descriptor(tensor_in)
+
+        # Verification
+        # We use equality here since we're working with an integer tensor
+        self.assertEqual(torch.linalg.matrix_norm(result - expected_result), 0)
+
+    def test_position_encoding(self):
+        # Setup
+        x = torch.tensor([[[1, 2, 3, 4]]])
+        y = torch.tensor([[[10, 9, 8, 4]]])
+        OUTPUT_SIZE = 16
+        FACTOR = 10000
+
+        # Action
+        position_embedding = model.encode_position(x, y, OUTPUT_SIZE, FACTOR)
+
+        # Verification
+        print(position_embedding.shape)
+        print(position_embedding)
+
+    def test_run_model(self):
+        # Setup
+        batch = batchify(Dataset(data=get_test_data())._data)
+        m = model.Reconstructor(model.ReconstructorParams(
+            descriptor_size=256,
+            descriptor_embedding_size=16,
+            position_encoding_factor=100000
+        ))
+
+        # Action
+        output = m(batch)
+
+        # Verification
+        self.assertIsNotNone(output)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
