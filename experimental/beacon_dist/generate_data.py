45a31213d144b026601de010b703098b5db7275c
Erick Fuentes <erick@csail.mit.edu>
2023-05-14 10:06:25 -0400

    Generate data using all capital letters (#160)

--- experimental/beacon_dist/generate_data.py
+++ experimental/beacon_dist/generate_data.py
@@ -1,16 +1,10 @@
 import argparse
-import cv2 as cv
-from wand.image import Image
-from wand.color import Color
-from wand.drawing import Drawing
 import numpy as np
 import time
-import tqdm.contrib.concurrent
+import multiprocessing
 from typing import NamedTuple
 
 from experimental.beacon_dist.utils import KeypointDescriptorDtype
-
-LETTERS = "ABC"
 
 
 class ImageSample(NamedTuple):
@@ -24,21 +18,32 @@ class LetterPosition(NamedTuple):
     angle: float
 
 
-def sample_letter_set(rng, height: int, width: int) -> dict[str, LetterPosition]:
+def sample_letter_set(
+    rng: np.random.Generator, letters: str, height: int, width: int
+) -> dict[str, LetterPosition]:
     out: dict[str, LetterPosition] = {}
-    for char in LETTERS:
-        if rng.random() < 0.9:
-            out[char] = LetterPosition(
-                x=rng.uniform(100, width - 100),
-                y=rng.uniform(100, height - 100),
-                angle=rng.uniform(0.0, 360.0),
-            )
+
+    num_letters = rng.integers(1, 10, endpoint=True)
+    # Note that the class label is currently 1 << (letters.find(letter)) so we can't
+    # yet handle multiple letters of the same kind. The fix is to export the sampled letters
+    # this will also allow us to regenerate the image in the future.
+    letter_idx = rng.choice(list(range(len(letters))), size=num_letters, replace=False)
+    for idx in letter_idx:
+        char = letters[idx]
+        out[char] = LetterPosition(
+            x=rng.uniform(100, width - 100),
+            y=rng.uniform(100, height - 100),
+            angle=rng.uniform(0.0, 360.0),
+        )
     return out
 
 
 def image_from_letter_set(
     letter_set: dict[str, LetterPosition], height, width
 ) -> np.ndarray:
+    from wand.image import Image
+    from wand.color import Color
+    from wand.drawing import Drawing
     with Image(width=width, height=height, background=Color("#fff")) as img:
         with Drawing() as ctx:
             ctx.font_size = 100
@@ -49,8 +54,10 @@ def image_from_letter_set(
         return np.array(img)
 
 
-def sample_image(rng, height: int = 720, width: int = 1280) -> ImageSample:
-    letter_set = sample_letter_set(rng, width, height)
+def sample_image(
+    rng, letters: str, height: int = 720, width: int = 1280
+) -> ImageSample:
+    letter_set = sample_letter_set(rng, letters, width, height)
     final_image = image_from_letter_set(letter_set, height, width)
     images: dict[str, Image] = {}
     for s, position in letter_set.items():
@@ -78,7 +85,8 @@ def serialize_keypoints_and_descriptors(image_id, keypoints, descriptors, class_
     return out
 
 
-def detect_orb_features(image_sample: ImageSample):
+def detect_orb_features(image_sample: ImageSample, letters: str):
+    import cv2 as cv
     orb = cv.ORB_create(nfeatures=200)
     # Detect keypoints in the final image
     keypoints = orb.detect(image_sample.final_image, None)
@@ -92,8 +100,8 @@ def detect_orb_features(image_sample: ImageSample):
         # if a keypoint doesn't overlap with the current letter, the descriptor will be all zeros
         # sum across all filters and mark the ones with non zero activation
         is_kp_sensitive = (np.sum(descriptors_sub, axis=1) > 0).astype(np.int64)
-        shift_amount = np.array(LETTERS.find(class_name))
-        assert shift_amount >= 0, f"Unable to find class {class_name} in {LETTERS}"
+        shift_amount = np.array(letters.find(class_name))
+        assert shift_amount >= 0, f"Unable to find class {class_name} in {letters}"
         label = np.left_shift(is_kp_sensitive, shift_amount)
         class_labels += label
 
@@ -101,25 +109,28 @@ def detect_orb_features(image_sample: ImageSample):
     return keypoints_all, descriptors_all, class_labels
 
 
-def generate_data(idx):
+def generate_data(idx: int, letters: str):
     rng = np.random.default_rng(idx)
-    sampled_images = sample_image(rng, height=1280, width=720)
-    keypoints, descriptors, class_labels = detect_orb_features(sampled_images)
+    sampled_images = sample_image(rng, letters, width=720, height=480)
+    keypoints, descriptors, class_labels = detect_orb_features(sampled_images, letters)
+
     return serialize_keypoints_and_descriptors(
         idx, keypoints, descriptors, class_labels
     )
 
 
-def main(num_images: int):
+def main(num_images: int, letters: str, output_path: str):
     start = time.time()
-    result = tqdm.contrib.concurrent.process_map(
-        generate_data, range(num_images), chunksize=30, max_workers=1
-    )
+    with multiprocessing.Pool(1) as p:
+        result = p.starmap(
+            generate_data,
+            zip(range(num_images), [letters] * num_images),
+        )
     end = time.time()
     print(
         f"{end-start} seconds to generate {num_images} images. {(end-start)/num_images} s/image"
     )
-    with open("/tmp/data.npy", "wb") as file_out:
+    with open(output_path, "wb") as file_out:
         np.save(file_out, np.concatenate(result))
 
 
@@ -131,5 +142,21 @@ if __name__ == "__main__":
         help="Number of images to generate",
         required=True,
     )
+    parser.add_argument(
+        "--letter_set",
+        choices=["abc", "alphabet"],
+        required=True,
+    )
+    parser.add_argument(
+        "--output",
+        required=True,
+    )
+
     args = parser.parse_args()
-    main(args.num_images)
+    import string
+
+    if args.letter_set == "abc":
+        letters = "ABC"
+    elif args.letter_set == "alphabet":
+        letters = string.ascii_uppercase
+    main(args.num_images, letters, args.output)
