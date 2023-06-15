ef1c6057241f143882043133f95d65a5ba9feca7
Erick Fuentes <erick@csail.mit.edu>
2023-06-15 15:52:37 -0400

    wip

---
+++ experimental/beacon_dist/multiview_dataset_test.py
@@ -0,0 +1,13 @@
+
+import unittest
+
+from experimental.beacon_dist.multiview_dataset import MultiviewDataset
+
+
+class MultiviewDatasetTest(unittest.TestCase):
+    def test_load(self):
+        self.assertTrue(False)
+
+
+if __name__ == "__main__":
+    unittest.main()
