98de9d30c3fdb452c47e6564f953439ac824845b
Jared Strader <jared.l.strader@gmail.com>
2023-08-23 10:58:47 -0400

    scripts for testing to/from scattering form (#203)
    Co-authored-by: Jared Strader <jstrader@mit.edu>
    Co-authored-by: Erick Fuentes <erick@csail.mit.edu>

---
+++ experimental/beacon_sim/analysis/analysis.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python3
+"""
+Tests for propagating the covariance matrix using the EKF with one-step updates
+"""
+
+import click
+import numpy as np
+import numpy.linalg as LA
+from collections import defaultdict
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+from experimental.beacon_sim.analysis.belief_propagation import BeliefPropagation
+from experimental.beacon_sim.analysis.models import SingleIntegrator, DoubleIntegrator
+from experimental.beacon_sim.analysis.star import scattering_from_transfer, transfer_from_scattering
+
+
+def add_xy_to_axes(x, y, ax, name='', xlabel='', ylabel='', use_log=False):
+    ax.semilogy(x, y, marker='o', label=name)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.grid(True)
+    ax.legend()
+
+
+@click.group()
+def main():
+    pass
+
+
+@main.command(name="cond", help='Plot condition number for one-step update forms')
+@click.argument("model", default="si")
+@click.option("--num-steps", type=int, default=100, help='number of time steps')
+@click.option("--dt", type=float, default=1, help='length of time step')
+@click.option("--incr", type=int, default=1, help='increments to display in figures')
+def run_cond(
+    model,
+    num_steps,
+    dt,
+    incr,
+):
+    #setup model
+    if model=='si':
+        P = np.identity(2)
+        robot = SingleIntegrator(dt=dt)
+    elif model=='di':
+        P = np.identity(4)
+        robot = DoubleIntegrator(dt=dt)
+    else:
+        print('error. invalid model')
+        raise
+
+    bp = BeliefPropagation(robot)   
+
+    data={}
+
+    #prop using gamma
+    data['tf']=defaultdict(list)
+    for i in range(num_steps):
+        k=i*incr
+        data['tf']['k'].append(k)
+
+        gamma = bp.compute_tf(k=k)
+        data['tf']['cond_gamma'].append(LA.cond(gamma))
+
+        s = scattering_from_transfer(gamma)
+        data['tf']['cond_s'].append(LA.cond(s))
+
+        gamma_back = transfer_from_scattering(s)
+        data['tf']['cond_gamma_back'].append(LA.cond(gamma_back))
+
+    #prop using scatter
+    data['star']=defaultdict(list)
+    for i in range(num_steps):
+        k=i*incr
+        data['star']['k'].append(k)
+
+        s = bp.compute_scatter(k=k)
+        data['star']['cond_s'].append(LA.cond(s))
+
+        gamma = transfer_from_scattering(s)
+        data['star']['cond_gamma'].append(LA.cond(gamma))
+
+        s_back = scattering_from_transfer(gamma)
+        data['star']['cond_s_back'].append(LA.cond(s_back))
+
+    #figures
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 5))
+
+    add_xy_to_axes(data['star']['k'], data['star']['cond_gamma'],axes[0], name='$\kappa(\gamma(S))$', xlabel='number of steps', ylabel='condition number')
+    add_xy_to_axes(data['star']['k'], data['star']['cond_s'],axes[0], name='$\kappa(S)$', xlabel='number of steps', ylabel='condition number')
+    add_xy_to_axes(data['star']['k'], data['star']['cond_s_back'],axes[0], name='$\kappa(S(\gamma(S)))$', xlabel='number of steps', ylabel='condition number')
+    axes[0].set_title('Condition Number $\kappa$ for Scattering Form',pad=20)
+
+    add_xy_to_axes(data['tf']['k'], data['tf']['cond_gamma'],axes[1], name='$\kappa(\gamma)$', xlabel='number of steps', ylabel='condition number')
+    add_xy_to_axes(data['tf']['k'], data['tf']['cond_s'],axes[1], name='$\kappa(S(\gamma))$', xlabel='number of steps', ylabel='condition number')
+    add_xy_to_axes(data['tf']['k'], data['tf']['cond_gamma_back'],axes[1], name='$\kappa(\gamma(S(\gamma)))$', xlabel='number of steps', ylabel='condition number')
+    axes[1].set_title('Condition Number $\kappa$ for Transfer Function Form',pad=20)
+
+    plt.show()
+
+
+@main.command(name="sens", help='Evaluate sensitivity to and from scatter form.')
+@click.argument("model", default="si")
+@click.option("--num-props", type=int, default=500)
+@click.option("--dt", type=float, default=0.1)
+def sens(
+    model,
+    num_props, #968, largest num props before fail for di
+    dt,
+):
+    #setup model
+    if model=='si':
+        P = np.identity(2)
+        robot = SingleIntegrator(dt=dt)
+    elif model=='di':
+        P = np.identity(4)
+        robot = DoubleIntegrator(dt=dt)
+    else:
+        print('error. invalid model')
+        raise
+
+    bp = BeliefPropagation(robot)
+
+    P_ekf = bp.prop(P, k=num_props)
+    P_scatter = bp.prop_scatter(P, k=num_props)
+    P_tf = bp.prop_tf(P, k=num_props)
+
+    gamma = bp.prop_tf(P, k=num_props, return_tf=True)
+    print('cond(gamma):',LA.cond(gamma)) 
+
+    rand_numbers = np.random.rand(1000)
+    rand_numbers = rand_numbers/np.sum(rand_numbers)
+    gamma_expected = np.zeros_like(gamma)
+    for r in rand_numbers:
+        gamma_expected += r*gamma
+    P_expected = bp.apply_tf_to_cov(P,gamma)
+
+    diff_ekf_scatter = LA.norm(P_ekf - P_scatter)
+    diff_ekf_tf = LA.norm(P_ekf - P_tf)
+    diff_expected = LA.norm(P_scatter - P_expected)
+    print('||P_ekf - P_scatter||      =', diff_ekf_scatter)
+    print('||P_ekf - P_tf||           =', diff_ekf_tf)
+    print('||P_scatter - P_expected|| =', diff_expected)
+
+
+if __name__ == "__main__":
+    mpl.use("GTK3Agg")
+    main()
---
+++ experimental/beacon_sim/analysis/belief_propagation.py
@@ -0,0 +1,129 @@
+"""
+Module modeling implementing belief progration for EKF with one-step transfor functions
+"""
+import numpy as np
+from numpy.linalg import inv, eig, cond, det, norm
+
+from experimental.beacon_sim.analysis.star import sblocks, sprod
+
+class BeliefPropagation:
+    """
+    Implements belief propagation using first-order approximation of belief dynamics
+    """
+    def __init__(self, model):
+        self.model=model
+
+    def remap_vars(self):
+        G = self.model.F()
+        R = self.model.L() @ self.model.Q @ self.model.L().transpose()
+        M = self.model.H().transpose() @ inv(self.model.R) @ self.model.H()
+        return G, R, M
+
+    def predict(self,P):
+        P = self.model.F() @ P @ self.model.F().transpose() + self.model.L() @ self.model.Q @ self.model.L().transpose()
+        return P
+
+    def update(self,P):
+        n = P.shape[0]
+        S = self.model.H() @ P @ self.model.H().transpose() + self.model.M() @ self.model.R @ self.model.M().transpose()
+        K= P @ self.model.H().transpose() @ inv(S)
+        return (np.identity(n) - K @ self.model.H()) @ P
+
+    #functions for propagating belief
+    def prop(self, P, k=1):
+        """
+        Propagates covariance matrix P through k steps using EKF equations
+        """
+        for i in range(k):
+            P = self.predict(P)
+            P = self.update(P)
+        return P
+
+    def prop_tf(self, P, k=1, only_predict=False, return_tf=False):
+        """
+        Propagates covariance matrix P through k steps using one-step transfer function
+        """
+        gamma = self.compute_tf(k=k, only_predict=only_predict)
+        n = self.model.n_x
+        I = np.identity(n)
+        if return_tf:
+            return gamma
+        else:
+            psi = np.block([[P],[I]])
+            gamma_p = gamma @ psi
+            B = gamma_p[0:n,0:n]
+            C = gamma_p[n:2*n,0:n]
+            return B @ inv(C)
+
+    def prop_scatter(self, P, k=1, only_predict=False, return_scatter=False):
+        """
+        Propagates covariance matrix P through k steps using scattering matrices
+        """
+        s = self.compose_scatter_from_cov(P)
+        if k>0:
+            s = sprod(s, self.compute_scatter(k=k, only_predict=only_predict))
+        n = s.shape[0]//2
+        if return_scatter:
+            return s
+        else:
+            return s[0:n,n:2*n]
+
+    #utilities for scattering form
+    def compose_scatter(self, only_predict=False):
+        n=self.model.n_x
+        I = np.identity(n)
+        Z = np.zeros((n,n))
+        G, R, M = self.remap_vars()
+
+        sc = np.block([[G, R],[Z,G.transpose()]]) #control step
+        sm = np.block([[I, Z],[-M,I]]) #update step
+
+        if only_predict:
+            return sc
+        else:
+            return sprod(sc,sm)
+
+    def compose_scatter_from_cov(self, P):
+        n = P.shape[0]
+        I = np.identity(n)
+        Z = np.zeros((n,n))
+        return np.block([[I, P],[Z,I]])
+
+    def compute_scatter(self, k=1, only_predict=False):
+        s = self.compose_scatter(only_predict=only_predict)
+        for i in range(k-1):
+            s = sprod(s, self.compose_scatter(only_predict=only_predict))
+        return s
+
+    def apply_scatter_to_cov(self, P, s):
+        s0= self.compose_scatter_from_cov(P)
+        s = sprod(s0, s)
+        n = s.shape[0]//2
+        return s[0:n,n:2*n]
+
+    #utilities for tf form
+    def compute_tf(self, k=1, only_predict=False):
+        n = self.model.n_x
+        I = np.identity(n)
+        Z = np.zeros((n,n))
+        G, R, M = self.remap_vars()
+
+        gamma = np.identity(2*n)
+        for i in range(k):
+            if only_predict:
+                X = np.block([[Z, I],[I,Z]])
+                Y = np.block([[Z, inv(G.transpose())],[G,R@inv(G.transpose())]])
+            else:
+                X = np.block([[Z, I],[I,M]])
+                Y = np.block([[Z, inv(G.transpose())],[G,R@inv(G.transpose())]])
+            gamma = X @ Y @ gamma
+        return gamma
+
+    def apply_tf_to_cov(self, P, gamma):
+        n = self.model.n_x
+        I = np.identity(n)
+        psi = np.block([[P],[I]])
+        gamma_p = gamma @ psi
+        B = gamma_p[0:n,0:n]
+        C = gamma_p[n:2*n,0:n]
+        return B @ inv(C)
