1a9b7fdb0004d2c77f285cd6c751502568963f34
Erick Fuentes <erick@csail.mit.edu>
2023-09-01 15:43:53 -0400

    lint

--- experimental/beacon_sim/correlated_beacons_python.cc
+++ experimental/beacon_sim/correlated_beacons_python.cc
@@ -3,8 +3,8 @@
 #include <sstream>
 
 #include "experimental/beacon_sim/correlated_beacons.hh"
-#include "pybind11/pybind11.h"
 #include "pybind11/eigen.h"
+#include "pybind11/pybind11.h"
 #include "pybind11/stl.h"
 
 namespace py = pybind11;
