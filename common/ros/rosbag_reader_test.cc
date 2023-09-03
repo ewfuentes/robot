9911ac6b8ddc3e5660c5b0a7f3b5784256f8ff24
Erick Fuentes <fuentes.erick@gmail.com>
2023-09-03 07:07:35 -0400

    include test bag

--- common/ros/rosbag_reader_test.cc
+++ common/ros/rosbag_reader_test.cc
@@ -4,10 +4,7 @@
 
 namespace robot::ros {
 GTEST_TEST(RosbagReaderTest, read_rosbag) {
-    std::vector<std::string> rosbag_paths = {
-        "/home/erick/scratch/symphony_lake_bags/20170307/2017-03-07-08-52-30_0.bag",
-        "/home/erick/scratch/symphony_lake_bags/20170307/2017-03-07-08-53-20_1.bag",
-    };
+    std::vector<std::string> rosbag_paths = {"common/ros/test.bag"};
     Embag::View view;
     for (const auto &path : rosbag_paths) {
         view.addBag(path);
