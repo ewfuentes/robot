c1b59f9eec951f6db47384634fa22dbc5429ecab
Erick Fuentes <erick@csail.mit.edu>
2023-09-27 14:22:55 -0400

    back out change

--- common/math/redheffer_star.cc
+++ common/math/redheffer_star.cc
@@ -34,23 +34,23 @@ Eigen::MatrixXd redheffer_star(const Eigen::MatrixXd &a, const Eigen::MatrixXd &
     // b = [[W X]
     //      [Y Z]]
     const int dim = a.rows() / 2;
-    const Eigen::MatrixXd A_11 = a.topLeftCorner(dim, dim);
-    const Eigen::MatrixXd A_12 = a.topRightCorner(dim, dim);
-    const Eigen::MatrixXd A_21 = a.bottomLeftCorner(dim, dim);
-    const Eigen::MatrixXd A_22 = a.bottomRightCorner(dim, dim);
+    const Eigen::MatrixXd A = a.topLeftCorner(dim, dim);
+    const Eigen::MatrixXd B = a.topRightCorner(dim, dim);
+    const Eigen::MatrixXd C = a.bottomLeftCorner(dim, dim);
+    const Eigen::MatrixXd D = a.bottomRightCorner(dim, dim);
 
-    const Eigen::MatrixXd B_11 = b.topLeftCorner(dim, dim);
-    const Eigen::MatrixXd B_12 = b.topRightCorner(dim, dim);
-    const Eigen::MatrixXd B_21 = b.bottomLeftCorner(dim, dim);
-    const Eigen::MatrixXd B_22 = b.bottomRightCorner(dim, dim);
+    const Eigen::MatrixXd W = b.topLeftCorner(dim, dim);
+    const Eigen::MatrixXd X = b.topRightCorner(dim, dim);
+    const Eigen::MatrixXd Y = b.bottomLeftCorner(dim, dim);
+    const Eigen::MatrixXd Z = b.bottomRightCorner(dim, dim);
 
     const Eigen::MatrixXd I = Eigen::MatrixXd::Identity(dim, dim);
-    const Eigen::MatrixXd I_min_A_12_B_21_inv = (I - A_12 * B_21).inverse();
-    const Eigen::MatrixXd I_min_B_21_A_12_inv = (I - B_21 * A_12).inverse();
+    const Eigen::MatrixXd I_min_BY_inv = (I - B * Y).inverse();
+    const Eigen::MatrixXd I_min_YB_inv = (I - Y * B).inverse();
 
-    return (Eigen::MatrixXd(2 * dim, 2 * dim) << B_11 * I_min_A_12_B_21_inv * A_11,
-            B_12 + B_11 * I_min_A_12_B_21_inv * A_12 * B_22,
-            A_21 + A_22 * I_min_B_21_A_12_inv * B_21 * A_11, A_22 * I_min_B_21_A_12_inv * B_22)
+    return (Eigen::MatrixXd(2 * dim, 2 * dim) << W * I_min_BY_inv * A,
+            X + W * I_min_BY_inv * B * Z,
+            C + D * I_min_YB_inv * Y * A, D * I_min_YB_inv * Z)
         .finished();
 }
 }  // namespace robot::math
