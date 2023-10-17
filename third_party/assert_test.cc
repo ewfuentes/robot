2a244c14663656949f78f133748babfbc8368aae
Erick Fuentes <erick@csail.mit.edu>
2023-10-16 23:31:33 -0400

    remove unnecessary pragmas

--- third_party/assert_test.cc
+++ third_party/assert_test.cc
@@ -4,8 +4,5 @@
 #include "gtest/gtest.h"
 
 TEST(AssertTest, assert_test) {
-    #pragma GCC diagnostic push
-    #pragma GCC diagnostic ignored "-Wunused-value"
     EXPECT_DEATH(DEBUG_ASSERT(false), "Debug Assertion failed at .*");
-    #pragma GCC diagnostic pop
 }
