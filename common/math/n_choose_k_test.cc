95aa1977f198b116764a6cdc5be832e8f52f2b87
Erick Fuentes <erick@csail.mit.edu>
2023-11-14 09:51:27 -0500

    lint

--- common/math/n_choose_k_test.cc
+++ common/math/n_choose_k_test.cc
@@ -4,12 +4,11 @@
 
 namespace robot::math {
 using Param = std::tuple<int, int, int>;
-class NChooseKTest: public testing::TestWithParam<Param> {
-};
+class NChooseKTest : public testing::TestWithParam<Param> {};
 TEST_P(NChooseKTest, n_choose_k_test) {
     // Setup
     const auto &[n, k, expected] = GetParam();
-    
+
     // Action
     const int n_choose_k_result = n_choose_k(n, k);
 
@@ -17,14 +16,9 @@ TEST_P(NChooseKTest, n_choose_k_test) {
     EXPECT_EQ(n_choose_k_result, expected);
 }
 
-INSTANTIATE_TEST_SUITE_P(
-    NChooseKTestSuite,
-    NChooseKTest, 
-    testing::Values(
-        std::make_tuple(10, 10, 1),
-        std::make_tuple(5, 3, 10),
-        std::make_tuple(33, 10, 92561040),
-        std::make_tuple(33, 16, 1166803110)
-   ));
+INSTANTIATE_TEST_SUITE_P(NChooseKTestSuite, NChooseKTest,
+                         testing::Values(std::make_tuple(10, 10, 1), std::make_tuple(5, 3, 10),
+                                         std::make_tuple(33, 10, 92561040),
+                                         std::make_tuple(33, 16, 1166803110)));
 
 }  // namespace robot::math
