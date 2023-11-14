030040438091738013c5e3e9c47588708086cb2e
Erick Fuentes <erick@csail.mit.edu>
2023-11-14 09:48:17 -0500

    Prevent overflow in N choose K

--- common/math/n_choose_k_test.cc
+++ common/math/n_choose_k_test.cc
@@ -3,26 +3,28 @@
 #include "gtest/gtest.h"
 
 namespace robot::math {
-TEST(NChooseKTest, n_choose_n_is_one) {
+using Param = std::tuple<int, int, int>;
+class NChooseKTest: public testing::TestWithParam<Param> {
+};
+TEST_P(NChooseKTest, n_choose_k_test) {
     // Setup
-    constexpr int N = 10;
-
+    const auto &[n, k, expected] = GetParam();
+    
     // Action
-    constexpr int n_choose_n = n_choose_k(N, N);
+    const int n_choose_k_result = n_choose_k(n, k);
 
     // Verification
-    EXPECT_EQ(n_choose_n, 1);
+    EXPECT_EQ(n_choose_k_result, expected);
 }
 
-TEST(NChooseKTest, five_choose_three_is_ten) {
-    // Setup
-    constexpr int N = 5;
-    constexpr int K = 3;
+INSTANTIATE_TEST_SUITE_P(
+    NChooseKTestSuite,
+    NChooseKTest, 
+    testing::Values(
+        std::make_tuple(10, 10, 1),
+        std::make_tuple(5, 3, 10),
+        std::make_tuple(33, 10, 92561040),
+        std::make_tuple(33, 16, 1166803110)
+   ));
 
-    // Action
-    constexpr int five_choose_three = n_choose_k(N, K);
-
-    // Verification
-    EXPECT_EQ(five_choose_three, 10);
-}
 }  // namespace robot::math
