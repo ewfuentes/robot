ad687e6cd371462029d60a8cfb55ec5e30b19368
Erick Fuentes <fuentes.erick@gmail.com>
2024-01-05 13:25:48 -0500

    remove template specification

--- experimental/beacon_sim/belief_road_map_planner.cc
+++ experimental/beacon_sim/belief_road_map_planner.cc
@@ -238,13 +238,14 @@ std::optional<planning::BRMPlan<RobotBelief>> compute_belief_road_map_plan(
         make_belief_updater(road_map, options.max_sensor_range_m, options.max_num_edge_transforms,
                             ekf, beacon_potential, TransformType::COVARIANCE);
     if (options.uncertainty_tolerance.has_value()) {
-        return planning::plan<RobotBelief>(
+        return planning::plan(
             road_map, initial_belief, belief_updater,
             [](const RobotBelief &b) { return uncertainty_size(b); },
             planning::MinUncertaintyToleranceOptions{options.uncertainty_tolerance.value()});
     } else {
-        return planning::plan<RobotBelief, double (*)(const RobotBelief &)>(
-            road_map, initial_belief, belief_updater, uncertainty_size,
+        return planning::plan(
+            road_map, initial_belief, belief_updater,
+            [](const RobotBelief &b) { return uncertainty_size(b); },
             planning::NoBacktrackingOptions{});
     }
 }
@@ -301,7 +302,7 @@ std::optional<planning::BRMPlan<LandmarkRobotBelief>> compute_landmark_belief_ro
     const auto belief_updater =
         make_landmark_belief_updater(road_map, options.max_sensor_range_m, sampled_belief_options,
                                      ekf, beacon_potential, TransformType::COVARIANCE);
-    return planning::plan<LandmarkRobotBelief>(
+    return planning::plan(
         road_map, initial_belief, belief_updater,
         [](const LandmarkRobotBelief &b) { return uncertainty_size(b); },
         planning::NoBacktrackingOptions{});
