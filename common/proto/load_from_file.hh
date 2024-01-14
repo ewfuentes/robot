4c19a6fe821dd08246aaca34f84a55ef2ab0651d
Erick Fuentes <fuentes.erick@gmail.com>
2024-01-14 12:25:52 -0500

    back out changes

--- common/proto/load_from_file.hh
+++ common/proto/load_from_file.hh
@@ -1,7 +1,6 @@
 
 #include <filesystem>
 #include <fstream>
-#include <iostream>
 #include <optional>
 
 #include "google/protobuf/text_format.h"
@@ -10,9 +9,7 @@ namespace robot::proto {
 
 template <typename MsgType>
 std::optional<MsgType> load_from_file(const std::filesystem::path &path) {
-    std::cout << "Trying to read from: " << path << std::endl;
     if (!std::filesystem::exists(path)) {
-        std::cout << "Does not exist!" << std::endl;
         return std::nullopt;
     }
 
@@ -23,16 +20,13 @@ std::optional<MsgType> load_from_file(const std::filesystem::path &path) {
     MsgType out;
 
     if (out.ParseFromString(sstream.str())) {
-        std::cout << "Found binary proto" << std::endl;
         return out;
     }
 
     if (google::protobuf::TextFormat::ParseFromString(sstream.str(), &out)) {
-        std::cout << "Found text proto" << std::endl;
         return out;
     }
 
-    std::cout << "couldn't parse as binary or text proto" << std::endl;
     return std::nullopt;
 }
 
