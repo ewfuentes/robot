f84b1709fc67aff8cf2366cbd6d6d243d6486332
Erick Fuentes <erick@csail.mit.edu>
2024-01-15 00:12:59 -0500

    Compute basic statistics (#252)

--- experimental/beacon_sim/run_experiment.cc
+++ experimental/beacon_sim/run_experiment.cc
@@ -3,6 +3,7 @@
 #include <execution>
 #include <filesystem>
 #include <limits>
+#include <optional>
 #include <random>
 
 #include "Eigen/Core"
@@ -15,7 +16,9 @@
 #include "experimental/beacon_sim/belief_road_map_planner.hh"
 #include "experimental/beacon_sim/ekf_slam.hh"
 #include "experimental/beacon_sim/experiment_config.pb.h"
+#include "experimental/beacon_sim/make_belief_updater.hh"
 #include "experimental/beacon_sim/mapped_landmarks_to_proto.hh"
+#include "experimental/beacon_sim/robot_belief.hh"
 #include "experimental/beacon_sim/world_map_config_to_proto.hh"
 #include "planning/road_map_to_proto.hh"
 
@@ -27,6 +30,15 @@ namespace {
 struct StartGoal {
     Eigen::Vector2d start;
     Eigen::Vector2d goal;
+};
+
+struct PlannerResult {
+    struct Plan {
+        std::vector<int> nodes;
+        double log_prob_mass_tracked;
+    };
+    time::RobotTimestamp::duration elapsed_time;
+    std::optional<Plan> plan;
 };
 
 std::vector<std::tuple<int, StartGoal>> sample_start_goal(const planning::RoadMap &map,
@@ -87,17 +99,11 @@ EkfSlam load_ekf_slam(const MappedLandmarks &mapped_landmarks) {
     return ekf;
 }
 
-std::vector<int> run_planner(const planning::RoadMap &road_map, const EkfSlam &ekf,
-                             const BeaconPotential &beacon_potential,
-                             const proto::LandmarkBRMPlanner &config,
-                             const double max_sensor_range_m,
-                             const std::optional<time::RobotTimestamp::duration> &timeout) {
-    // if (config.has_max_num_components()) {
-    //     std::cout << "landmark belief components max: " << config.max_num_components() <<
-    //     std::endl;
-    // } else {
-    //     std::cout << "landmark belief with unlimited num components" << std::endl;
-    // }
+PlannerResult run_planner(const planning::RoadMap &road_map, const EkfSlam &ekf,
+                          const BeaconPotential &beacon_potential,
+                          const proto::LandmarkBRMPlanner &config, const double max_sensor_range_m,
+                          const std::optional<time::RobotTimestamp::duration> &timeout) {
+    const time::RobotTimestamp start_time = time::current_robot_time();
     const auto plan = compute_landmark_belief_road_map_plan(
         road_map, ekf, beacon_potential,
         {.max_sensor_range_m = max_sensor_range_m,
@@ -107,14 +113,21 @@ std::vector<int> run_planner(const planning::RoadMap &road_map, const EkfSlam &e
                        .max_num_components = config.max_num_components(), .seed = 12345})
                  : std::nullopt,
          .timeout = timeout});
-    CHECK(plan.has_value());
+    const time::RobotTimestamp::duration elapsed_time = time::current_robot_time() - start_time;
 
     // std::cout << "log probability mass tracked: "
     //           << plan->beliefs.back().log_probability_mass_tracked << std::endl;
-    return plan->nodes;
+
+    return {.elapsed_time = elapsed_time,
+            .plan = plan.has_value() ? std::make_optional(PlannerResult::Plan{
+                                           .nodes = plan->nodes,
+                                           .log_prob_mass_tracked =
+                                               plan->beliefs.back().log_probability_mass_tracked,
+                                       })
+                                     : std::nullopt};
 }
 
-std::vector<int> run_planner(
+PlannerResult run_planner(
     [[maybe_unused]] const planning::RoadMap &road_map, [[maybe_unused]] const EkfSlam &ekf,
     [[maybe_unused]] const BeaconPotential &beacon_potential,
     [[maybe_unused]] const proto::ExpectedBRMPlanner &config,
@@ -135,20 +148,86 @@ std::vector<int> run_planner(
     return {};
 }
 
-std::vector<int> run_planner(const planning::RoadMap &road_map, const EkfSlam &ekf,
-                             [[maybe_unused]] const BeaconPotential &beacon_potential,
-                             [[maybe_unused]] const proto::OptimisticBRMPlanner &config,
-                             const double max_sensor_range_m,
-                             const std::optional<time::RobotTimestamp::duration> &timeout) {
+PlannerResult run_planner(const planning::RoadMap &road_map, const EkfSlam &ekf,
+                          [[maybe_unused]] const BeaconPotential &beacon_potential,
+                          [[maybe_unused]] const proto::OptimisticBRMPlanner &config,
+                          const double max_sensor_range_m,
+                          const std::optional<time::RobotTimestamp::duration> &timeout) {
     const BeliefRoadMapOptions options =
         BeliefRoadMapOptions{.max_sensor_range_m = max_sensor_range_m,
                              .uncertainty_tolerance = std::nullopt,
                              .max_num_edge_transforms = std::numeric_limits<int>::max(),
                              .timeout = timeout};
+    const time::RobotTimestamp start_time = time::current_robot_time();
     const auto maybe_plan = compute_belief_road_map_plan(road_map, ekf, {}, options);
-    CHECK(maybe_plan.has_value());
+    const time::RobotTimestamp::duration elapsed_time = time::current_robot_time() - start_time;
 
-    return maybe_plan->nodes;
+    const double log_prob = beacon_potential.log_prob(beacon_potential.members());
+
+    return {.elapsed_time = elapsed_time,
+            .plan = maybe_plan.has_value() ? std::make_optional(PlannerResult::Plan{
+                                                 .nodes = maybe_plan->nodes,
+                                                 .log_prob_mass_tracked = log_prob,
+                                             })
+                                           : std::nullopt};
+}
+
+struct ComparisonResult {
+    struct PlanStatistics {
+        time::RobotTimestamp::duration elapsed_time;
+        double expected_covariance_determinant;
+        int num_successful_plans;
+    };
+    std::unordered_map<std::string, PlanStatistics> results;
+    StartGoal start_goal;
+};
+
+ComparisonResult evaluate_results([[maybe_unused]] const StartGoal &start_goal,
+                                  const WorldMap &world_map, const planning::RoadMap &road_map,
+                                  const EkfSlam &ekf,
+                                  const std::unordered_map<std::string, PlannerResult> &results,
+                                  const int evaluation_seed, const int num_eval_trials,
+                                  const double max_sensor_range_m) {
+    std::mt19937 gen(evaluation_seed);
+
+    std::unordered_map<std::string, ComparisonResult::PlanStatistics> results_map;
+    for (int i = 0; i < num_eval_trials; i++) {
+        const auto present_beacons = world_map.beacon_potential().sample(make_in_out(gen));
+        const auto belief_updater = make_belief_updater(road_map, max_sensor_range_m, ekf,
+                                                        present_beacons, TransformType::COVARIANCE);
+        const RobotBelief initial_belief = {
+            .local_from_robot = ekf.estimate().local_from_robot(),
+            .cov_in_robot = ekf.estimate().robot_cov(),
+        };
+        for (const auto &[name, result] : results) {
+            if (results_map.find(name) == results_map.end()) {
+                results_map[name] = {
+                    .elapsed_time = result.elapsed_time,
+                    .expected_covariance_determinant = 0.0,
+                    .num_successful_plans = 0,
+                };
+            }
+
+            if (result.plan.has_value()) {
+                RobotBelief belief = initial_belief;
+                const auto &plan = result.plan->nodes;
+                for (int i = 1; i < static_cast<int>(plan.size()); i++) {
+                    belief = belief_updater(belief, plan.at(i - 1), plan.at(i));
+                }
+
+                results_map[name].num_successful_plans++;
+                const double ratio = 1.0 / results_map[name].num_successful_plans;
+                results_map[name].expected_covariance_determinant =
+                    (1 - ratio) * results_map[name].expected_covariance_determinant +
+                    ratio * belief.cov_in_robot.determinant();
+            }
+        }
+    }
+
+    return {
+        .results = results_map,
+        .start_goal = start_goal,
+    };
 }
 
 }  // namespace
@@ -184,56 +263,58 @@ void run_experiment(const ExperimentConfig &config, const std::filesystem::path 
     const std::optional<time::RobotTimestamp::duration> timeout =
         config.has_plan_timeout_s() ? std::make_optional(time::as_duration(config.plan_timeout_s()))
                                     : std::nullopt;
-    std::for_each(std::execution::par, start_goals.begin(), start_goals.end(),
-                  [=, &remaining](const auto &elem) mutable {
-                      const auto &[idx, start_goal] = elem;
-                      // Add the start goal to the road map
-                      road_map.add_start_goal(
-                          {.start = start_goal.start,
-                           .goal = start_goal.goal,
-                           .connection_radius_m = config.start_goal_connection_radius_m()});
 
-                      // Set the initial pose
-                      const liegroups::SE2 local_from_robot =
-                          liegroups::SE2::trans(start_goal.start);
-                      ekf.estimate().local_from_robot(local_from_robot);
+    std::for_each(
+        std::execution::par, start_goals.begin(), start_goals.end(),
+        [=, &remaining, eval_base_seed = config.evaluation_base_seed(),
+         num_eval_trials = config.num_eval_trials(),
+         max_sensor_range_m = config.max_sensor_range_m()](const auto &elem) mutable {
+            const auto &[idx, start_goal] = elem;
+            // Add the start goal to the road map
+            road_map.add_start_goal(
+                {.start = start_goal.start,
+                 .goal = start_goal.goal,
+                 .connection_radius_m = config.start_goal_connection_radius_m()});
 
-                      std::unordered_map<std::string, std::vector<int>> results;
+            // Set the initial pose
+            const liegroups::SE2 local_from_robot = liegroups::SE2::trans(start_goal.start);
+            ekf.estimate().local_from_robot(local_from_robot);
 
-                      for (const auto &planner_config : config.planner_configs()) {
-                          CHECK(planner_config.planner_config_oneof_case() !=
-                                proto::PlannerConfig::PLANNER_CONFIG_ONEOF_NOT_SET);
-                          switch (planner_config.planner_config_oneof_case()) {
-                              case proto::PlannerConfig::kLandmarkBrmConfig: {
-                                  results[planner_config.name()] =
-                                      run_planner(road_map, ekf, world_map.beacon_potential(),
-                                                  planner_config.landmark_brm_config(),
-                                                  planner_config.max_sensor_range_m(), timeout);
-                                  break;
-                              }
-                              case proto::PlannerConfig::kOptimisticBrmConfig: {
-                                  results[planner_config.name()] =
-                                      run_planner(road_map, ekf, world_map.beacon_potential(),
-                                                  planner_config.optimistic_brm_config(),
-                                                  planner_config.max_sensor_range_m(), timeout);
-                                  break;
-                              }
-                              case proto::PlannerConfig::kExpectedBrmConfig: {
-                                  results[planner_config.name()] =
-                                      run_planner(road_map, ekf, world_map.beacon_potential(),
-                                                  planner_config.expected_brm_config(),
-                                                  planner_config.max_sensor_range_m(), timeout);
-                                  break;
-                              }
-                              default: {
-                                  CHECK(false, "Unhandled Planner Config type",
-                                        planner_config.planner_config_oneof_case());
-                              }
-                          }
-                      }
-                      // evaluate_results(world_map, road_map, results);
-                      std::cout << "remaining: " << --remaining << std::endl;
-                  });
+            std::unordered_map<std::string, PlannerResult> results;
+
+            for (const auto &planner_config : config.planner_configs()) {
+                CHECK(planner_config.planner_config_oneof_case() !=
+                      proto::PlannerConfig::PLANNER_CONFIG_ONEOF_NOT_SET);
+                switch (planner_config.planner_config_oneof_case()) {
+                    case proto::PlannerConfig::kLandmarkBrmConfig: {
+                        results[planner_config.name()] = run_planner(
+                            road_map, ekf, world_map.beacon_potential(),
+                            planner_config.landmark_brm_config(), max_sensor_range_m, timeout);
+                        break;
+                    }
+                    case proto::PlannerConfig::kOptimisticBrmConfig: {
+                        results[planner_config.name()] = run_planner(
+                            road_map, ekf, world_map.beacon_potential(),
+                            planner_config.optimistic_brm_config(), max_sensor_range_m, timeout);
+                        break;
+                    }
+                    case proto::PlannerConfig::kExpectedBrmConfig: {
+                        results[planner_config.name()] = run_planner(
+                            road_map, ekf, world_map.beacon_potential(),
+                            planner_config.expected_brm_config(), max_sensor_range_m, timeout);
+                        break;
+                    }
+                    default: {
+                        CHECK(false, "Unhandled Planner Config type",
+                              planner_config.planner_config_oneof_case());
+                    }
+                }
+            }
+
+            evaluate_results(start_goal, world_map, road_map, ekf, results, eval_base_seed + idx,
+                             num_eval_trials, max_sensor_range_m);
+            std::cout << "remaining: " << --remaining << std::endl;
+        });
 
     // Write out the results
 }
