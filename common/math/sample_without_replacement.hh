aaa7a2bee3c274ebf4e91d492bb7c888b32be114
Erick Fuentes <fuentes.erick@gmail.com>
2024-01-17 12:33:13 -0500

    implement faster sampling without replacement

--- common/math/sample_without_replacement.hh
+++ common/math/sample_without_replacement.hh
@@ -4,12 +4,9 @@
 #include <random>
 #include <vector>
 
-#include "absl/container/flat_hash_set.h"
 #include "common/argument_wrapper.hh"
 
 namespace robot::math {
-absl::flat_hash_set<int> reservoir_sample_without_replacement(const std::vector<double> &p,
-                                                              const int num_samples,
-                                                              const bool is_log_p,
-                                                              InOut<std::mt19937> gen);
+std::vector<int> sample_without_replacement(const std::vector<double> &p, const int num_samples,
+                                            const bool is_log_p, InOut<std::mt19937> gen);
 }
