222d5b6d6948879496a90774f93616577a378edc
Erick Fuentes <fuentes.erick@gmail.com>
2024-01-24 22:40:00 -0500

    get ipympl plots working

--- common/python/jupyter_notebook.py
+++ common/python/jupyter_notebook.py
@@ -7,6 +7,7 @@ from jupyterlab import labapp
 if __name__ == '__main__':
     print(labapp.__file__)
     app_dir = Path(labapp.__file__).parent / '..' / '..' / 'data/share/jupyter/lab'
+    app_dir = app_dir.resolve()
     sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
     sys.argv.extend(['--app-dir', str(app_dir)])
     print(sys.argv)
