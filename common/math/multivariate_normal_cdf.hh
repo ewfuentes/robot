783ff604b06fc737b74b6e56e8461241d9b046ba
Erick Fuentes <fuentes.erick@gmail.com>
2024-01-29 21:15:48 -0500

    Add wrapper around approxcdf

---
+++ common/math/multivariate_normal_cdf.hh
@@ -0,0 +1,19 @@
+
+#pragma once
+
+#include <optional>
+
+#include "Eigen/Core"
+
+namespace robot::math {
+
+// Compute the probability P(x_0 < ub[0], x_1 < ub[1], ..., x_n < ub[n))
+// where the underlying probability density function is a multivariate normal
+// with mean mu and covariance sigma.
+//
+// Returns std::nullopt if an error occurs.
+std::optional<double> multivariate_normal_cdf(const Eigen::VectorXd &mu,
+                                              const Eigen::MatrixXd &sigma,
+                                              const Eigen::VectorXd &upper_bound,
+                                              const bool return_log_p);
+}  // namespace robot::math
