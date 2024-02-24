563ee808ed39c410ad715450445276c75674f172
Erick Fuentes <erick@csail.mit.edu>
2024-02-24 05:13:33 -0500

    build nearest neighbors graph

--- experimental/prm_guarantees/nearest_neighbor_graph_test.cc
+++ experimental/prm_guarantees/nearest_neighbor_graph_test.cc
@@ -1,14 +1,16 @@
 
-#include "gtest/gtest.h"
+#include <algorithm>
+#include <execution>
+#include <iostream>
+#include <random>
 
 #include "annoy/annoylib.h"
 #include "annoy/kissrandom.h"
-
-#include <random>
-#include <iostream>
+#include "gtest/gtest.h"
 
 namespace robot::experimental::prm_guarantee {
-using AnnoyIndex = Annoy::AnnoyIndex<int, double, Annoy::Euclidean, Annoy::Kiss32Random, Annoy::AnnoyIndexMultiThreadedBuildPolicy>;
+using AnnoyIndex = Annoy::AnnoyIndex<int, double, Annoy::Euclidean, Annoy::Kiss32Random,
+                                     Annoy::AnnoyIndexMultiThreadedBuildPolicy>;
 
 TEST(NearestNeighborGraphTest, dim_20_1m_pts) {
     // Sample 1m points in a unit hypercube [0, 1]^n and add them to the index
@@ -30,7 +32,8 @@ TEST(NearestNeighborGraphTest, dim_20_1m_pts) {
         // The index makes a copy of the data, so it's okay overwrite it on the next iteration
         annoy_index.add_item(idx, data.data());
 
-        std::cout << "Adding items to index: " << static_cast<double>(i) / NUM_PTS * 100.0 << " %\r";
+        std::cout << "Adding items to index: " << static_cast<double>(i) / NUM_PTS * 100.0
+                  << " %\r";
     }
     std::cout << std::endl;
 
@@ -48,23 +51,26 @@ TEST(NearestNeighborGraphTest, dim_20_1m_pts) {
 
     // Build the nearest neighbor graph
     constexpr int SEARCH_K = -1;
-    constexpr int IDX = 0;
     constexpr int NUM_NEIGHBORS = 256;
 
-    std::vector<std::vector<int>> neighbors_by_idx;
-    neighbors_by_idx.reserve(NUM_PTS);
+    std::vector<std::vector<int>> neighbors_by_idx(NUM_PTS, std::vector<int>{});
+    std::vector<std::vector<double>> distances_by_idx(NUM_PTS, std::vector<double>{});
 
+    std::vector<int> idxs(NUM_PTS, 0);
+    std::iota(idxs.begin(), idxs.end(), 0);
 
-    for (int i = 0; i < NUM_PTS; i++) {
-        neighbors_by_idx.push_back({});
-        neighbors_by_idx.back().reserve(NUM_NEIGHBORS);
-        annoy_index.get_nns_by_item(IDX, NUM_NEIGHBORS, SEARCH_K, &neighbors_by_idx.back(), nullptr);
-
-        if (i % 1000 == 0) {
-            std::cout << "Computing nearest neighbors: " << static_cast<float>(i) / NUM_PTS * 100.0 << " %\r" << std::flush;
+    std::atomic<int> counter(0);
+    std::for_each(std::execution::par_unseq, idxs.begin(), idxs.end(), [&](const int idx) mutable {
+        std::vector<int> &neighbors = neighbors_by_idx.at(idx);
+        std::vector<double> &distances = distances_by_idx.at(idx);
+        neighbors.reserve(NUM_NEIGHBORS);
+        annoy_index.get_nns_by_item(idx, NUM_NEIGHBORS, SEARCH_K, &neighbors, &distances);
+        const int completed_count = ++counter;
+        if (completed_count % 1000 == 0) {
+            std::cout << "Computing nearest neighbors: "
+                      << static_cast<double>(completed_count) / NUM_PTS * 100.0 << " %\r"
+                      << std::flush;
         }
-    }
-    std::cout << std::endl;
-
+    });
 }
-}
+}  // namespace robot::experimental::prm_guarantee
