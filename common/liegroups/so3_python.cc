7f40ebbfded4be3006ce6546a6e53dc55aebc97d
Erick Fuentes <fuentes.erick@gmail.com>
2024-03-21 12:14:18 -0400

    add python bindings

---
+++ common/liegroups/so3_python.cc
@@ -0,0 +1,54 @@
+#include "common/liegroups/so3.hh"
+#include "pybind11/eigen.h"
+#include "pybind11/operators.h"
+#include "pybind11/pybind11.h"
+
+namespace py = pybind11;
+
+namespace robot::liegroups {
+
+PYBIND11_MODULE(so3_python, m) {
+    py::class_<SO3>(m, "SO3")
+        .def(py::init<>())
+        // Construct from quaternion. Note that we use the (x, y, z, w) convention
+        .def_static("from_quat", [](const Eigen::Vector<double, 4> &quat) -> SO3 { 
+            const Eigen::Quaterniond eigen_quat(quat(3), quat(0), quat(1), quat(2));
+            return SO3(eigen_quat); 
+        })
+        .def_static("from_matrix", [](const Eigen::Matrix3d &rot) -> SO3 { 
+            return SO3(rot); 
+        })
+        .def_static("exp", [](const Eigen::Vector3d &tangent_vec) -> SO3 { return SO3::exp(tangent_vec); })
+        .def_static("rotX", [](const double theta) -> SO3 { return SO3::rotX(theta); })
+        .def_static("rotY", [](const double theta) -> SO3 { return SO3::rotY(theta); })
+        .def_static("rotZ", [](const double theta) -> SO3 { return SO3::rotZ(theta); })
+        .def("log", &SO3::log)
+        .def("inverse", [](const SO3 &a) -> SO3 { return a.inverse(); })
+        .def("matrix", &SO3::matrix)
+        .def("quaternion", [](const SO3 &self){ 
+            const Eigen::Quaterniond quat = self.unit_quaternion();
+            return Eigen::Vector<double, 4>{quat.x(), quat.y(), quat.z(), quat.w()};
+        })
+        .def(py::self * Eigen::Vector3d())
+        .def(
+            "__matmul__",
+            [](const SO3 &a, const Eigen::Matrix3Xd &b) -> Eigen::Matrix3Xd {
+                Eigen::Matrix3Xd out(3, b.cols());
+                for (int i = 0; i < b.cols(); i++) {
+                    out.col(i) = a * b.col(i);
+                }
+                return out;
+            },
+            py::is_operator())
+        .def(
+            "__mul__", [](const SO3 &a, const SO3 &b) -> SO3 { return a * b; }, py::is_operator())
+        .def(
+            "__imul__",
+            [](SO3 &a, const SO3 &b) -> SO3 & {
+                a *= b;
+                return a;
+            },
+            py::is_operator());
+}
+}  // namespace robot::liegroups
+
---
+++ common/liegroups/so3_python_test.py
@@ -0,0 +1,162 @@
+
+import unittest
+import numpy as np
+
+from common.liegroups.so3_python import SO3
+
+class SO3PythonTest(unittest.TestCase):
+
+    def test_default_construction_is_identity(self):
+        # Action
+        b_from_a = SO3()
+
+        # Verification
+        quat = b_from_a.quaternion()
+        self.assertEqual(quat[0], 0.0)
+        self.assertEqual(quat[1], 0.0)
+        self.assertEqual(quat[2], 0.0)
+        self.assertEqual(quat[3], 1.0)
+
+        mat = b_from_a.matrix()
+        for i in range(3):
+            for j in range(3):
+                self.assertEqual(mat[i, j], 1.0 if i == j else 0.0)
+
+    def test_construct_from_quat(self):
+        # Setup
+        rotation_axis = np.array([1, 2, 3])
+        rotation_axis = rotation_axis / np.linalg.norm(rotation_axis)
+        rotation_angle = 0.456
+        c = np.cos(rotation_angle / 2.0)
+        s = np.sin(rotation_angle / 2.0)
+        b_from_a_quat = np.concatenate([rotation_axis * s, [c]])
+        a_from_b_quat = np.concatenate([-rotation_axis * s, [c]])
+
+        # Action
+        b_from_a = SO3.from_quat(b_from_a_quat)
+        a_from_b = SO3.from_quat(a_from_b_quat)
+
+        # Verification
+        a_from_a = a_from_b * b_from_a
+        a_from_a_quat = a_from_a.quaternion()
+        self.assertEqual(a_from_a_quat[0], 0.0)
+        self.assertEqual(a_from_a_quat[1], 0.0)
+        self.assertEqual(a_from_a_quat[2], 0.0)
+        self.assertEqual(a_from_a_quat[3], 1.0)
+
+    def test_inverse(self):
+        # Setup
+        rotation_axis = np.array([1, 2, 3])
+        rotation_axis = rotation_axis / np.linalg.norm(rotation_axis)
+        rotation_angle = 0.456
+        c = np.cos(rotation_angle / 2.0)
+        s = np.sin(rotation_angle / 2.0)
+        b_from_a_quat = np.concatenate([rotation_axis * s, [c]])
+
+        # Action
+        b_from_a = SO3.from_quat(b_from_a_quat)
+        a_from_b = b_from_a.inverse()
+
+        # Verification
+        a_from_a = a_from_b * b_from_a
+        a_from_a_quat = a_from_a.quaternion()
+        self.assertEqual(a_from_a_quat[0], 0.0)
+        self.assertEqual(a_from_a_quat[1], 0.0)
+        self.assertEqual(a_from_a_quat[2], 0.0)
+        self.assertEqual(a_from_a_quat[3], 1.0)
+
+    def test_rot_x(self):
+        # Setup
+        rotation_angle = 0.456
+        c = np.cos(rotation_angle / 2.0)
+        s = np.sin(rotation_angle / 2.0)
+
+        # Action
+        b_from_a = SO3.rotX(rotation_angle)
+
+        # Verification
+        b_from_a_quat = b_from_a.quaternion()
+        self.assertEqual(b_from_a_quat[0], s)
+        self.assertEqual(b_from_a_quat[1], 0.0)
+        self.assertEqual(b_from_a_quat[2], 0.0)
+        self.assertEqual(b_from_a_quat[3], c)
+
+    def test_rot_y(self):
+        # Setup
+        rotation_angle = 0.456
+        c = np.cos(rotation_angle / 2.0)
+        s = np.sin(rotation_angle / 2.0)
+
+        # Action
+        b_from_a = SO3.rotY(rotation_angle)
+
+        # Verification
+        b_from_a_quat = b_from_a.quaternion()
+        self.assertEqual(b_from_a_quat[0], 0.0)
+        self.assertEqual(b_from_a_quat[1], s)
+        self.assertEqual(b_from_a_quat[2], 0.0)
+        self.assertEqual(b_from_a_quat[3], c)
+
+    def test_rot_z(self):
+        # Setup
+        rotation_angle = 0.456
+        c = np.cos(rotation_angle / 2.0)
+        s = np.sin(rotation_angle / 2.0)
+
+        # Action
+        b_from_a = SO3.rotZ(rotation_angle)
+
+        # Verification
+        b_from_a_quat = b_from_a.quaternion()
+        self.assertEqual(b_from_a_quat[0], 0.0)
+        self.assertEqual(b_from_a_quat[1], 0.0)
+        self.assertEqual(b_from_a_quat[2], s)
+        self.assertEqual(b_from_a_quat[3], c)
+
+    def test_exp_log(self):
+        # Setup
+        rotation_angle = 0.456
+        rotation_axis = np.array([1, 2, 3])
+        rotation_axis = rotation_axis / np.linalg.norm(rotation_axis)
+        c = np.cos(rotation_angle / 2.0)
+        s = np.sin(rotation_angle / 2.0)
+        tangent = rotation_axis * rotation_angle
+        b_from_a_quat_expected = np.concatenate([rotation_axis * s, [c]])
+
+        # Action
+        b_from_a = SO3.exp(tangent)
+
+        # Verification
+        b_from_a_quat = b_from_a.quaternion()
+        b_from_a_log = b_from_a.log()
+
+        self.assertAlmostEqual(b_from_a_quat[0], b_from_a_quat_expected[0])
+        self.assertAlmostEqual(b_from_a_quat[1], b_from_a_quat_expected[1])
+        self.assertAlmostEqual(b_from_a_quat[2], b_from_a_quat_expected[2])
+        self.assertAlmostEqual(b_from_a_quat[3], b_from_a_quat_expected[3])
+
+        self.assertAlmostEqual(b_from_a_log[0], tangent[0])
+        self.assertAlmostEqual(b_from_a_log[1], tangent[1])
+        self.assertAlmostEqual(b_from_a_log[2], tangent[2])
+
+    def test_group_action(self):
+        # Setup
+        b_from_a = SO3.rotX(np.pi / 2.0)
+        x_in_a = np.array([1, 0, 0])
+        y_in_a = np.array([0, 1, 0])
+
+        # Action
+        x_in_b = b_from_a * x_in_a
+        y_in_b = b_from_a * y_in_a
+
+        # Verification
+        self.assertAlmostEqual(x_in_b[0], x_in_a[0])
+        self.assertAlmostEqual(x_in_b[1], x_in_a[1])
+        self.assertAlmostEqual(x_in_b[2], x_in_a[2])
+
+        self.assertAlmostEqual(y_in_b[0], 0.0)
+        self.assertAlmostEqual(y_in_b[1], 0.0)
+        self.assertAlmostEqual(y_in_b[2], 1.0)
+
+if __name__ == "__main__":
+    unittest.main()
