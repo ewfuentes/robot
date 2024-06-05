ee07474822f83e6f192a108158046882c9bb333c
Erick Fuentes <fuentes.erick@gmail.com>
2024-06-05 05:58:35 -0400

    lint

--- common/thread_pool_test.cc
+++ common/thread_pool_test.cc
@@ -2,22 +2,21 @@
 #include <chrono>
 #include <thread>
 
-#include "gtest/gtest.h"
 #include "BS_thread_pool.hpp"
-
+#include "gtest/gtest.h"
 
 namespace robot {
 TEST(ThreadPoolTest, future_test) {
     // Setup
     // Action + Verification
-    
+
     BS::thread_pool pool(4);
     std::array<int, 2> results = {0, 0};
-    pool.detach_task([result = &results[0]](){
+    pool.detach_task([result = &results[0]]() {
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
         *result = 1;
     });
-    pool.detach_task([result = &results[1]](){
+    pool.detach_task([result = &results[1]]() {
         std::this_thread::sleep_for(std::chrono::milliseconds(250));
         *result = 2;
     });
@@ -25,4 +24,4 @@ TEST(ThreadPoolTest, future_test) {
 
     std::cout << results[0] << " " << results[1] << std::endl;
 }
-}
+}  // namespace robot
