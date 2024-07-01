508103f7653ee302255b34c55930d5b3285aeccb
Erick Fuentes <erick@csail.mit.edu>
2024-07-01 14:40:45 -0400

    add goal and initial pose to sim config

--- experimental/beacon_sim/sim_config.hh
+++ experimental/beacon_sim/sim_config.hh
@@ -5,6 +5,8 @@
 #include <string>
 #include <variant>
 
+#include "Eigen/Core"
+#include "common/liegroups/se2.hh"
 #include "common/time/robot_time.hh"
 
 namespace robot::experimental::beacon_sim {
@@ -29,6 +31,8 @@ struct SimConfig {
     std::optional<std::string> map_output_path;
     std::optional<std::string> world_map_config;
     std::optional<std::string> road_map_config;
+    Eigen::Vector2d goal_in_map;
+    liegroups::SE2 map_from_initial_robot;
     time::RobotTimestamp::duration dt;
     PlannerConfig planner_config;
     bool load_off_diagonals;
