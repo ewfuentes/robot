17da06d0e468d71abaf18698aa605de6ec2301a0
Erick Fuentes <erick@csail.mit.edu>
2024-07-02 16:20:06 -0400

    Add utility function to get process memory usage

---
+++ common/linux/get_memory_usage.cc
@@ -0,0 +1,21 @@
+
+#include <fstream>
+#include <sstream>
+#include <regex>
+
+#include "common/check.hh"
+
+namespace robot::linux {
+
+int get_memory_usage_kb() {
+    std::ifstream in("/proc/self/smaps_rollup");
+    std::stringstream stream;
+    stream << in.rdbuf();
+    std::regex memory_matcher("Rss:\\s+(\\d+)");
+    std::smatch match;
+    std::string contents = stream.str();
+    const bool match_found = std::regex_search(contents, match, memory_matcher);
+    CHECK(match_found);
+    return std::stoi(match[1]);
+}
+}
