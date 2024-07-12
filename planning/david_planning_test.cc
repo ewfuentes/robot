4b256e3deed5cc83de630d0035c2293293749c06
David Flores <davidjmflores@gmail.com>
2024-07-12 10:36:17 -0400

    Planner State at first test

--- planning/david_planning_test.cc
+++ planning/david_planning_test.cc
@@ -9,7 +9,6 @@ TEST(DavidPlannerTest, david_planning_test) {
     // Setup
     const DavidPlannerConfig david_config{
         .max_paths = 100,
-        .max_states_per_path = 50,
         .max_beacons = 2,
         .favor_goal = 0.8,
         .favor_exploration = 0.2,
@@ -71,73 +70,6 @@ TEST(DavidPlannerTest, david_planning_test) {
         const experimental::beacon_sim::EkfSlamEstimate &est = ekf_slam.estimate();
         const DavidPlannerConfig david_config{
             .max_plans = 1,
-            .max_states_per_path = 50,
-            .favor_goal = 0.7,
-            .favor_exploration = 1 - david_config.favor_goal,
-            .edge_cost = 5.0,
-            .max_num_edge_transforms = std::numeric_limits<int>::max(),
-            .timeout = std::nullopt,
-            .uncertainty_tolerance = std::nullopt,
-        };
-
-        auto successor_func = [&road_map](const int &node_idx) -> std::vector<Successor<int>> {
-            std::vector<Successor<int>> out;
-            const std::vector<std::tuple<int, Eigen::Vector2d>> &vector = road_map.neighbors(node_idx);
-
-            for(auto tuple : vector){
-                const Eigen::Vector2d a_coords = road_map.point(node_idx);
-                const Eigen::Vector2d b_coords = get<1>(tuple);
-                double d = sqrt(pow((b_coords[0]-a_coords[0]),2)+pow((b_coords[1]-a_coords[1]),2));
-
-                out.push_back({
-                        .state = get<0>(tuple),
-                        .edge_cost = d,
-                        });
-            };
-            return out;
-        };
-        auto termination_check = [](std::unordered_map<int, double>) { return false; };
-
-        // ACTION
-
-        const auto maybe_plan = david_planner<int>(successor_func,termination_check,road_map,david_config,est,beacon_potential,ekf_config);
-
-        // VERIFICATION
-        
-    }
-*/
-    TEST(DavidPlannerTest, testing_evaluation){
-
-        // SETUP
-        const experimental::beacon_sim::EkfSlamConfig ekf_config{
-        .max_num_beacons = 2,
-        .initial_beacon_uncertainty_m = 100.0,
-        .along_track_process_noise_m_per_rt_meter = 0.05,
-        .cross_track_process_noise_m_per_rt_meter = 0.05,
-        .pos_process_noise_m_per_rt_s = 0.0,
-        .heading_process_noise_rad_per_rt_meter = 1e-3,
-        .heading_process_noise_rad_per_rt_s = 0.0,
-        .beacon_pos_process_noise_m_per_rt_s = 1e-6,
-        .range_measurement_noise_m = 1e-1,
-        .bearing_measurement_noise_rad = 1e-1,
-        .on_map_load_position_uncertainty_m = 2.0,
-        .on_map_load_heading_uncertainty_rad = 0.1,
-        };
-
-        const double P_FIRST_BEACON = 0.9;
-        const double P_SECOND_BEACON = 0.9;
-        const auto &[grid, ekf_slam, potential] = create_david_grid_environment(ekf_config,P_FIRST_BEACON,P_SECOND_BEACON);
-        const auto road_map = grid;
-        const auto beacon_potential = potential;
-        const auto ekf = ekf_slam;
-
-        const experimental::beacon_sim::EkfSlamEstimate &est = ekf_slam.estimate();
-        const DavidPlannerConfig david_config{
-            .max_plans = 1,
-            .max_states_per_path = 75,
-            .favor_goal = 0.5,
-            .favor_exploration = 1 - david_config.favor_goal,
-            .edge_cost = 5.0,
             .max_num_edge_transforms = std::numeric_limits<int>::max(),
             .timeout = std::nullopt,
             .max_sensor_range_m = 4,
@@ -163,13 +95,87 @@ TEST(DavidPlannerTest, david_planning_test) {
         auto termination_check = [](std::unordered_map<int, double>) { return false; };
 
         // ACTION
-        const auto maybe_plan = david_planner<int>(successor_func,termination_check,road_map,david_config,est,beacon_potential,ekf);
+
+        const auto beacons = beacon_mapping<int>(successor_func,termination_check,road_map,est,david_config);
+        // VERIFICATION
+        std::cout << "Elements in beacons: " << beacons.size() << std::endl;
+        std::vector<int> beacon_ids = est.beacon_ids;
+        std::unordered_map<int,bool> p_beacon_pass_in;
+        for(int i=0;i<(int)beacon_ids.size();i++){
+            std::cout << "Beacon #" << i+1 << ":" << std::endl;
+            std::cout << "Beacon ID: " << beacon_ids[i] << std::endl;
+            p_beacon_pass_in.clear();
+            p_beacon_pass_in[beacon_ids[i]] = true;
+            std::cout << "Beacon Probability: " << exp(beacon_potential.log_prob(p_beacon_pass_in,true)) << std:: endl;
+            std::cout << "Beacon Mapped to Node: " << beacons.at(beacon_ids[i]).node << std::endl;
+            std::cout << std::endl;
+
+        }
+
+    }
+    */
+    TEST(DavidPlannerTest, testing_evaluation){
+
+        // SETUP
+        const experimental::beacon_sim::EkfSlamConfig ekf_config{
+        .max_num_beacons = 2,
+        .initial_beacon_uncertainty_m = 100.0,
+        .along_track_process_noise_m_per_rt_meter = 0.05,
+        .cross_track_process_noise_m_per_rt_meter = 0.05,
+        .pos_process_noise_m_per_rt_s = 0.0,
+        .heading_process_noise_rad_per_rt_meter = 1e-3,
+        .heading_process_noise_rad_per_rt_s = 0.0,
+        .beacon_pos_process_noise_m_per_rt_s = 1e-6,
+        .range_measurement_noise_m = 1e-1,
+        .bearing_measurement_noise_rad = 1e-1,
+        .on_map_load_position_uncertainty_m = 2.0,
+        .on_map_load_heading_uncertainty_rad = 0.1,
+        };
+
+        const double P_FIRST_BEACON = 0.9;
+        const double P_SECOND_BEACON = 0.01;
+        const auto &[grid, ekf_slam, potential] = create_david_grid_environment(ekf_config,P_FIRST_BEACON,P_SECOND_BEACON);
+        const auto road_map = grid;
+        const auto beacon_potential = potential;
+        const auto ekf = ekf_slam;
+
+        const experimental::beacon_sim::EkfSlamEstimate &est = ekf_slam.estimate();
+        const DavidPlannerConfig david_config{
+            .max_visits = 2,
+            .max_plans = 1,
+            .max_num_edge_transforms = std::numeric_limits<int>::max(), // am not using right now
+            .timeout = std::nullopt,
+            .max_sensor_range_m = 4,
+            .uncertainty_tolerance = std::nullopt,
+        };
+
+        auto successor_func = [&road_map](const int &node_idx) -> std::vector<Successor<int>> {
+            std::vector<Successor<int>> out;
+            const std::vector<std::tuple<int, Eigen::Vector2d>> &vector = road_map.neighbors(node_idx);
+
+            for(auto tuple : vector){
+                const Eigen::Vector2d a_coords = road_map.point(node_idx);
+                const Eigen::Vector2d b_coords = get<1>(tuple);
+                double d = sqrt(pow((b_coords[0]-a_coords[0]),2)+pow((b_coords[1]-a_coords[1]),2));
+
+                out.push_back({
+                        .state = get<0>(tuple),
+                        .edge_cost = d,
+                        });
+            };
+            return out;
+        };
+        auto termination_check = [](std::unordered_map<int, double>) { return false; };
+
+        // ACTION
+        const double FAVOR_GOAL = 0.3;
+        const auto maybe_plan = david_planner<int>(successor_func,termination_check,road_map,david_config,est,beacon_potential,ekf,FAVOR_GOAL);
         //VERIFICATION
 
         // Printing beacons
         std::vector<int> beacon_ids = est.beacon_ids;
         std::unordered_map<int,bool> p_beacon_pass_in;
-        const auto beacons = beacon_mapping<int>(successor_func,termination_check,road_map,est);
+        const auto beacons = beacon_mapping<int>(successor_func,termination_check,road_map,est,david_config);
         for(int i=0;i<(int)beacon_ids.size();i++){
             std::cout << "Beacon #" << i+1 << ":" << std::endl;
             std::cout << "Beacon ID: " << beacon_ids[i] << std::endl;
@@ -181,8 +187,8 @@ TEST(DavidPlannerTest, david_planning_test) {
 
         }
         // Printing config
-        std::cout << david_config.favor_goal*100 << "% Goal oriented and " << david_config.favor_exploration*100 << "% exploratory" << std::endl;
-        std::cout << "Sampled From " << david_config.max_plans << " Plans, With All Below " << david_config.max_states_per_path << " States Per Path." << std::endl;
+        std::cout << FAVOR_GOAL*100 << "% Goal oriented and " << (1-FAVOR_GOAL)*100 << "% exploratory" << std::endl;
+        std::cout << "Sampled From " << david_config.max_plans << " Plans" << std::endl;
         std::cout << std::endl;
         // Printing optimal path
         std::cout << "Optimal Path: [";
