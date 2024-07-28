79875dc98174afdc47a7a772188eae58f10c40c0
Erick Fuentes <fuentes.erick@gmail.com>
2024-07-28 11:29:21 -0400

    Add support for binding parameters to queries

--- common/sqlite3/sqlite3.cc
+++ common/sqlite3/sqlite3.cc
@@ -5,6 +5,12 @@
 #include "sqlite3/sqlite3.h"
 
 namespace robot::sqlite3 {
+
+struct Database::Statement::Impl {
+    std::shared_ptr<sqlite3_stmt> stmt;
+    std::shared_ptr<std::vector<std::string>> column_names;
+};
+
 struct Database::Impl {
     Impl(const std::filesystem::path &path) { check_result(sqlite3_open(path.c_str(), &db_)); }
     ~Impl() { check_result(sqlite3_close(db_)); }
@@ -14,60 +20,100 @@ struct Database::Impl {
         CHECK(error_code == SQLITE_OK, sqlite3_errmsg(db_));
     };
 
-    std::vector<Row> query(const std::string &statement) {
-        struct OutParams {
-            std::vector<Row> rows;
-            std::shared_ptr<std::vector<std::string>> column_names;
-        };
-        OutParams out;
-
+    Database::Statement prepare(const std::string &statement) {
         sqlite3_stmt *stmt;
-        check_result(sqlite3_prepare_v2(db_, statement.c_str(), statement.size(), &stmt, nullptr));
+        sqlite3_prepare_v2(db_, statement.c_str(), statement.size(), &stmt, nullptr);
+        return Statement{.impl_ =
+                             std::make_unique<Database::Statement::Impl>(Database::Statement::Impl{
+                                 .stmt = std::shared_ptr<sqlite3_stmt>(
+                                     stmt, [](sqlite3_stmt *stmt) { sqlite3_finalize(stmt); }),
+                             })};
+    }
 
-        int result = SQLITE_OK;
-        const int num_columns = sqlite3_column_count(stmt);
-        while ((result = sqlite3_step(stmt)) != SQLITE_DONE) {
-            CHECK(result == SQLITE_ROW);
-
-            if (out.column_names == nullptr) {
-                out.column_names = std::make_shared<std::vector<std::string>>();
-                for (int i = 0; i < num_columns; i++) {
-                    out.column_names->push_back(sqlite3_column_name(stmt, i));
-                }
-            }
-
-            std::vector<Database::Value> values;
-            for (int i = 0; i < num_columns; i++) {
-                switch (sqlite3_column_type(stmt, i)) {
-                    case SQLITE_NULL: {
-                        values.push_back(std::nullopt);
-                        break;
+    void bind(const Statement &stmt, const std::unordered_map<std::string, Database::Value> &args) {
+        sqlite3_stmt *stmt_ptr = stmt.impl_->stmt.get();
+        CHECK(args.size() == sqlite3_bind_parameter_count(stmt_ptr),
+              "insufficient number of arguments", sqlite3_sql(stmt_ptr), args);
+        for (const auto &[key, value] : args) {
+            const int param_idx = sqlite3_bind_parameter_index(stmt_ptr, key.c_str());
+            std::visit(
+                [param_idx, stmt_ptr](const auto &arg) {
+                    using T = std::decay_t<decltype(arg)>;
+                    if constexpr (std::is_same_v<T, std::nullopt_t>) {
+                        sqlite3_bind_null(stmt_ptr, param_idx);
+                    } else if constexpr (std::is_same_v<T, int>) {
+                        sqlite3_bind_int(stmt_ptr, param_idx, arg);
+                    } else if constexpr (std::is_same_v<T, double>) {
+                        sqlite3_bind_double(stmt_ptr, param_idx, arg);
+                    } else if constexpr (std::is_same_v<T, std::vector<unsigned char>>) {
+                        sqlite3_bind_blob(stmt_ptr, param_idx, arg.data(), arg.size(),
+                                          SQLITE_TRANSIENT);
+                    } else if constexpr (std::is_same_v<T, std::string>) {
+                        constexpr int TO_FIRST_TERMINATOR = -1;
+                        sqlite3_bind_text(stmt_ptr, param_idx, arg.c_str(), TO_FIRST_TERMINATOR,
+                                          SQLITE_TRANSIENT);
                     }
-                    case SQLITE_INTEGER: {
-                        values.push_back(sqlite3_column_int(stmt, i));
-                        break;
-                    }
-                    case SQLITE_FLOAT: {
-                        values.push_back(sqlite3_column_double(stmt, i));
-                        break;
-                    }
-                    case SQLITE3_TEXT: {
-                        values.push_back(
-                            reinterpret_cast<const char *>(sqlite3_column_text(stmt, i)));
-                        break;
-                    }
-                    case SQLITE_BLOB: {
-                        const int size = sqlite3_column_bytes(stmt, i);
-                        const unsigned char *buf =
-                            reinterpret_cast<const unsigned char *>(sqlite3_column_blob(stmt, i));
-                        values.emplace_back(std::vector(buf, buf + size));
-                    }
-                }
-            }
-            out.rows.emplace_back(Row(std::move(values), out.column_names));
+                },
+                value);
         }
-        sqlite3_finalize(stmt);
-        return out.rows;
+    }
+
+    std::optional<Row> step(const Statement &stmt) {
+        sqlite3_stmt *stmt_ptr = stmt.impl_->stmt.get();
+        const int result = sqlite3_step(stmt_ptr);
+        if (result == SQLITE_DONE) {
+            return std::nullopt;
+        }
+        CHECK(result == SQLITE_ROW, sqlite3_errmsg(db_));
+        const int num_columns = sqlite3_column_count(stmt_ptr);
+        if (stmt.impl_->column_names == nullptr) {
+            std::vector<std::string> column_names;
+            for (int i = 0; i < num_columns; i++) {
+                column_names.push_back(sqlite3_column_name(stmt_ptr, i));
+            }
+            stmt.impl_->column_names =
+                std::make_shared<std::vector<std::string>>(std::move(column_names));
+        }
+
+        std::vector<Database::Value> values;
+        for (int i = 0; i < num_columns; i++) {
+            switch (sqlite3_column_type(stmt_ptr, i)) {
+                case SQLITE_NULL: {
+                    values.push_back(std::nullopt);
+                    break;
+                }
+                case SQLITE_INTEGER: {
+                    values.push_back(sqlite3_column_int(stmt_ptr, i));
+                    break;
+                }
+                case SQLITE_FLOAT: {
+                    values.push_back(sqlite3_column_double(stmt_ptr, i));
+                    break;
+                }
+                case SQLITE3_TEXT: {
+                    values.push_back(
+                        reinterpret_cast<const char *>(sqlite3_column_text(stmt_ptr, i)));
+                    break;
+                }
+                case SQLITE_BLOB: {
+                    const int size = sqlite3_column_bytes(stmt_ptr, i);
+                    const unsigned char *buf =
+                        reinterpret_cast<const unsigned char *>(sqlite3_column_blob(stmt_ptr, i));
+                    values.emplace_back(std::vector(buf, buf + size));
+                }
+            }
+        }
+        return Row(std::move(values), stmt.impl_->column_names);
+    }
+
+    std::vector<Row> query(const std::string &statement) {
+        Statement stmt = prepare(statement);
+
+        std::vector<Row> out;
+        for (std::optional<Row> r = step(stmt); r.has_value(); r = step(stmt)) {
+            out.push_back(r.value());
+        }
+        return out;
     }
     struct sqlite3 *db_;
 };
@@ -82,6 +128,8 @@ int Database::Row::size() const { return column_names_->size(); }
 
 const Database::Value &Database::Row::value(const int idx) const { return values_.at(idx); }
 
+Database::Statement::~Statement() = default;
+
 Database::~Database() = default;
 Database::Database(const std::filesystem::path &path)
     : impl_(std::make_unique<Database::Impl>(path)) {}
@@ -89,4 +137,18 @@ Database::Database(const std::filesystem::path &path)
 std::vector<Database::Row> Database::query(const std::string &statement) {
     return impl_->query(statement);
 }
+
+Database::Statement Database::prepare(const std::string &statement) {
+    return impl_->prepare(statement);
+}
+
+void Database::bind(const Database::Statement &stmt,
+                    const std::unordered_map<std::string, Database::Value> &args) {
+    impl_->bind(stmt, args);
+}
+
+std::optional<Database::Row> Database::step(const Database::Statement &stmt) {
+    return impl_->step(stmt);
+}
+
 }  // namespace robot::sqlite3
--- common/sqlite3/sqlite3_test.cc
+++ common/sqlite3/sqlite3_test.cc
@@ -14,7 +14,7 @@ namespace robot::sqlite3 {
 TEST(Sqlite3Test, open_database_test) {
     // Setup
     const char *tmp_dir = std::getenv("TEST_TMPDIR");
-    const std::filesystem::path db_path = std::filesystem::path(tmp_dir) / "test.db";
+    const std::filesystem::path db_path = std::filesystem::path(tmp_dir) / "test_query.db";
 
     Database db(db_path);
     // Action
@@ -38,6 +38,44 @@ TEST(Sqlite3Test, open_database_test) {
                         EXPECT_NEAR(arg, 789.456, 1e-6);
                     } else {
                         EXPECT_EQ(arg, "hello");
+                    }
+                },
+                row.value(i));
+        }
+    }
+}
+
+TEST(Sqlite3Test, bind_statement_test) {
+    // Setup
+    const char *tmp_dir = std::getenv("TEST_TMPDIR");
+    const std::filesystem::path db_path = std::filesystem::path(tmp_dir) / "test_bind_statement.db";
+    Database db(db_path);
+
+    // Action
+    db.query(
+        "CREATE TABLE test_table (rowid INTEGER PRIMARY KEY ASC, name TEXT NOT NULL, age REAL) "
+        "STRICT;");
+    const auto statement =
+        db.prepare("INSERT INTO test_table (rowid,name,age) VALUES (:rowid,:name,:age);");
+    db.bind(statement, {{":rowid", 10}, {":name", "Foo"}, {":age", 34.456}});
+    db.step(statement);
+
+    const auto rows = db.query("SELECT * FROM test_table;");
+
+    // Verification
+    for (const auto &row : rows) {
+        for (int i = 0; i < row.size(); i++) {
+            std::visit(
+                [](const auto &arg) {
+                    using T = std::decay_t<decltype(arg)>;
+                    if constexpr (std::is_same_v<T, std::nullopt_t>) {
+                    } else if constexpr (std::is_same_v<T, std::vector<unsigned char>>) {
+                    } else if constexpr (std::is_same_v<T, int>) {
+                        EXPECT_EQ(arg, 10);
+                    } else if constexpr (std::is_same_v<T, double>) {
+                        EXPECT_NEAR(arg, 34.456, 1e-6);
+                    } else {
+                        EXPECT_EQ(arg, "Foo");
                     }
                 },
                 row.value(i));
