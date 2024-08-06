f73318858f6d6dd649d4664188bcbf91a15b09c9
Erick Fuentes <erick@csail.mit.edu>
2024-08-05 22:09:11 -0400

    Add regret bound (#317)

--- experimental/beacon_sim/reprocess_result.cc
+++ experimental/beacon_sim/reprocess_result.cc
@@ -1,5 +1,7 @@
 
 #include <filesystem>
+#include <limits>
+#include <optional>
 #include <random>
 
 #include "BS_thread_pool.hpp"
@@ -8,6 +10,7 @@
 #include "common/math/matrix_to_proto.hh"
 #include "common/proto/load_from_file.hh"
 #include "cxxopts.hpp"
+#include "experimental/beacon_sim/beacon_potential.hh"
 #include "experimental/beacon_sim/belief_road_map_planner.hh"
 #include "experimental/beacon_sim/ekf_slam.hh"
 #include "experimental/beacon_sim/experiment_results.pb.h"
@@ -20,6 +23,11 @@
 #include "planning/road_map_to_proto.hh"
 
 namespace robot::experimental::beacon_sim {
+namespace {
+struct StartGoal {
+    Eigen::Vector2d start;
+    Eigen::Vector2d goal;
+};
 
 EkfSlam load_ekf_slam(const MappedLandmarks &mapped_landmarks) {
     EkfSlam ekf(
@@ -68,9 +76,91 @@ std::tuple<BeaconPotential, planning::RoadMap, EkfSlam> load_environment(
     return {world_map.beacon_potential(), road_map, load_ekf_slam(mapped_landmarks)};
 }
 
+std::vector<std::vector<proto::PlanMetrics>> compute_oracle_metrics(
+    const planning::RoadMap &road_map, const std::vector<std::vector<int>> &present_beacon_samples,
+    const BeaconPotential &beacon_potential, const std::vector<StartGoal> &start_goals,
+    const EkfSlam &ekf, const double max_sensor_range_m,
+    const double start_goal_connection_radius_m, const int num_threads) {
+    std::vector<std::vector<proto::PlanMetrics>> oracle_plan_metrics_by_eval_trial_id(
+        present_beacon_samples.size());
+
+    const auto expected_det_metric =
+        make_uncertainty_size<RobotBelief>(ExpectedDeterminant{.position_only = false});
+    const auto expected_pos_det_metric =
+        make_uncertainty_size<RobotBelief>(ExpectedDeterminant{.position_only = true});
+    const auto prob_mass_in_region_metric = make_uncertainty_size<RobotBelief>(ProbMassInRegion{
+        .position_x_half_width_m = 0.5,
+        .position_y_half_width_m = 0.5,
+        .heading_half_width_rad = 6.0,
+    });
+
+    const auto expected_trace_metric =
+        make_uncertainty_size<RobotBelief>(ExpectedTrace{.position_only = false});
+    const auto expected_position_trace_metric =
+        make_uncertainty_size<RobotBelief>(ExpectedTrace{.position_only = true});
+
+    std::atomic<int> remaining(present_beacon_samples.size() * start_goals.size());
+    BS::thread_pool pool(num_threads);
+    for (int i = 0; i < static_cast<int>(present_beacon_samples.size()); i++) {
+        pool.detach_task([=, &present_beacons = present_beacon_samples.at(i), &start_goals,
+                          &plan_metrics = oracle_plan_metrics_by_eval_trial_id.at(i),
+                          &beacon_potential, road_map = road_map, ekf = ekf, &remaining]() mutable {
+            std::unordered_map<int, bool> assignment;
+            for (const int beacon_id : present_beacons) {
+                const auto iter = std::find(beacon_potential.members().begin(),
+                                            beacon_potential.members().end(), beacon_id);
+                assignment[beacon_id] = iter != beacon_potential.members().end();
+            }
+            BeaconPotential conditioned_potential = beacon_potential.conditioned_on(assignment);
+            for (int start_goal_idx = 0; start_goal_idx < static_cast<int>(start_goals.size());
+                 start_goal_idx++) {
+                const auto &start_goal = start_goals.at(start_goal_idx);
+                road_map.add_start_goal({.start = start_goal.start,
+                                         .goal = start_goal.goal,
+                                         .connection_radius_m = start_goal_connection_radius_m});
+                const liegroups::SE2 map_from_robot = liegroups::SE2::trans(start_goal.start);
+                ekf.estimate().local_from_robot(map_from_robot);
+                const auto brm_plan = compute_belief_road_map_plan(
+                    road_map, ekf, conditioned_potential,
+                    {.max_sensor_range_m = max_sensor_range_m,
+                     .uncertainty_tolerance = std::nullopt,
+                     .max_num_edge_transforms = std::numeric_limits<int>::max(),
+                     .timeout = std::nullopt,
+                     .uncertainty_size_options = ProbMassInRegion{
+                         .position_x_half_width_m = 0.5,
+                         .position_y_half_width_m = 0.5,
+                         .heading_half_width_rad = 6.0,
+                     }});
+
+                proto::PlanMetrics metrics;
+                metrics.set_expected_trace(expected_trace_metric(brm_plan.value().beliefs.back()));
+                metrics.set_expected_position_trace(
+                    expected_position_trace_metric(brm_plan.value().beliefs.back()));
+                metrics.set_expected_determinant(
+                    expected_det_metric(brm_plan.value().beliefs.back()));
+                metrics.set_expected_position_determinant(
+                    expected_pos_det_metric(brm_plan.value().beliefs.back()));
+                metrics.set_prob_mass_in_region(
+                    prob_mass_in_region_metric(brm_plan.value().beliefs.back()));
+
+                plan_metrics.push_back(metrics);
+
+                const int plans_remaining = --remaining;
+
+                if (plans_remaining % 1 == 0) {
+                    std::cout << "Oracle metrics remaining: " << plans_remaining << std::endl;
+                }
+            }
+        });
+    }
+    pool.wait();
+    return oracle_plan_metrics_by_eval_trial_id;
+}
+}  // namespace
+
 void reprocess_result(const proto::ExperimentResult &result,
                       const std::filesystem::path &experiment_config_path,
-                      const std::filesystem::path &output_path) {
+                      const std::filesystem::path &output_path, const int num_threads) {
     proto::ExperimentResult out = result;
     const auto &experiment_config = result.experiment_config();
     // Load the beacon potential, road map and ekf from the config file
@@ -113,17 +203,33 @@ void reprocess_result(const proto::ExperimentResult &result,
         out.add_landmark_eval_samples(std::move(sample_string));
     }
 
-    BS::thread_pool pool(0);
+    // Get a list of the start goals
+    std::vector<StartGoal> start_goals;
+    for (const auto &proto_sg : result.start_goal()) {
+        start_goals.push_back({
+            .start = unpack_from<Eigen::Vector2d>(proto_sg.start()),
+            .goal = unpack_from<Eigen::Vector2d>(proto_sg.goal()),
+        });
+    }
+
+    const auto &oracle_metrics_by_eval_trial_id =
+        compute_oracle_metrics(road_map, samples, beacon_potential, start_goals, ekf,
+                               experiment_config.max_sensor_range_m(),
+                               experiment_config.start_goal_connection_radius_m(), num_threads);
+
+    BS::thread_pool pool(num_threads);
     std::vector<proto::PlannerResult> new_results(out.results_size());
     for (int i = 0; i < out.results_size(); i++) {
         new_results.at(i).CopyFrom(result.results(i));
         pool.detach_task([=, &samples, &experiment_result = result, &out = new_results.at(i),
-                          &experiment_config]() mutable {
+                          &experiment_config, &oracle_metrics_by_eval_trial_id]() mutable {
             double expected_det = 0.0;
             double expected_pos_det = 0.0;
             double expected_prob = 0.0;
             double expected_trace = 0.0;
             double expected_position_trace = 0.0;
+
+            double expected_prob_regret = 0.0;
             out.mutable_plan()->mutable_sampled_plan_metrics()->Reserve(
                 experiment_config.num_eval_trials());
             for (int eval_trial_id = 0; eval_trial_id < experiment_config.num_eval_trials();
@@ -154,11 +260,16 @@ void reprocess_result(const proto::ExperimentResult &result,
                                             out.plan().nodes(plan_node_idx + 1));
                 }
 
+                const auto &oracle_metrics =
+                    oracle_metrics_by_eval_trial_id.at(eval_trial_id).at(out.trial_id());
                 const double sampled_expected_det = expected_det_metric(belief);
                 const double sampled_expected_pos_det = expected_pos_det_metric(belief);
                 const double sampled_prob_mass_in_region = prob_mass_in_region_metric(belief);
                 const double sampled_expected_trace = expected_trace_metric(belief);
                 const double sampled_expected_pos_trace = expected_position_trace_metric(belief);
+
+                const double prob_regret =
+                    sampled_prob_mass_in_region - oracle_metrics.prob_mass_in_region();
 
                 expected_det += sampled_expected_det / experiment_config.num_eval_trials();
                 expected_pos_det += sampled_expected_pos_det / experiment_config.num_eval_trials();
@@ -167,12 +278,15 @@ void reprocess_result(const proto::ExperimentResult &result,
                 expected_position_trace +=
                     sampled_expected_pos_trace / experiment_config.num_eval_trials();
 
+                expected_prob_regret += (prob_regret) / experiment_config.num_eval_trials();
+
                 auto &sampled_metrics = *out.mutable_plan()->add_sampled_plan_metrics();
                 sampled_metrics.set_expected_determinant(sampled_expected_det);
                 sampled_metrics.set_expected_position_determinant(sampled_expected_pos_det);
                 sampled_metrics.set_prob_mass_in_region(sampled_prob_mass_in_region);
                 sampled_metrics.set_expected_trace(sampled_expected_trace);
                 sampled_metrics.set_expected_position_trace(sampled_expected_pos_trace);
+                sampled_metrics.set_prob_mass_in_region_regret(prob_regret);
             }
             // curr_result.mutable_plan()->clear_expected_size();
             out.mutable_plan()->mutable_average_plan_metrics()->set_expected_determinant(
@@ -184,6 +298,8 @@ void reprocess_result(const proto::ExperimentResult &result,
             out.mutable_plan()->mutable_average_plan_metrics()->set_expected_trace(expected_trace);
             out.mutable_plan()->mutable_average_plan_metrics()->set_expected_position_trace(
                 expected_position_trace);
+            out.mutable_plan()->mutable_average_plan_metrics()->set_prob_mass_in_region_regret(
+                expected_prob_regret);
             std::cout << out.trial_id() << " " << out.planner_id() << std::endl;
         });
     }
@@ -204,12 +320,15 @@ void reprocess_result(const proto::ExperimentResult &result,
 
 int main(int argc, const char **argv) {
     // clang-format off
+    const std::string DEFAULT_NUM_THREADS = "0";
     cxxopts::Options options("reprocess_result",
                              "Add additional uncertainty metrics to ExperimentResult protos");
     options.add_options()
         ("results_file", "Path to results file", cxxopts::value<std::string>())
         ("experiment_config_path", "Path to Experiment Config file", cxxopts::value<std::string>())
         ("output_path", "Output path", cxxopts::value<std::string>())
+        ("num_threads", "Number of threads to use",
+            cxxopts::value<int>()->default_value(DEFAULT_NUM_THREADS))
         ("help", "Print usage");
     // clang-format on
 
@@ -241,5 +360,6 @@ int main(int argc, const char **argv) {
     robot::experimental::beacon_sim::reprocess_result(
         maybe_results_file.value(),
         std::filesystem::path(args["experiment_config_path"].as<std::string>()),
-        std::filesystem::path(args["output_path"].as<std::string>()));
+        std::filesystem::path(args["output_path"].as<std::string>()),
+        args["num_threads"].as<int>());
 }
