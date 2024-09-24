5f9ed84e0a4f7158c957a5a62b9dfe7c47b8c732
Erick Fuentes <erick@csail.mit.edu>
2024-09-24 16:02:36 -0400

    Add Weathers to CanadianTravelerGraph (#323)

--- domain/canadian_traveler.cc
+++ domain/canadian_traveler.cc
@@ -2,11 +2,18 @@
 #include "domain/canadian_traveler.hh"
 
 #include <algorithm>
+#include <memory>
 
 namespace robot::domain {
+namespace {
+using CTG = CanadianTravelerGraph;
+}
 
-CanadianTravelerGraph::CanadianTravelerGraph(std::vector<CanadianTravelerGraph::Node> nodes,
-                                             std::vector<CanadianTravelerGraph::Edge> edges)
+std::shared_ptr<CTG> CTG::create(std::vector<CTG::Node> nodes, std::vector<CTG::Edge> edges) {
+    return std::shared_ptr<CTG>(new CTG(std::move(nodes), std::move(edges)));
+}
+
+CTG::CanadianTravelerGraph(std::vector<CTG::Node> nodes, std::vector<CTG::Edge> edges)
     : nodes_{std::move(nodes)}, edges_{} {
     // Push back a copy of each edge with the directions reversed
     edges_.reserve(2 * edges.size());
@@ -44,7 +51,68 @@ CanadianTravelerGraph::CanadianTravelerGraph(std::vector<CanadianTravelerGraph::
     }
 }
 
-bool CanadianTravelerGraph::Edge::operator==(const CanadianTravelerGraph::Edge &other) const {
+CTG::Weather CTG::create_weather(const std::vector<EdgeBelief> &edge_beliefs) const {
+    return CTG::Weather(weak_from_this(), edge_beliefs);
+}
+
+CTG::Weather::Weather(std::weak_ptr<const CTG> graph,
+                      const std::vector<CTG::EdgeBelief> &edge_beliefs)
+    : graph_(std::move(graph)) {
+    const auto graph_ptr = graph_.lock();
+    edges_.reserve(graph_ptr->edges().size());
+
+    for (const auto &edge : graph_ptr->edges()) {
+        if (!edge.traversal_prob.has_value()) {
+            edges_.push_back(edge);
+        } else {
+            // This is a probabilistic edge, check to see if we have an observation for it
+            const auto iter = std::find_if(
+                edge_beliefs.begin(), edge_beliefs.end(), [&edge](const auto &edge_belief) -> bool {
+                    const bool is_match =
+                        (edge_belief.id_a == edge.node_a && edge_belief.id_b == edge.node_b) ||
+                        (edge_belief.id_a == edge.node_b && edge_belief.id_b == edge.node_a);
+                    return is_match;
+                });
+            if (iter == edge_beliefs.end()) {
+                // No observation for this edge, copy the edge over
+                edges_.push_back(edge);
+            }
+
+            if (iter->traversability == CTG::EdgeState::Unknown) {
+                edges_.push_back(edge);
+            } else if (iter->traversability == CTG::EdgeState::Traversable) {
+                edges_.push_back(CTG::Edge{
+                    .node_a = edge.node_a,
+                    .node_b = edge.node_b,
+                    .cost = edge.cost,
+                    .traversal_prob = std::nullopt,
+                });
+            }
+        }
+    }
+
+    neighbors_.reserve(graph_ptr->nodes().size());
+    auto range_start = edges_.begin();
+    for (auto iter = edges_.begin(); iter != edges_.end(); iter++) {
+        if (iter->node_a != range_start->node_a) {
+            while (static_cast<int>(neighbors_.size()) != range_start->node_a) {
+                neighbors_.push_back(std::span<Edge>());
+            }
+            neighbors_.push_back(std::span(range_start, iter));
+            range_start = iter;
+        }
+    }
+    while (static_cast<int>(neighbors_.size()) != range_start->node_a) {
+        neighbors_.push_back(std::span<Edge>());
+    }
+    neighbors_.push_back(std::span(range_start, edges_.end()));
+
+    while (neighbors_.size() != graph_ptr->nodes().size()) {
+        neighbors_.push_back(std::span<Edge>());
+    }
+}
+
+bool CTG::Edge::operator==(const CTG::Edge &other) const {
     return node_a == other.node_a && node_b == other.node_b && cost == other.cost &&
            traversal_prob == other.traversal_prob;
 }
--- domain/canadian_traveler.hh
+++ domain/canadian_traveler.hh
@@ -1,6 +1,7 @@
 
 #pragma once
 
+#include <memory>
 #include <optional>
 #include <span>
 #include <vector>
@@ -9,7 +10,9 @@
 
 namespace robot::domain {
 
-class CanadianTravelerGraph {
+class Weather;
+
+class CanadianTravelerGraph : public std::enable_shared_from_this<CanadianTravelerGraph> {
    public:
     using NodeId = int;
     struct Node {
@@ -26,15 +29,44 @@ class CanadianTravelerGraph {
         bool operator==(const Edge &other) const;
     };
 
-    CanadianTravelerGraph(std::vector<Node> nodes, std::vector<Edge> edges);
+    enum class EdgeState { Unknown, Untraversable, Traversable };
+    struct EdgeBelief {
+        CanadianTravelerGraph::NodeId id_a;
+        CanadianTravelerGraph::NodeId id_b;
+        EdgeState traversability;
+    };
+
+    // A weather represents a (possibly incomplete) traversability assignment to the unknown
+    // edges of a CanadianTravelerGraph
+    class Weather {
+       public:
+        const std::span<Edge> &neighbors(const NodeId id) const { return neighbors_.at(id); }
+
+       private:
+        friend CanadianTravelerGraph;
+        Weather(std::weak_ptr<const CanadianTravelerGraph> graph,
+                const std::vector<EdgeBelief> &edge_beliefs);
+        std::vector<Edge> edges_;
+        std::vector<std::span<Edge>> neighbors_;
+        std::weak_ptr<const CanadianTravelerGraph> graph_;
+    };
+
+    static std::shared_ptr<CanadianTravelerGraph> create(std::vector<Node> nodes,
+                                                         std::vector<Edge> edges);
 
     const std::vector<Node> &nodes() const { return nodes_; }
 
     const std::span<Edge> &neighbors(const NodeId id) const { return neighbors_.at(id); }
 
+    const std::vector<Edge> &edges() const { return edges_; }
+
+    Weather create_weather(const std::vector<EdgeBelief> &edge_beliefs = {}) const;
+
    private:
+    CanadianTravelerGraph(std::vector<Node> nodes, std::vector<Edge> edges);
     std::vector<Node> nodes_;
     std::vector<Edge> edges_;
     std::vector<std::span<Edge>> neighbors_;
 };
+
 }  // namespace robot::domain
