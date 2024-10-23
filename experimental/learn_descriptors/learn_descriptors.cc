f71f22242dcb7b40179593f9bdc41a1fcdcc6248
Nico Buono <66542842+PizzaRoll04@users.noreply.github.com>
2024-10-23 15:01:38 -0400

    nbuono first commit! added learn_descriptors. just general setup (#330)
    plz accept

---
+++ experimental/learn_descriptors/learn_descriptors.cc
@@ -0,0 +1,7 @@
+#include "experimental/learn_descriptors/learn_descriptors.hh"
+
+#include <iostream>
+
+namespace robot::experimental::learn_descriptors {
+void hello_world(const std::string &in) { std::cout << in << std::endl; }
+}  // namespace robot::experimental::learn_descriptors
---
+++ experimental/learn_descriptors/learn_descriptors_test.cc
@@ -0,0 +1,7 @@
+#include "experimental/learn_descriptors/learn_descriptors.hh"
+
+#include "gtest/gtest.h"
+
+namespace robot::experimental::learn_descriptors {
+TEST(LearnDescriptorsTest, hello_world) { hello_world("sup"); }
+}  // namespace robot::experimental::learn_descriptors
