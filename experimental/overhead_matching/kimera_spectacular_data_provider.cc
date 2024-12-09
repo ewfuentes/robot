125e3b9a2a5856be8b8790fc446db5d21b92633c
Ethan Fahnestock <ekf@mit.edu>
2024-12-09 16:54:24 -0500

    finishing up frame delivery

--- experimental/overhead_matching/kimera_spectacular_data_provider.cc
+++ experimental/overhead_matching/kimera_spectacular_data_provider.cc
@@ -1,7 +1,7 @@
 #include "experimental/overhead_matching/kimera_spectacular_data_provider.hh"
 
 #include <gflags/gflags.h>
-#include <glog/logging.h>
+// #include <glog/logging.h>
 #include <gtsam/base/Vector.h>
 #include <gtsam/geometry/Cal3DS2.h>
 #include <gtsam/geometry/Pose3.h>
@@ -29,6 +29,11 @@ DEFINE_int64(final_k, 100000,
 
 namespace robot::experimental::overhead_matching {
 
+VIO::Timestamp vio_time_from_robot_time(const time::RobotTimestamp& robot_time) {
+    /// VIO timestamp is int64_t nanoseconds. time::RobotTimestamp is std::nano
+    return robot_time.time_since_epoch().count();
+}
+
 /* -------------------------------------------------------------------------- */
 SpectacularDataProviderInterface::SpectacularDataProviderInterface(const std::string& dataset_path,
                                                                    const int& initial_k,
@@ -37,12 +42,12 @@ SpectacularDataProviderInterface::SpectacularDataProviderInterface(const std::st
     : DataProviderInterface(),
       vio_params_(vio_params),
       dataset_path_(dataset_path),
-      spec_log_(dataset_path),
       current_k_(std::numeric_limits<VIO::FrameId>::max()),
       initial_k_(initial_k),
       final_k_(final_k),
-      imu_measurements_() {
-    CHECK(!dataset_path_.empty()) << "Dataset path for EurocDataProvider is empty.";
+      imu_measurements_(),
+      spec_log_(dataset_path) {
+    // CHECK(!dataset_path_.empty()) << "Dataset path for SpectacularDataProvider is empty.";
 
     // Start processing dataset from frame initial_k.
     // Useful to skip a bunch of images at the beginning (imu calibration).
@@ -51,6 +56,12 @@ SpectacularDataProviderInterface::SpectacularDataProviderInterface(const std::st
     // Finish processing dataset at frame final_k.
     // Last frame to process (to avoid processing the entire dataset),
     // skip last frames.
+
+    if (final_k_ > spec_log_.num_frames()) {
+        LOG(WARNING) << "Provided final frame for KimeraSpectacularDataProvider was above the number of frames. Reducing to match size of dataset";
+        final_k_ = spec_log_.num_frames();
+    }
+
     CHECK_GT(final_k_, 0);
 
     CHECK_GT(final_k_, initial_k_)
@@ -94,7 +105,7 @@ bool SpectacularDataProviderInterface::spin() {
             return true;
         }
     }
-    LOG_IF(INFO, shutdown_) << "EurocDataProvider shutdown requested.";
+    LOG_IF(INFO, shutdown_) << "shutdown requested.";
     return false;
 }
 
@@ -103,50 +114,61 @@ bool SpectacularDataProviderInterface::hasData() const { return current_k_ < fin
 /* -------------------------------------------------------------------------- */
 bool SpectacularDataProviderInterface::spinOnce() {
     CHECK_LT(current_k_, std::numeric_limits<VIO::FrameId>::max())
-        << "Are you sure you've initialized current_k_?";
+    //     << "Are you sure you've initialized current_k_?";
     if (current_k_ >= final_k_) {
-        LOG(INFO) << "Finished spinning Euroc dataset.";
+        LOG(INFO) << "Finished spinning dataset.";
         return false;
     }
+    //TODO: How do we want to handle camera parameters? 
+    // const VIO::CameraParams& left_cam_info = vio_params_.camera_params_.at(0);
+    const VIO::CameraParams left_cam_FAKE_PARAMS;
+    const bool& equalize_image = false;
+        // vio_params_.frontend_params_.stereo_matching_params_.equalize_image_;
 
-    const VIO::CameraParams& left_cam_info = vio_params_.camera_params_.at(0);
-    const bool& equalize_image =
-        vio_params_.frontend_params_.stereo_matching_params_.equalize_image_;
+    std::optional<robot::experimental::overhead_matching::FrameGroup> maybe_frame = spec_log_.get_frame(current_k_);
+    // CHECK(maybe_frame.has_value());
+    
 
-    const VIO::Timestamp& timestamp_frame_k = timestampAtFrame(current_k_);
-    VLOG(10) << "Sending left frames k= " << current_k_ << " with timestamp: " << timestamp_frame_k;
+    const VIO::Timestamp& timestamp_frame_k = vio_time_from_robot_time(maybe_frame->time_of_validity);
+    // LOG(INFO) << "Sending left frames k= " << current_k_ << " with timestamp: " << timestamp_frame_k;
 
-    // TODO(Toni): ideally only send cv::Mat raw images...:
-    // - pass params to vio_pipeline ctor
-    // - make vio_pipeline actually equalize or transform images as necessary.
+    cv::Mat bgr = maybe_frame->bgr_frame;
+    if (bgr.channels() > 1) {
+    //   LOG(INFO) << "Converting img from BGR to GRAY...";
+      cv::cvtColor(bgr, bgr, cv::COLOR_BGR2GRAY);
+    }
+    if (equalize_image) {
+        LOG(WARNING) << "- Histogram Equalization for image";
+        cv::equalizeHist(bgr, bgr);
+    }
+       
+    cv::Mat depth_meters = maybe_frame->depth_frame;
+    CHECK(depth_meters.type() == CV_32FC1); // type assumed if depth is distance in meters
 
-    // std::string left_img_filename;
-    // bool available_left_img = getLeftImgName(current_k_, &left_img_filename);
-    // if (available_left_img) {
-    //     // Both stereo images are available, send data to VIO
-    //     CHECK(left_frame_callback_);
-    //     left_frame_callback_(std::make_unique<VIO::AnmsAlgorithmType>(
-    //         current_k_, timestamp_frame_k,
-    //         // TODO(Toni): this info should be passed to
-    //         // the camera... not all the time here...
-    //         left_cam_info,
-    //         VIO::UtilsOpenCV::ReadAndConvertToGrayScale(left_img_filename, equalize_image)));
-    // } else {
-    //     LOG(ERROR) << "Missing left image, proceeding to the next one.";
-    // }
-    // This is done directly when parsing the Imu data.
-    // imu_single_callback_(imu_meas);
+    // CHECK(left_frame_callback_);
+    left_frame_callback_(std::make_unique<VIO::Frame>(
+        current_k_, 
+        timestamp_frame_k,
+        left_cam_FAKE_PARAMS,
+        bgr
+    ));
 
-    VLOG(10) << "Finished VIO processing for frame k = " << current_k_;
+    depth_frame_callback_(std::make_unique<VIO::DepthFrame>(
+            current_k_,
+            timestamp_frame_k,
+            depth_meters
+    ));
+
+    // LOG(INFO) << "Finished VIO processing for frame k = " << current_k_;
     current_k_++;
     return true;
 }
 
+
 VIO::ImuMeasurement vio_imu_from_robot_imu(const ImuSample& robot_imu) {
     Eigen::Matrix<double, 6, 1> imu_acc_gyro;
     imu_acc_gyro << robot_imu.accel_mpss, robot_imu.gyro_radps;
-    VIO::ImuMeasurement vio_imu(robot_imu.time_of_validity.time_since_epoch()
-                                    .count(),  // timestamps are int64_t nanoseconds
+    VIO::ImuMeasurement vio_imu(vio_time_from_robot_time(robot_imu.time_of_validity),
                                 imu_acc_gyro);
 
     return vio_imu;
@@ -177,34 +199,6 @@ void SpectacularDataProviderInterface::sendImuData() const {
     }
 }
 
-// /* -------------------------------------------------------------------------- */
-// bool SpectacularDataProviderInterface::parseDataset() {
-//     // Parse IMU data.
-//     CHECK(parseImuData(dataset_path_, kImuName));
-
-//     // Parse Camera data.
-//     VIO::CameraImageLists left_cam_image_list;
-//     // parseCameraData(kLeftCamName, &left_cam_image_list);
-//     // if (VLOG_IS_ON(1)) left_cam_image_list.print();
-//     // TODO(Toni): remove camera_names_ and camera_image_lists_...
-//     camera_names_.push_back(kLeftCamName);
-//     // WARNING Use [x] not .at() because we are adding entries that do not exist.
-//     camera_image_lists_[kLeftCamName] = left_cam_image_list;
-//     // CHECK(sanityCheckCameraData(camera_names_, &camera_image_lists_));
-
-//     // Parse Ground-Truth data.
-
-//     clipFinalFrame();
-
-//     return true;
-// }
-
-// /* -------------------------------------------------------------------------- */
-// bool SpectacularDataProviderInterface::parseCameraData(const std::string& cam_name,
-//                                                        VIO::CameraImageLists* cam_list_i) {
-//     CHECK_NOTNULL(cam_list_i)->parseCamImgList(dataset_path_ + "/mav0/" + cam_name, "data.csv");
-//     return true;
-// }
 
 std::string SpectacularDataProviderInterface::getDatasetName() {
     if (dataset_name_.empty()) {
@@ -230,10 +224,7 @@ std::string SpectacularDataProviderInterface::getDatasetName() {
 size_t SpectacularDataProviderInterface::getNumImages() const {
     CHECK_GT(camera_names_.size(), 0u);
     const std::string& left_cam_name = camera_names_.at(0);
-    const std::string& right_cam_name = camera_names_.at(0);
     size_t n_left_images = getNumImagesForCamera(left_cam_name);
-    size_t n_right_images = getNumImagesForCamera(right_cam_name);
-    CHECK_EQ(n_left_images, n_right_images);
     return n_left_images;
 }
 
--- experimental/overhead_matching/kimera_spectacular_data_provider_test.cc
+++ experimental/overhead_matching/kimera_spectacular_data_provider_test.cc
@@ -9,19 +9,11 @@
 #include "gtest/gtest.h"
 #include "opencv2/opencv.hpp"
 
+#include "common/video.hh"
+
 #include "kimera-vio/pipeline/Pipeline-definitions.h"
 
 namespace robot::experimental::overhead_matching {
-
-bool images_equal(cv::Mat img1, cv::Mat img2) {
-    if (img1.size() != img2.size() || img1.type() != img2.type()) {
-        return false;
-    }
-    cv::Mat diff;
-    cv::absdiff(img1, img2, diff);
-    diff = diff.reshape(1);
-    return cv::countNonZero(diff) == 0;
-}
 
 std::ostream &operator<<(std::ostream &out, const time::RobotTimestamp &t) {
     std::ostringstream ss;
@@ -62,6 +54,52 @@ bool compare_imu_samples(const robot::experimental::overhead_matching::ImuSample
                             return true;
                          }
 
+bool compare_bgr_frame(const std::unique_ptr<VIO::Frame>& kimera_frame, 
+                       const cv::Mat& spec_bgr,
+                       const time::RobotTimestamp& spec_time,
+                       const int& sequence_index) {
+
+    // check image
+    cv::Mat grey_image;
+    if (spec_bgr.channels() > 1) {
+        cv::cvtColor(spec_bgr, grey_image, cv::COLOR_BGR2GRAY);
+    } else {
+        grey_image = spec_bgr;
+    }
+    if (!common::images_equal(kimera_frame->img_, grey_image)) {
+        return false;
+    }
+    // check timestamps
+    if (kimera_frame->timestamp_ != spec_time.time_since_epoch().count()) {
+        return false;
+    }
+    // check index number
+    if (kimera_frame->id_ != sequence_index) {
+        return false;
+    }
+    return true;
+}
+bool compare_depth_frame(const std::unique_ptr<VIO::DepthFrame>& kimera_depth, 
+                         const cv::Mat& spec_depth,
+                         const time::RobotTimestamp& spec_time,
+                         const int& sequence_index) {
+
+    // check image
+    if (!common::images_equal(kimera_depth->depth_img_, spec_depth)) {
+        return false;
+    }
+    // check timestamps
+    if (kimera_depth->timestamp_ != spec_time.time_since_epoch().count()) {
+        std::cout << "ID mismatch! " << kimera_depth->id_ << " " << spec_time.time_since_epoch().count() << std::endl;
+        return false;
+    }
+    // check index number
+    if (kimera_depth->id_ != sequence_index) {
+        return false;
+    }
+    return true;
+}
+
 TEST(KimeraSpectacularDataProviderTest, happy_case) {
     // Setup
     const std::filesystem::path log_path(
@@ -96,20 +134,37 @@ TEST(KimeraSpectacularDataProviderTest, happy_case) {
     );
 
     std::vector<VIO::ImuMeasurement> imu_queue;
+    std::vector<std::unique_ptr<VIO::Frame>> bgr_queue;
+    std::vector<std::unique_ptr<VIO::DepthFrame>> depth_queue;
 
     auto imu_callback = [&imu_queue](const VIO::ImuMeasurement& measurement) -> void {
         imu_queue.push_back(measurement);
         return;
     };
 
+    auto color_camera_callback = [&bgr_queue](std::unique_ptr<VIO::Frame>&& bgr_frame) -> void {
+        bgr_queue.push_back(std::move(bgr_frame));
+        return;
+    };
+
+    auto depth_camera_callback = [&depth_queue](std::unique_ptr<VIO::DepthFrame>&& depth_frame) -> void {
+        depth_queue.push_back(std::move(depth_frame));
+        return;
+    };
+
     // bind IMU callback
     s_interface.registerImuSingleCallback(imu_callback);
+    // bind images callback
+    s_interface.registerLeftFrameCallback(color_camera_callback);
+    // bind depth callback
+    s_interface.registerDepthFrameCallback(depth_camera_callback);
 
     // Action
-    s_interface.spin(); 
+    while (s_interface.spin()) {
+
+    }
 
     // Verification
-    std::cout << "Size of imu queue: " << imu_queue.size() << std::endl;
     EXPECT_TRUE(imu_queue.size() == times.size());
     // for (auto [imu_true, imu_kimera] : std::views::zip(original_imu_samples, imu_queue)) {
         // EXPECT_TRUE(compare_imu_samples(imu_true, imu_kimera));
@@ -117,7 +172,16 @@ TEST(KimeraSpectacularDataProviderTest, happy_case) {
         EXPECT_TRUE(compare_imu_samples(original_imu_samples[ i ], imu_queue[ i ]));
         break;
     }
-    std::cout << "all of the imu information checked out!" << std::endl;
+
+    // bgr and depth images
+    EXPECT_TRUE(depth_queue.size() == log.num_frames() && bgr_queue.size() == log.num_frames());
+    for (size_t i = 0; i < log.num_frames(); i++) {
+        std::optional<FrameGroup> fg = log.get_frame(i);
+        EXPECT_TRUE(fg.has_value());
+
+        EXPECT_TRUE(compare_bgr_frame(bgr_queue[ i ], fg->bgr_frame, fg->time_of_validity, i));
+        EXPECT_TRUE(compare_depth_frame(depth_queue[ i ], fg->depth_frame, fg->time_of_validity, i));
+    }
 
 }
 
