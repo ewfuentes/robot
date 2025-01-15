1099971c3e19ae1a1e7f293d79269912ee135a50
Erick Fuentes <erick@csail.mit.edu>
2025-01-15 17:01:09 -0500

    add initial cvusa evaluation pipeline

--- experimental/overhead_matching/cvusa_evaluation_pipeline.py
+++ experimental/overhead_matching/cvusa_evaluation_pipeline.py
@@ -4,12 +4,11 @@ import numpy as np
 from dataclasses import dataclass
 
 from collections.abc import Callable
-
+from PIL import Image
 
 @dataclass
 class TrialResult:
     prediction: (float, float)
-
 
 @dataclass
 class EvaluationResult:
@@ -30,3 +29,34 @@ def cvusa_evaluation(
     where 0,0 corresponds to the top left corner of the overhead image and (1, 0) corresponds to the
     top right corner of the overhead image.
     """
+
+    pano_dir = cvusa_path / 'streetview/panos'
+    overhead_dir = cvusa_path / 'bingmap/20'
+
+    per_image_results = {}
+    accumulated_error = 0.0
+    count = 0
+    for pano_path in pano_dir.iterdir():
+        file_name = pano_path.name
+        overhead_path = overhead_dir / file_name
+        assert overhead_path.exists()
+
+        result = method_under_test(
+            overhead=np.asarray(Image.open(overhead_path).convert("RGB")),
+            ego=np.asarray(Image.open(pano_path).convert("RGB")),
+        )
+
+        error_x = result[0] - 0.5
+        error_y = result[1] - 0.5
+        error = (error_x * error_x + error_y * error_y) ** 0.5
+
+        accumulated_error += error
+        count += 1
+
+        per_image_results[file_name] = TrialResult(prediction=result)
+
+    return EvaluationResult(
+        per_image_result=per_image_results,
+        mean_absolute_error=accumulated_error / count
+    )
+
--- experimental/overhead_matching/cvusa_evaluation_pipeline_test.py
+++ experimental/overhead_matching/cvusa_evaluation_pipeline_test.py
@@ -6,18 +6,23 @@ import numpy as np
 
 import experimental.overhead_matching.cvusa_evaluation_pipeline as cep
 
-def test_method(ego: np.ndarray, overhead: np.ndarray):
-    return (0.5, 0.5)
-
 class CVUSAEvaluationPipelineTest(unittest.TestCase):
     def test_happy_case(self):
         # Setup
         dataset_path = Path("external/cvusa_minisubset")
-        self.assertTrue(dataset_path.exists())
+
+        def test_method(*, ego, overhead):
+            ego_height, ego_width, _ = ego.shape
+            ego_aspect = ego_width / ego_height
+            self.assertGreater(ego_aspect, 2.0)
+            return (0.0, 0.0)
 
         # Action
+        result = cep.cvusa_evaluation(dataset_path, test_method)
 
         # Verification
+        expected_mae = 0.5 * 2 ** 0.5
+        self.assertAlmostEqual(result.mean_absolute_error, expected_mae, places=5)
 
 
 
