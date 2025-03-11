Fix diff and move image normalization inside model (#382)
Co-authored-by: Erick Fuentes <fuentes.erick@gmail.com>

common/torch/load_and_save_models.py
@@ -2,10 +2,10 @@ import json
 import copy
 from pathlib import Path
 from datetime import datetime
-import subprocess
 import common.torch.load_torch_deps
 import torch
-
+from toolchain import git_info
+import base64
 
 from dataclasses import is_dataclass
 from collections import namedtuple
@@ -16,28 +16,15 @@ import numpy as np
 
 def get_git_commit_hash():
     """Returns None if fails to get commit hash (e.g. not in a git repo)"""
-    try:
-        commit_hash = subprocess.check_output(
-            ["git", "rev-parse", "HEAD"],
-            stderr=subprocess.STDOUT
-        ).decode("utf-8").strip()
-    except subprocess.CalledProcessError as e:
-        commit_hash = None
-        print("Error retrieving commit hash:", e.output.decode("utf-8"))
-    return commit_hash
+    return git_info.STABLE_GIT_COMMIT
 
 
 def get_git_diff():
-    try:
-        diff = subprocess.check_output(
-            ["git", "diff"],
-            stderr=subprocess.STDOUT
-        ).decode("utf-8").strip()
-    except subprocess.CalledProcessError as e:
-        diff = None
-        print("Error retrieving git diff:")
-        # print("Error retrieving git diff:", e.output.decode("utf-8"))
-    return diff
+    """Returns None if fails to get diff (either not in repo or no active changes)"""
+    if hasattr(git_info, "STABLE_GIT_DIFF"):
+        return base64.b64decode(git_info.STABLE_GIT_DIFF).decode('utf-8')
+    else:
+        return None
 
 
 def deep_equal(a, b, rtol=1e-5, atol=1e-8, print_reason: bool = False):

experimental/overhead_matching/learned/scripts/train_clevr_transformer.py
@@ -6,8 +6,6 @@ import common.torch.load_torch_deps
 import shutil
 from common.torch.load_and_save_models import save_model, load_model
 import torch
-import torchvision as tv
-import torchvision.transforms.v2 as tf
 import numpy as np
 import copy
 from torch.utils.tensorboard import SummaryWriter
@@ -16,12 +14,6 @@ from experimental.overhead_matching.learned.data import clevr_dataset
 from experimental.overhead_matching.learned.model import (
     clevr_tokenizer,
     clevr_transformer,
-)
-
-IMAGE_NORMALIZATION = torch.nn.Sequential(
-    tf.ToImage(),
-    tf.ToDtype(torch.float32, scale=True),
-    tf.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 )
 
 
@@ -129,8 +121,7 @@ def main(dataset_path: Path, output_path: Path, load_model_path: Path | None, tr
     dataset = clevr_dataset.ClevrDataset(dataset_path,
                                          load_overhead=train_config['overhead_image'],
                                          load_ego_images=train_config['ego_image'],
-                                         overhead_transform=IMAGE_NORMALIZATION,
-                                         ego_transform=IMAGE_NORMALIZATION)
+                                         )
     vocabulary = dataset.vocabulary()
     train_dataset, val_dataset = torch.utils.data.random_split(dataset, [0.8, 0.2], generator=torch.Generator().manual_seed(1023))
     loader = clevr_dataset.get_dataloader(train_dataset, batch_size=64, num_workers=12)