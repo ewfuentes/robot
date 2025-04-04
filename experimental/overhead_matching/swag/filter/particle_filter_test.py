1d65d6019c7d9a71fda1baae97b839082a5fd8cd
harelb <harelb@mit.edu>
2025-04-04 13:23:22 -0400

    address pr comments

--- experimental/overhead_matching/swag/filter/particle_filter_test.py
+++ experimental/overhead_matching/swag/filter/particle_filter_test.py
@@ -4,8 +4,8 @@ import torch
 import numpy as np
 from particle_filter import (
     wag_motion_model,
-    wag_observation_liklihood_from_similarity_matrix,
-    wag_update_particle_weights,
+    wag_observation_log_likelihood_from_similarity_matrix,
+    wag_calculate_log_particle_weights,
     wag_multinomial_resampling
 )
 
@@ -42,29 +42,26 @@ class TestParticleFilter(unittest.TestCase):
 
         # Check that noise is applied
         std_dev = torch.std(new_particles[:, 0])
-        self.assertTrue(std_dev > 0)
+        self.assertGreater(std_dev, 0)
 
-    def test_wag_observation_liklihood_from_similarity_matrix(self):
+    def test_wag_observation_likelihood_from_similarity_matrix(self):
         similarity_matrix = torch.tensor([[0.8, 0.3], [0.5, 0.9]])
         sigma = 0.1
 
-        liklihood = wag_observation_liklihood_from_similarity_matrix(
+        log_likelihood = wag_observation_log_likelihood_from_similarity_matrix(
             similarity_matrix, sigma
         )
 
         # Check shape
-        self.assertEqual(liklihood.shape, similarity_matrix.shape)
+        self.assertEqual(log_likelihood.shape, similarity_matrix.shape)
 
         # Check that maximum similarity gives max likelihood
         max_sim_idx = torch.argmax(similarity_matrix)
-        max_lik_idx = torch.argmax(liklihood)
+        max_lik_idx = torch.argmax(log_likelihood)
         self.assertEqual(max_sim_idx, max_lik_idx)
 
-        # Check that values are positive
-        self.assertTrue(torch.all(liklihood > 0))
-
     def test_wag_update_particle_weights(self):
-        observation_liklihood_matrix = torch.tensor([[0.1, 0.2], [0.3, 0.4]])
+        observation_log_likelihood_matrix = torch.log(torch.tensor([[0.1, 0.2], [0.3, 0.4]]))
         patch_positions = torch.zeros((2, 2, 2))
         # Set patch positions at (0,0), (1,0), (0,1), (1,1)
         patch_positions[0, 0] = torch.tensor([0.0, 0.0])
@@ -80,18 +77,15 @@ class TestParticleFilter(unittest.TestCase):
             [0.9, 0.9]   # Close to (1,1)
         ])
 
-        weights = wag_update_particle_weights(
-            observation_liklihood_matrix, patch_positions, particles
+        log_weights = wag_calculate_log_particle_weights(
+            observation_log_likelihood_matrix, patch_positions, particles
         )
 
         # Check shape
-        self.assertEqual(weights.shape, (particles.shape[0],))
+        self.assertEqual(log_weights.shape, (particles.shape[0],))
 
         # Check that weights sum to 1
-        self.assertTrue(torch.isclose(torch.sum(weights), torch.tensor(1.0)))
-
-        # Check that all weights are positive
-        self.assertTrue(torch.all(weights > 0))
+        self.assertTrue(torch.isclose(torch.sum(torch.exp(log_weights)), torch.tensor(1.0)))
 
     def test_wag_multinomial_resampling(self):
         particles = torch.tensor([
@@ -102,17 +96,20 @@ class TestParticleFilter(unittest.TestCase):
         ])
 
         # Heavily weight one particle
-        weights = torch.tensor([0.7, 0.1, 0.1, 0.1])
+        NUM_SAMPLES = 1000
+        log_weights = torch.log(torch.tensor([0.7, 0.1, 0.1, 0.1]))
 
         resampled = wag_multinomial_resampling(
-            particles, weights, self.generator
+            particles, log_weights, self.generator, NUM_SAMPLES
         )
 
         # Check shape
-        self.assertEqual(resampled.shape, particles.shape)
+        self.assertEqual(resampled.shape, (NUM_SAMPLES, 2))
 
         # With high probability, the first particle should appear multiple times
         # due to its high weight, but we can't test this deterministically
+        num_high_likelihood_particle = torch.isclose(resampled, particles[0:1]).prod(dim=1).sum()
+        self.assertGreater(num_high_likelihood_particle, NUM_SAMPLES // 2)
 
     def test_particle_filter_path(self):
         # Initial particles scattered around origin
@@ -142,7 +139,7 @@ class TestParticleFilter(unittest.TestCase):
         motion_steps = torch.tensor([20.0, 0.0])
 
         # Parameters
-        noise_percent = 0.2
+        noise_percent = 0.1
 
         # Save positions for plotting
         all_particle_positions = [particles.clone()]
@@ -160,25 +157,25 @@ class TestParticleFilter(unittest.TestCase):
 
             # Generate similarity matrix (higher value = more similar)
             # Simulating similarity based on distance from ground truth
-            obs_likelihood = torch.zeros((grid_size, grid_size))
+            obs_log_liklihood = torch.zeros((grid_size, grid_size))
             for i in range(grid_size):
                 for j in range(grid_size):
                     patch_pos = patch_positions[i, j]
                     dist = torch.norm(patch_pos - position)
                     # Inverse relationship - closer patches have higher similarity
-                    obs_likelihood[i, j] = torch.exp(-dist/1.0)
+                    obs_log_liklihood[i, j] = -dist
 
             # fig, ax = plt.subplots()
             # ax.imshow(obs_likelihood)
             # plt.savefig(f"/tmp/sim_matrix_{_}.png")
 
             # Update particle weights
-            weights = wag_update_particle_weights(
-                obs_likelihood, patch_positions, particles
+            log_weights = wag_calculate_log_particle_weights(
+                obs_log_liklihood, patch_positions, particles
             )
             # Resample particles
             particles = wag_multinomial_resampling(
-                particles, weights, self.generator
+                particles, log_weights, self.generator
             )
 
             # Save for plotting
