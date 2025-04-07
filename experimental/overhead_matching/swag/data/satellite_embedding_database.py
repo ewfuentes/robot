cc86520485baa50c35e200749a33a398312e4fc4
harelb <harelb@mit.edu>
2025-04-07 16:23:04 -0400

    nans and disable assert option

--- experimental/overhead_matching/swag/data/satellite_embedding_database.py
+++ experimental/overhead_matching/swag/data/satellite_embedding_database.py
@@ -3,21 +3,25 @@ import torch
 
 import experimental.overhead_matching.swag.data.vigor_dataset as vig_dataset
 
+
 def build_satellite_embedding_database(model: torch.nn.Module,
                                        dataloader: torch.utils.data.DataLoader,
                                        device: torch.device = "cuda:0",
-                                       )->torch.Tensor:
+                                       *,
+                                       disable_dataloader_check: bool = False
+                                       ) -> torch.Tensor:
     model.to(device)
     model.eval()
     inf_results = []
     all_indexes = []
     with torch.no_grad():
         for patch_dataset_item in dataloader:
-            assert patch_dataset_item.panorama is None, "Are you sure you want to be buliding the embedding database over a dataset that isn't just overhead patches?"
+            if not disable_dataloader_check:
+                assert patch_dataset_item.panorama is None, "Are you sure you want to be buliding the embedding database over a dataset that isn't just overhead patches?"
             embeddings = model(patch_dataset_item.satellite.to(device))
             all_indexes.extend([x['index'] for x in patch_dataset_item.satellite_metadata])
             inf_results.append(embeddings)
     unsorted_embeddings = torch.concatenate(inf_results, dim=0)
-    sorted_embeddings = torch.zeros_like(unsorted_embeddings)
+    sorted_embeddings = torch.ones_like(unsorted_embeddings) * torch.nan
     sorted_embeddings[torch.tensor(all_indexes, dtype=torch.long)] = unsorted_embeddings
     return sorted_embeddings
