b68ceef127a037d0895e2058041bdc21ce17ac6b
Nicolaniello Buono <mason.cod02@gmail.com>
2025-04-08 10:59:30 -0400

    created a spatial scene helper to create test scenes.  did first pass at refactoring feature handling in SFM, SFM is currently broken.

---
+++ experimental/learn_descriptors/feature_set.hh
@@ -0,0 +1,68 @@
+#pragma once
+#include <optional>
+#include <sstream>
+#include <stdexcept>
+#include <unordered_map>
+
+#include "gtsam/geometry/Pose3.h"
+#include "gtsam/inference/Symbol.h"
+#include "opencv2/opencv.hpp"
+
+namespace std {
+template <>
+struct hash<cv::KeyPoint> {
+    size_t operator()(const cv::KeyPoint &kp) const {
+        size_t h1 = hash<float>()(kp.pt.x);
+        size_t h2 = hash<float>()(kp.pt.y);
+        size_t h3 = hash<float>()(kp.size);
+        size_t h4 = hash<float>()(kp.angle);
+        size_t h5 = hash<float>()(kp.response);
+        size_t h6 = hash<int>()(kp.octave);
+        size_t h7 = hash<int>()(kp.class_id);
+
+        return (h1 ^ (h2 << 1)) ^ (h3 << 2) ^ (h4 << 3) ^ (h5 << 4) ^ (h6 << 5) ^ (h7 << 6);
+    }
+};
+}  // namespace std
+namespace cv {
+inline bool operator==(const cv::KeyPoint &kp1, const cv::KeyPoint &kp2) {
+    return kp1.pt == kp2.pt && kp1.size == kp2.size && kp1.angle == kp2.angle &&
+           kp1.response == kp2.response && kp1.octave == kp2.octave && kp1.class_id == kp2.class_id;
+}
+}  // namespace cv
+namespace robot::experimental::learn_descriptors {
+class FeatureSet {
+   public:
+    struct keypoints_descriptors {
+        keypoints_descriptors(const std::vector<cv::KeyPoint> &keypoints,
+                              const cv::Mat &descriptors)
+            : kpts(keypoints), descriptors(descriptors) {}
+        std::vector<cv::KeyPoint> kpts;
+        cv::Mat descriptors;
+    };
+
+    FeatureSet(const std::vector<cv::KeyPoint> &kpts, const cv::Mat &descriptors)
+        : kpts_descriptors_(keypoints_descriptors(kpts, descriptors)){};
+    ~FeatureSet() = default;
+
+    void insert_symbol(const cv::KeyPoint &kpt, const gtsam::Symbol &symbol) {
+        if (kpt_to_symbol_.find(kpt) != kpt_to_symbol_.end()) {
+            std::stringstream error_msg;
+            error_msg << "Keypoint already has symbol! Keypoint has symbol: "
+                      << kpt_to_symbol_.at(kpt);
+            throw std::runtime_error(error_msg.str());
+        }
+        kpt_to_symbol_.emplace(kpt, symbol);
+    };
+    std::optional<gtsam::Symbol> get_symbol(const cv::KeyPoint &kpt) {
+        if (kpt_to_symbol_.find(kpt) != kpt_to_symbol_.end()) {
+            return kpt_to_symbol_.at(kpt);
+        }
+        return std::nullopt;
+    };
+
+   private:
+    keypoints_descriptors kpts_descriptors_;
+    std::unordered_map<cv::KeyPoint, gtsam::Symbol> kpt_to_symbol_;
+};
+}  // namespace robot::experimental::learn_descriptors
--- experimental/learn_descriptors/structure_from_motion.hh
+++ experimental/learn_descriptors/structure_from_motion.hh
@@ -8,6 +8,7 @@
 
 #include "Eigen/Core"
 #include "common/geometry/camera.hh"
+#include "experimental/learn_descriptors/feature_manager.hh"
 #include "gtsam/geometry/Point3.h"
 #include "gtsam/geometry/Pose3.h"
 #include "gtsam/inference/Symbol.h"
@@ -17,28 +18,29 @@
 #include "gtsam/nonlinear/Values.h"
 #include "opencv2/opencv.hpp"
 
-namespace std {
-template <>
-struct hash<cv::KeyPoint> {
-    size_t operator()(const cv::KeyPoint &kp) const {
-        size_t h1 = hash<float>()(kp.pt.x);
-        size_t h2 = hash<float>()(kp.pt.y);
-        size_t h3 = hash<float>()(kp.size);
-        size_t h4 = hash<float>()(kp.angle);
-        size_t h5 = hash<float>()(kp.response);
-        size_t h6 = hash<int>()(kp.octave);
-        size_t h7 = hash<int>()(kp.class_id);
+// namespace std {
+// template <>
+// struct hash<cv::KeyPoint> {
+//     size_t operator()(const cv::KeyPoint &kp) const {
+//         size_t h1 = hash<float>()(kp.pt.x);
+//         size_t h2 = hash<float>()(kp.pt.y);
+//         size_t h3 = hash<float>()(kp.size);
+//         size_t h4 = hash<float>()(kp.angle);
+//         size_t h5 = hash<float>()(kp.response);
+//         size_t h6 = hash<int>()(kp.octave);
+//         size_t h7 = hash<int>()(kp.class_id);
 
-        return (h1 ^ (h2 << 1)) ^ (h3 << 2) ^ (h4 << 3) ^ (h5 << 4) ^ (h6 << 5) ^ (h7 << 6);
-    }
-};
-}  // namespace std
-namespace cv {
-inline bool operator==(const cv::KeyPoint &kp1, const cv::KeyPoint &kp2) {
-    return kp1.pt == kp2.pt && kp1.size == kp2.size && kp1.angle == kp2.angle &&
-           kp1.response == kp2.response && kp1.octave == kp2.octave && kp1.class_id == kp2.class_id;
-}
-}  // namespace cv
+//         return (h1 ^ (h2 << 1)) ^ (h3 << 2) ^ (h4 << 3) ^ (h5 << 4) ^ (h6 << 5) ^ (h7 << 6);
+//     }
+// };
+// }  // namespace std
+// namespace cv {
+// inline bool operator==(const cv::KeyPoint &kp1, const cv::KeyPoint &kp2) {
+//     return kp1.pt == kp2.pt && kp1.size == kp2.size && kp1.angle == kp2.angle &&
+//            kp1.response == kp2.response && kp1.octave == kp2.octave && kp1.class_id ==
+//            kp2.class_id;
+// }
+// }  // namespace cv
 namespace robot::experimental::learn_descriptors {
 class Frontend {
    public:
@@ -208,6 +210,8 @@ class StructureFromMotion {
     const std::vector<std::vector<cv::DMatch>> get_matches() { return matches_; };
 
    private:
+    FeatureManager feature_manager_;
+
     gtsam::Pose3 initial_pose_;
     std::vector<std::pair<std::vector<cv::KeyPoint>, cv::Mat>> img_keypoints_and_descriptors_;
     std::vector<std::vector<cv::DMatch>> matches_;
