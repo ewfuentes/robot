1faa4e60b27f9d19e7c648d45b2b56c8cf243c06
Erick Fuentes <efuentes@mit.edu>
2025-06-12 22:30:14 -0400

    wip

--- experimental/overhead_matching/swag/scripts/aws/README.md
+++ experimental/overhead_matching/swag/scripts/aws/README.md
@@ -1,8 +1,8 @@
 Launch Training Jobs on AWS
----------------------------
+===========================
 
 AWS Setup
-=========
+---------
 1. Get added to the AWS group, ask Erick for details.
 2. Sign into the AWS console and on the IAM page, create a new user.
 3. Add that user to the `overhead-matching` group.
@@ -14,3 +14,10 @@ sudo ./aws/install
 ```
 5. Create an access key for your user and enter the required details in `aws configure`.
 6. If everything works, you should be able to run `aws s3 ls` and see the `rrg-overhead-matching` bucket.
+
+Tooling Setup
+-------------
+1. Install (uv)[https://docs.astral.sh/uv/#highlights].
+
+Launching a  Job
+----------------
---
+++ experimental/overhead_matching/swag/scripts/aws/train.py
@@ -0,0 +1,47 @@
+
+import subprocess
+import sys
+import logging
+from pathlib import Path
+import os
+import boto3
+from urllib.parse import urlparse
+import glob
+
+
+def install_s3_wheel(s3_uri):
+    """Download and install wheel from S3"""
+    # Parse S3 URI
+    parsed = urlparse(s3_uri)
+    if parsed.scheme == "s3":
+        bucket = parsed.netloc
+        key = parsed.path.lstrip('/')
+        # Download wheel
+        s3_client = boto3.client('s3')
+        local_wheel_path = f'/tmp/{key.split("/")[-1]}'
+        s3_client.download_file(bucket, key, local_wheel_path)
+    else:
+        local_wheel_path = parsed.path
+    # Install wheel
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--force-reinstall', local_wheel_path])
+
+    # This is a hack due to bazel wheels packaging dependencies in another site-packages directory
+    to_add = []
+    for p in sys.path:
+        p = Path(p)
+        maybe_to_add = p / 'site-packages'
+        if maybe_to_add.exists():
+            to_add.append(str(maybe_to_add))
+    sys.path.extend(to_add)
+
+
+wheel_s3_uri = os.environ.get("CUSTOM_WHEEL_S3_URI")
+install_s3_wheel(wheel_s3_uri)
+
+import experimental.overhead_matching.swag.scripts.train as t
+
+train_config_path = glob.glob("/opt/ml/input/data/config_file/*.yaml")[0]
+print(t.main)
+t.main(dataset_path=Path("/opt/ml/input/data/train"),
+       opt_config=Path(train_config_path),
+       output_dir=Path("/opt/ml/model"))
