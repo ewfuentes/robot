add round trip test and fix sign error

common/gps/web_mercator.py
@@ -9,13 +9,13 @@ def latlon_to_pixel_coords(lat_deg: float, lon_deg: float, zoom_level: int):
     map_size = 2 ** (8 + zoom_level)
     scale_factor = map_size / (2 * np.pi)
 
-    x = scale_factor * (np.pi - lon_rad)
-    y = scale_factor * (np.pi - np.log(np.tan(np.pi / 4.0 - lat_rad / 2.0)))
+    x = scale_factor * (np.pi + lon_rad)
+    y = scale_factor * (np.pi - np.log(np.tan(np.pi / 4.0 + lat_rad / 2.0)))
     # Note that return type is row and column
     return y, x
 
 
-def pixel_coords_to_latlon(py, px, zoom_level):
+def pixel_coords_to_latlon(py: float, px: float, zoom_level: int):
     map_size = 2 ** (8 + zoom_level)
     scale_factor = 2 * np.pi / map_size
 