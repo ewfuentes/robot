fix precision issue with haversine and add mcl sampling

experimental/overhead_matching/swag/evaluation/swag_algorithm.py
@@ -49,49 +49,55 @@ def measurement_wag(
     num_mcl_samples = particles.shape[0] - num_dual_mcl_samples
     # Create new MCL samples
     # calculate observation likelihoods
-    observation_log_likelihoods = pf.wag_observation_log_likelihood_from_similarity_matrix(
-        similarity_matrix, wag_config.sigma_obs_prob_from_sim)
-    log_particle_weights = pf.wag_calculate_log_particle_weights(
-        observation_log_likelihoods,
-        particles,
-        patch_index_from_particle)
+    if num_mcl_samples:
+        observation_log_likelihoods = pf.wag_observation_log_likelihood_from_similarity_matrix(
+            similarity_matrix, wag_config.sigma_obs_prob_from_sim)
+        log_particle_weights = pf.wag_calculate_log_particle_weights(
+            observation_log_likelihoods,
+            particles,
+            patch_index_from_particle)
 
-    # resample particles
-    resampled_primal_particles = pf.wag_multinomial_resampling(
-            particles, log_particle_weights, generator, num_samples=num_mcl_samples)
+        # resample particles
+        resampled_primal_particles = pf.wag_multinomial_resampling(
+                particles, log_particle_weights, generator, num_samples=num_mcl_samples)
+    else:
+        resampled_primal_particles = torch.zeros((0, particles.shape[1]), device=particles.device)
 
     # Create new Dual MCL samples
     # compute the state likelihood by normalizing the observation likelihoods
-    observation_log_likelihoods = pf.wag_observation_log_likelihood_from_similarity_matrix(
-            similarity_matrix, wag_config.sigma_obs_prob_from_sim)
-    normalizer = torch.logsumexp(observation_log_likelihoods, 0)
-    state_log_likelihood = observation_log_likelihoods - normalizer
+    if num_dual_mcl_samples:
+        observation_log_likelihoods = pf.wag_observation_log_likelihood_from_similarity_matrix(
+                similarity_matrix, wag_config.sigma_obs_prob_from_sim)
+        normalizer = torch.logsumexp(observation_log_likelihoods, 0)
+        state_log_likelihood = observation_log_likelihoods - normalizer
 
-    # Sample particles based on the likelihood of being in a particular satellite patch
-    dev_satellite_patch_locations = satellite_patch_locations.to(state_log_likelihood.device)
-    dual_mcl_particles = pf.wag_multinomial_resampling(
+        # Sample particles based on the likelihood of being in a particular satellite patch
+        dev_satellite_patch_locations = satellite_patch_locations.to(state_log_likelihood.device)
+        dual_mcl_particles = pf.wag_multinomial_resampling(
+                dev_satellite_patch_locations,
+                state_log_likelihood,
+                generator,
+                num_samples=num_dual_mcl_samples)
+
+        # Compute the log likelihood of a patch based on the prior belief
+        belief_log_likelihoods = pf.wag_belief_log_likelihood_from_particles(
+            particles,
             dev_satellite_patch_locations,
-            state_log_likelihood,
-            generator,
-            num_samples=num_dual_mcl_samples)
+            wag_config.dual_mcl_belief_phantom_counts_frac,
+            patch_index_from_particle)
 
-    # Compute the log likelihood of a patch based on the prior belief
-    belief_log_likelihoods = pf.wag_belief_log_likelihood_from_particles(
-        particles,
-        dev_satellite_patch_locations,
-        wag_config.dual_mcl_belief_phantom_counts_frac,
-        patch_index_from_particle)
+        # Compute particle weights based on the computed belief log likelihoods
+        dual_log_particle_weights = pf.wag_calculate_log_particle_weights(
+                belief_log_likelihoods, dual_mcl_particles, patch_index_from_particle)
 
-    # Compute particle weights based on the computed belief log likelihoods
-    dual_log_particle_weights = pf.wag_calculate_log_particle_weights(
-            belief_log_likelihoods, dual_mcl_particles, patch_index_from_particle)
-
-    # Sample new dual particles based on the likelihood coming from the belief
-    resampled_dual_particles = pf.wag_multinomial_resampling(
-            dual_mcl_particles,
-            dual_log_particle_weights,
-            generator,
-            num_samples=num_dual_mcl_samples)
+        # Sample new dual particles based on the likelihood coming from the belief
+        resampled_dual_particles = pf.wag_multinomial_resampling(
+                dual_mcl_particles,
+                dual_log_particle_weights,
+                generator,
+                num_samples=num_dual_mcl_samples)
+    else:
+        resampled_dual_particles = torch.zeros((0, particles.shape[1]), device=particles.device)
 
     resampled_particles = torch.cat([resampled_primal_particles, resampled_dual_particles])
     if return_past_particle_weights:

experimental/overhead_matching/swag/scripts/model_eval.py
@@ -15,6 +15,9 @@ def _():
     from pprint import pprint
     import numpy as np
     import seaborn
+    import math
+
+    import re
 
     import matplotlib.pyplot as plt
     import matplotlib
@@ -24,28 +27,36 @@ def _():
     from experimental.overhead_matching.swag.evaluation import evaluate_swag
     import experimental.overhead_matching.swag.model.patch_embedding
     from common.torch.load_and_save_models import load_model
+    from experimental.overhead_matching.swag.evaluation.wag_config_pb2 import WagConfig, SatellitePatchConfig
+    from common.math import haversine
     from pathlib import Path
     from typing import NamedTuple
     import seaborn
 
     import importlib
     importlib.reload(evaluate_swag)
+    importlib.reload(haversine)
     return (
         NamedTuple,
         Path,
+        SatellitePatchConfig,
+        WagConfig,
         alt,
         common,
         evaluate_swag,
         experimental,
+        haversine,
         importlib,
         itertools,
         load_model,
+        math,
         matplotlib,
         mo,
         np,
         pd,
         plt,
         pprint,
+        re,
         satellite_embedding_database,
         seaborn,
         torch,
@@ -112,14 +123,14 @@ def _(NamedTuple, Path, itertools, vigor_dataset):
 @app.cell
 def _(datasets, get_top_k_results, itertools, model_paths, pd):
     dfs = []
-    for (model_name, model_path), (data_name, dataset) in itertools.product(model_paths.items(), datasets.items()):
+    for (model_name, model_path), (data_name, _dataset) in itertools.product(model_paths.items(), datasets.items()):
         print(model_name, data_name)
-        df = get_top_k_results(model_path, dataset)
+        df = get_top_k_results(model_path, _dataset)
         df["dataset"] = data_name
         df["model"] = [model_name]*len(df)
         dfs.append(df)
     df = pd.concat(dfs)
-    return data_name, dataset, df, dfs, model_name, model_path
+    return data_name, df, dfs, model_name, model_path
 
 
 @app.cell
@@ -218,7 +229,274 @@ def _(Path, mo, model_selector, path_slider, plt, torch):
 
 
 @app.cell
+def _(NamedTuple, Path, pd, re, torch):
+    # Mixture MCL Evaluation
+
+    class MixtureSettings(NamedTuple):
+        frac: float
+        phantom_counts: float
+
+    def extract_mixture_info(p):
+        m = re.match(r"mcl_frac_([0-9.]+)_phantom_counts_([0-9.]+)", p.name)
+        if not m:
+            print(f"Could not parse settings from: {p}")
+            return
+
+        out = []
+
+        settings = MixtureSettings(frac=float(m.group(1)), phantom_counts=float(m.group(2)))
+        for eval_path in sorted(p.glob("[0-9]*")):
+            error = torch.load(eval_path / "error.pt")
+            var = torch.load(eval_path / "var.pt")
+            out.append({
+                "path_id": int(eval_path.name),
+                "dual_mcl_frac": settings.frac,
+                "phantom_counts": settings.phantom_counts,
+                "final_error_m": error[-1].item(),
+                "final_var_sq_m": var[-1].item(),
+                "settings": settings})
+
+        return pd.DataFrame.from_records(out)
+
+
+
+    _experiment_folder = Path('/data/overhead_matching/evaluation/results/mixture_mcl_sweep/')
+    _dfs = []
+    for p in _experiment_folder.glob("mcl_frac*"):
+        _dfs.append(extract_mixture_info(p))
+
+    mixture_mcl_df = pd.concat(_dfs).reset_index(drop=True)
+    return MixtureSettings, extract_mixture_info, mixture_mcl_df, p
+
+
+@app.cell
+def _(mixture_mcl_df):
+    mixture_mcl_df
+    return
+
+
+@app.cell
+def _(p, torch):
+    error = torch.load(p / "0000099" / 'error.pt')
+    error.shape
+    return (error,)
+
+
+@app.cell
+def _(p):
+    print(p)
+    return
+
+
+@app.cell
+def _(error, mo, plt):
+    plt.figure()
+    fig = plt.plot(error)
+    mo.mpl.interactive(plt.gcf())
+    return (fig,)
+
+
+@app.cell
+def _(evaluate_swag, load_model, vigor_dataset):
+    dataset = vigor_dataset.VigorDataset('/data/overhead_matching/datasets/VIGOR/NewYork/',
+                                        vigor_dataset.VigorDatasetConfig(
+                                            panorama_neighbor_radius=1e-6,
+                                            satellite_patch_size=(320, 320),
+                                            panorama_size=(320, 640),
+                                            factor=1))
+    sat_model = load_model('/data/overhead_matching/models/20250616_8_way_experiment/all_chicago_lr_schedule_False_negative_mining_False_pos_semipos_False/0059_satellite/')
+    pano_model = load_model('/data/overhead_matching/models/20250616_8_way_experiment/all_chicago_lr_schedule_False_negative_mining_False_pos_semipos_False/0059_panorama/')
+
+    all_similarity = evaluate_swag.compute_cached_similarity_matrix(
+        sat_model=sat_model,
+        pano_model=pano_model,
+        dataset=dataset,
+        device='cuda:0',
+        use_cached_similarity=True)
+    return all_similarity, dataset, pano_model, sat_model
+
+
+@app.cell
 def _():
+    with open('/data/overhead_matching/evaluation/paths/NewYork_100_paths.json', 'r') as file_in:
+        import json
+        all_paths = json.load(file_in)
+
+        eval_path = all_paths["paths"][99]
+    return all_paths, eval_path, file_in, json
+
+
+@app.cell
+def _(
+    SatellitePatchConfig,
+    WagConfig,
+    all_similarity,
+    dataset,
+    eval_path,
+    evaluate_swag,
+    math,
+):
+    EARTH_RADIUS_M = 6_371_000.0
+    eval_results = evaluate_swag.construct_inputs_and_evalulate_path(
+        vigor_dataset=dataset,
+        path=eval_path,
+        path_similarity_values=all_similarity[eval_path],
+        generator_seed=42 * 99,
+        device="cuda",
+        wag_config=WagConfig(
+            noise_percent_motion_model=0.02,  # page 71 thesis
+            # offset was fixed at 1.3km in thesis (page 71)
+            initial_particle_distribution_std_deg=2970.0 / EARTH_RADIUS_M * 180.0 / math.pi,
+            num_particles=100_000,
+           sigma_obs_prob_from_sim=0.1,
+           satellite_patch_config=SatellitePatchConfig(
+               zoom_level=20,
+               patch_height_px=640,
+               patch_width_px=640),
+            dual_mcl_frac=0.05,
+            dual_mcl_belief_phantom_counts_frac=0.5),
+        return_intermediates=True)
+    return EARTH_RADIUS_M, eval_results
+
+
+@app.cell
+def _(eval_results, mo, plt):
+    print(eval_results.particle_history[-1].shape)
+    print(eval_results.log_particle_weights[-1].shape)
+
+    particles = eval_results.particle_history[-1]
+
+    plt.figure()
+    plt.scatter(particles[:, 1], particles[:, 0])
+    plt.axis('equal')
+    mo.mpl.interactive(plt.gcf())
+    return (particles,)
+
+
+@app.cell
+def _(particles):
+    particles.mean(dim=0)
+    return
+
+
+@app.cell
+def _(dataset, eval_path):
+    dataset._panorama_metadata.loc[eval_path[-1]]
+    return
+
+
+@app.cell
+def _(dataset, eval_path, evaluate_swag, particles):
+    evaluate_swag.get_distance_error_between_pano_and_particles_meters(dataset, eval_path[-1], particles)
+    return
+
+
+@app.cell
+def _(particles):
+    particles.mean(dim=0)
+    return
+
+
+@app.cell
+def _(EARTH_RADIUS_M, dataset, eval_path, haversine, particles):
+    true_latlon = dataset.get_panorama_positions(eval_path[-1:])
+    (EARTH_RADIUS_M * haversine.find_d_on_unit_circle(particles, true_latlon)).mean()
+    return (true_latlon,)
+
+
+@app.cell
+def _(particles):
+    particles.shape
+    return
+
+
+@app.cell
+def _(true_latlon):
+    true_latlon.shape
+    return
+
+
+@app.cell
+def _(EARTH_RADIUS_M, haversine, importlib, torch, true_latlon):
+    importlib.reload(haversine)
+
+    def get_distance_error_between_pano_and_particles_meters(
+          vigor_dataset: vigor_dataset.VigorDataset,
+          panorama_index: int | list[int],
+          particles: torch.Tensor)->torch.Tensor:
+
+        if isinstance(panorama_index, int):
+            panorama_index = [panorama_index]
+            assert particles.ndim == 2
+            particles = particles.unsqueeze(0)
+        true_latlong = vigor_dataset.get_panorama_positions(panorama_index).to(device=particles.device)
+        print(f"{true_latlong=}")
+        particle_latlong_estimate = particles.mean(dim=1)
+        print(f"{particle_latlong_estimate=}")
+        # mean_deviation_m = EARTH_RADIUS_M * haversine.find_d_on_unit_circle(
+        #       particles, particle_latlong_estimate[:, None, :])
+        # var_sq_m = torch.mean(mean_deviation_m ** 2, -1)
+        var_sq_m = 123.456
+        mean_error_m = []
+        print(f"{haversine.find_d_on_unit_circle(true_latlon, particle_latlong_estimate)=}")
+        for i in range(len(panorama_index)):
+            distance_error_meters = EARTH_RADIUS_M * haversine.find_d_on_unit_circle(
+                  true_latlong[i], particle_latlong_estimate[i])
+            print(i, distance_error_meters)
+            mean_error_m.append(distance_error_meters)
+        mean_error_m = torch.tensor(mean_error_m)
+        if len(mean_error_m) == 1:
+            mean_error_m = mean_error_m[0]
+        return mean_error_m, var_sq_m
+    return (get_distance_error_between_pano_and_particles_meters,)
+
+
+@app.cell
+def _(
+    dataset,
+    eval_path,
+    get_distance_error_between_pano_and_particles_meters,
+    particles,
+):
+    get_distance_error_between_pano_and_particles_meters(dataset, eval_path[-1], particles)
+    return
+
+
+@app.cell
+def _(EARTH_RADIUS_M, dataset, eval_path, np, particles, torch):
+    def hav(theta_rad):
+        return np.sin(theta_rad/2.0)**2
+
+    def hav_latlon(p1, p2):
+        p1 = torch.deg2rad(p1)
+        p2 = torch.deg2rad(p2)
+        delta_lat = p2[0] - p1[0]
+        delta_lon = p2[1] - p1[1]
+    
+        return hav(delta_lat) + np.cos(p1[0]) * np.cos(p2[0]) * hav(delta_lon)
+
+    
+    p1 = dataset.get_panorama_positions(eval_path[-1:])[0]
+    p2 = particles.mean(dim=0)
+    EARTH_RADIUS_M * 2 * torch.asin(torch.sqrt(hav_latlon(p1, p2)))
+    return hav, hav_latlon, p1, p2
+
+
+@app.cell
+def _(p1):
+    p1
+    return
+
+
+@app.cell
+def _(EARTH_RADIUS_M, np):
+    np.cos(1000 / EARTH_RADIUS_M)
+    return
+
+
+@app.cell
+def _(hav_latlon):
+    hav_latlon()
     return
 
 