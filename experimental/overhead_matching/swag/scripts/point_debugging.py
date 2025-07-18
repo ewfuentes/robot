df1d289644e75daa0a329caea4c866fe90515653
Erick Fuentes <efuentes@mit.edu>
2025-07-18 10:15:12 -0400

    add debugging script

--- experimental/overhead_matching/swag/scripts/point_debugging.py
+++ experimental/overhead_matching/swag/scripts/point_debugging.py
@@ -10,67 +10,149 @@ def _():
     import matplotlib.pyplot as plt
     import experimental.overhead_matching.swag.data.vigor_dataset as vd
     import scipy.ndimage
+    import supervision as sv
+
+    import torch
 
     import numpy as np
 
+    from sam2.automatic_mask_generator import SAM2AutomaticMaskGenerator
+
+    from  pprint import pprint
+
     from pathlib import Path
-    return Path, mo, np, plt, scipy, vd
+    import tqdm
+    return (
+        Path,
+        SAM2AutomaticMaskGenerator,
+        mo,
+        np,
+        plt,
+        pprint,
+        scipy,
+        sv,
+        torch,
+        tqdm,
+        vd,
+    )
 
 
 @app.cell
 def _(Path, vd):
     _dataset_path = Path('/data/overhead_matching/datasets/VIGOR/Chicago')
-    _dataset_config = vd.VigorDatasetConfig(panorama_neighbor_radius=1e-6)
+    _dataset_config = vd.VigorDatasetConfig(
+        panorama_neighbor_radius=1e-6, 
+        satellite_patch_size=(320, 320),
+        panorama_size=(320, 640)
+    )
     dataset = vd.VigorDataset(_dataset_path, _dataset_config)
     return (dataset,)
 
 
 @app.cell
+def _():
+    pano_ids = [
+        'eGAGP4_z5WqSKUCduBnTTQ',
+        'DFV4rWr6uNXyRXADxw-drA',
+        'i4wmqIoYC4MpwHwtX1Ne_Q',
+        'FCIaFY85O35KFBHiSvNDrw',
+        'px9glW7aHgTjo_DqD3WHlg',
+        'IxT7VPmcIclFC0EUS-pX5Q',
+        'tbW7AKK6CZ8FiTujoomT8Q',
+        'T4hFoaVPzZFqZ9n58TjOwA',
+        'C9zFejx1IIzjS_0aOqj03Q',
+        'o65m4lSocxg8q1iDgKeeKA',
+        'XCqGpT7YETzc4tcx_kHTVA',
+        'T4l-wzDwORCSRcooL_Ds6w',
+        'unNrTGdRYM-uWDecK2Lduw',
+        'oCyAJcIFVbvR3_ntDjdECg',
+        'LQtcBwmGf95sorQtsNCqQA',
+        '8j_7Og8Ij8WisYjeatIv2w',
+        '4gjTma9fOnICZNNPV7odJA',
+        '6Ky7G-T1w1qgwRSP1MUIwg',
+    ]
+    return (pano_ids,)
+
+
+@app.cell
 def _(np):
-    def sample_pts(image_shape):
+    def sample_pts():
         gen = np.random.default_rng(0)
-        rvs = gen.random((500, 2))
-        azimuth_rad = rvs[:, 1] * 2 * np.pi
+        rvs = gen.random((256, 2))
         elevation_rad = np.arcsin(2 * rvs[:, 0] - 1)
 
         row_frac = 1.0/2.0 - elevation_rad / np.pi
-        row =  row_frac * image_shape[1]
-        col =  rvs[:, 1] * image_shape[2]
-        return np.stack((row, col), axis=1)
+        col_frac =  rvs[:, 1]
+        return np.stack((col_frac, row_frac), axis=1)
     return (sample_pts,)
 
 
 @app.cell
-def _(dataset, mo, plt, sample_pts):
-    _item = dataset[20]
-    pts = sample_pts(_item.panorama.shape)
-    plt.figure(figsize=(12, 6))
-    image = _item.panorama.permute(1, 2, 0).numpy()
-    plt.imshow(_item.panorama.permute(1, 2, 0))
-    plt.plot(pts[:, 1], pts[:, 0], 'o')
-    mo.mpl.interactive(plt.gcf())
-    return image, pts
+def _(SAM2AutomaticMaskGenerator):
+    # Run SAM2
+    sam_model = SAM2AutomaticMaskGenerator.from_pretrained(
+        "facebook/sam2.1-hiera-large",
+        # points_per_side=None,
+        points_per_batch=128,
+        # point_grids=[sample_pts()],
+        stability_score_thresh=0.85
+    )
+    return (sam_model,)
 
 
 @app.cell
-def _():
-    # def spherical_pixel_from_azimuth_elevation(azimuth_rad, elev_rad, image_shape):
-    #     # 0 degrees azimuth is east
-    #     col_frac = (np.pi - azimuth_rad) / ( 2 * np.pi)
-    #     row_frac = elev_rad / np.pi + 1 / 2.0
+def _(dataset, sam_model, tqdm, vd):
+    # for _p in pano_ids:
+    #     print(_p)
+    #     idx = dataset._panorama_metadata[dataset._panorama_metadata.pano_id == _p].index[0]
+    #     _item = dataset[idx]
+    #     results = sam_model.generate(_item.panorama.permute(1, 2, 0).numpy())
+    #     for _i, _r in enumerate(results):
+    #         if _r["bbox"][2] == 0.0 or _r["bbox"][3] == 0.0:
+    #             print(_item)
+    #             print(_i, _r)
+    #             print(results)
+    #             break
 
-    #     return np.stack([row_frac * image_shape[0], col_frac * image_shape[1]], axis=0)
 
-    # def azimuth_elevation_from_pinhole_pixel(row_frac, col_frac, fov):
-    #     # row_frac and col_frac are in [-1, 1]
-    #     # compute the distance of the plane
-    #     plane_dist_x = 1 / np.tan(fov[0] / 2.0)
-    #     plane_dist_y = 1 / np.tan(fov[1] / 2.0)
+    dataloader = vd.get_dataloader(dataset.get_pano_view(), num_workers=8, batch_size=18)
 
-    #     azimuth_rad = np.arctan2(col_frac, plane_dist_x)
-    #     elev_rad = np.arctan2(row_frac, plane_dist_y)
-    #     return azimuth_rad, elev_rad
-    return
+    for batch in tqdm.tqdm(dataloader):
+        for batch_idx in range(len(batch.panorama_metadata)):
+            _results = sam_model.generate(batch.panorama[batch_idx].permute(1, 2, 0).numpy())
+            for _i, _r in enumerate(_results):
+                if _r["bbox"][2] == 0.0 or _r["bbox"][3] == 0.0:
+                    print(batch.panorama_metadata[batch_idx])
+                    print(_results)
+    return batch, batch_idx, dataloader
+
+
+@app.cell
+def _(dataset, mo, plt, sample_pts):
+    item = dataset[123]
+    pts = sample_pts()
+    plt.figure(figsize=(12, 6))
+    image = item.panorama.permute(1, 2, 0).numpy()
+    plt.imshow(item.panorama.permute(1, 2, 0))
+    plt.plot(pts[:, 0] * item.panorama.shape[2], pts[:, 1] * item.panorama.shape[1], 'o')
+    mo.mpl.interactive(plt.gcf())
+    return image, item, pts
+
+
+@app.cell
+def _(item, mo, np, plt, sam_model, sv):
+    results = sam_model.generate(item.panorama.permute(1, 2, 0).numpy())
+    detections = sv.Detections.from_sam(sam_result=results)
+
+    img = np.ascontiguousarray(np.uint8(item.panorama.permute(1, 2, 0).numpy() * 255.0))
+    print(img.shape)
+    sv.MaskAnnotator(color_lookup=sv.ColorLookup.INDEX).annotate(img, detections)
+
+    plt.figure(figsize=(12, 6))
+    plt.imshow(img)
+    mo.mpl.interactive(plt.gcf())
+
+    return detections, img, results
 
 
 @app.cell
@@ -202,13 +284,19 @@ def _(mo, plt, reproj):
     plt.figure(figsize=(22,6))
 
     for i, (yaw, img) in enumerate(reproj.items()):
-        plt.subplot(1, len(reproj), i+1)
+        print(i, yaw)
+        plt.subplot(2, 4, i+1)
         plt.imshow(img)
         plt.title(f"{yaw=:0.3f}")
-        plt.tight_layout()
-
+    plt.tight_layout()
     mo.mpl.interactive(plt.gcf())
     return i, img, yaw
+
+
+@app.cell
+def _(reproj):
+    print(len(reproj))
+    return
 
 
 @app.cell
