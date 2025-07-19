3bb0a99fb0d2a745c89b2c0971e3b51c0391f869
Erick Fuentes <efuentes@mit.edu>
2025-07-19 19:59:41 -0400

    fix unit test

--- experimental/overhead_matching/swag/model/semantic_segment_extractor_test.py
+++ experimental/overhead_matching/swag/model/semantic_segment_extractor_test.py
@@ -14,8 +14,7 @@ import tqdm
 class SemanticSegmentExtractorTest(unittest.TestCase):
     def test_sample_image(self):
         # Setup
-        BATCH_SIZE = 30
-        CLIP_FEATURE_DIM = 512
+        BATCH_SIZE = 5
         dataset = vd.VigorDataset(
             Path("external/vigor_snippet/vigor_snippet"),
             vd.VigorDatasetConfig(panorama_neighbor_radius=1e-6))
@@ -23,7 +22,7 @@ class SemanticSegmentExtractorTest(unittest.TestCase):
 
         model = sse.SemanticSegmentExtractor(
             sse.SemanticSegmentExtractorConfig(points_per_batch=32)
-        )
+        ).cuda()
 
         # Action
         batch = next(iter(dataloader))
@@ -33,7 +32,7 @@ class SemanticSegmentExtractorTest(unittest.TestCase):
         self.assertEqual(out.positions.shape[0], BATCH_SIZE)
         self.assertEqual(out.positions.shape[2], 2)
         self.assertEqual(out.features.shape[0], BATCH_SIZE)
-        self.assertEqual(out.features.shape[2], CLIP_FEATURE_DIM)
+        self.assertEqual(out.features.shape[2], model.output_dim)
         self.assertEqual(out.mask.shape[0], BATCH_SIZE)
         self.assertEqual(out.mask.shape[1], out.features.shape[1])
         self.assertEqual(out.mask.shape[1], out.positions.shape[1])
