1a7fbb8d301b3ffa585aeb8469f67258af7bbb46
Erick Fuentes <fuentes.erick@gmail.com>
2025-08-04 22:47:21 -0400

    fetch the config token on startup instead of at service up time

---
+++ .github/dockerfiles/launch.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+
+set -euo pipefail
+
+GITHUB_RUNNER_TOKEN=$(curl -s -X POST \
+            -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" \
+            -H "Accept: application/vnd.github+json" \
+            https://api.github.com/repos/ewfuentes/robot/actions/runners/registration-token \
+            | jq -r .token)
+
+./config.sh --url https://github.com/ewfuentes/robot --token ${GITHUB_RUNNER_TOKEN} --unattended
+./run.sh
