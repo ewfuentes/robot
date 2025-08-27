f71bedf2cc543aa33acfea0604efe969dc1dfa38
Erick Fuentes <erick@csail.mit.edu>
2025-08-27 15:16:40 -0400

    Support arm64 on ubuntu noble (#470)

--- setup.sh
+++ setup.sh
@@ -4,21 +4,17 @@ set -x # enable echo
 
 CODENAME=`lsb_release --codename --short`
 ARCH=$(uname -m)
+cat <<EOF > .bazelrc_ubuntu
+build --config=clang
+build:clang --platforms=//toolchain:${CODENAME}_clang_${ARCH}
+build:gcc --platforms=//toolchain:${CODENAME}_gcc_${ARCH}
+EOF
+
 if [ "${CODENAME}" = "noble" ]; then
-    ln -s .bazelrc_noble .bazelrc_ubuntu
     PACKAGES="clang-18 clang-format-18 gcc-14 g++-14 gcc-11 g++-11"
 elif [ "${CODENAME}" = "jammy" ]; then
     PACKAGES="clang-15 clang-format-15 gcc-12 g++-12 gcc-11 g++-11"
-    if [ "${ARCH}" = "x86_64" ]; then
-        ln -s .bazelrc_jammy .bazelrc_ubuntu
-    elif [ "${ARCH}" = "aarch64" ]; then
-        ln -s .bazelrc_jammy_arm64 .bazelrc_ubuntu
-    else
-        echo "Unsupported architecture: ${ARCH}"
-        exit 1
-    fi
 fi
-
 
 # Install clang14 and other required system packages
 sudo apt-get update
@@ -28,16 +24,14 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get -y install libxcursor-dev clang \
      libgirepository1.0-dev libcairo2-dev libgtk2.0-dev libcanberra-gtk-module libsuitesparse-dev \
      python-is-python3 build-essential ${PACKAGES}
 
-if [ "${CODENAME}" = "jammy" ]; then
-    if [ "${ARCH}" = "aarch64" ]; then
-        # We need to regenerate the python requirements. Use uv
-        command -v uv >/dev/null 2>&1 || curl -LsSf https://astral.sh/uv/install.sh | sh
-        uv python install 3.12
-        rm third_party/python/requirements_3_12.txt
-        uv pip compile --python-version 3.12 -o third_party/python/requirements_3_12.txt \
-            --format requirements.txt --index-strategy unsafe-best-match --generate-hashes \
-            --emit-index-url third_party/python/requirements_3_12.in
-    fi
+if [ "${ARCH}" = "aarch64" ]; then
+    # We need to regenerate the python requirements. Use uv
+    command -v uv >/dev/null 2>&1 || curl -LsSf https://astral.sh/uv/install.sh | sh
+    uv python install 3.12
+    rm third_party/python/requirements_3_12.txt
+    uv pip compile --python-version 3.12 -o third_party/python/requirements_3_12.txt \
+        --format requirements.txt --index-strategy unsafe-best-match --generate-hashes \
+        --emit-index-url third_party/python/requirements_3_12.in
 fi
 
 # Install bazelisk if not found
@@ -52,6 +46,7 @@ else
         echo "Unsupported architecture: ${ARCH}"
         exit 1
     fi
+    mkdir -p ~/.local/bin
     curl -o ~/.local/bin/bazel -L "${BAZELISK_URL}"
     chmod +x ~/.local/bin/bazel
 fi
