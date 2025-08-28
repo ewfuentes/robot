4836710e8cc42921d62891596a0d850cb6ae3535
Erick Fuentes <erick@csail.mit.edu>
2025-08-28 11:12:25 -0400

    Make evaluation faster using autocast (#472)

--- experimental/overhead_matching/swag/data/satellite_embedding_database.py
+++ experimental/overhead_matching/swag/data/satellite_embedding_database.py
@@ -15,7 +15,7 @@ def build_embeddings_from_model(model: torch.nn.Module,
     model.to(device)
     model.eval()
     inf_results = []
-    with torch.no_grad():
+    with torch.no_grad(), torch.autocast('cuda', dtype=torch.bfloat16):
         for data in tqdm.tqdm(dataloader, disable=not verbose):
             embeddings = model(model_input_from_dataloader(data).to(device))
             inf_results.append(embeddings)
