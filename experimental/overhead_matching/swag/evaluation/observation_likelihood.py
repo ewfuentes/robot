e9c796209e0b9c6fc925c6706c882723211b3ad6
Erick Fuentes <efuentes@mit.edu>
2025-11-07 10:52:35 -0500

    implement get_similarities

--- experimental/overhead_matching/swag/evaluation/observation_likelihood.py
+++ experimental/overhead_matching/swag/evaluation/observation_likelihood.py
@@ -3,6 +3,7 @@ import common.torch.load_torch_deps
 import torch
 from dataclasses import dataclass
 import geopandas as gpd
+import pandas as pd
 import itertools
 
 
@@ -38,11 +39,52 @@ class QueryData:
     particle_locs_deg: torch.Tensor
 
 
-def _get_similarities(prior_data: PriorData, pano_ids: list[str]):
-    mask = prior_data.pano_metadata.isin(pano_ids)
-    requested_pano_metadata = prior_data.pano_metadata[mask]
-    assert len(requested_pano_metadata) == len(pano_ids)
-    return None, None
+@dataclass
+class Similarities:
+    # num_panos x num_satellite_patches
+    sat_patch: torch.Tensor
+    # num_panos x num_pano_landmarks x num_osm_landmarks
+    landmark: torch.Tensor
+    # num_panos x num_pano_landmarks
+    # True means that it is a valid landmark
+    landmark_mask: torch.Tensor
+
+
+def _get_similarities(prior_data: PriorData, pano_ids: list[str]) -> Similarities:
+
+    if len(pano_ids) == 0:
+        return Similarities(
+            sat_patch=torch.zeros((0, prior_data.pano_sat_similarity.shape[1])),
+            landmark=torch.zeros((0, 0, prior_data.pano_osm_landmark_similarity.shape[1])),
+            landmark_mask=torch.zeros((0, 0), dtype=torch.bool)
+        )
+
+    pano_metadata = pd.concat([
+        prior_data.pano_metadata[prior_data.pano_metadata.pano_id == p]
+        for p in pano_ids
+    ])
+    assert len(pano_metadata) == len(pano_ids)
+
+    sat_patch_similarities = prior_data.pano_sat_similarity[pano_metadata.index]
+
+    num_panos = len(pano_ids)
+    max_num_pano_landmarks = pano_metadata.pano_lm_idxs.apply(len).max()
+    max_num_pano_landmarks = max_num_pano_landmarks if len(pano_metadata) > 0 else 0
+    num_osm_landmarks = len(prior_data.osm_geometry)
+    landmark_similarities = torch.zeros((num_panos, max_num_pano_landmarks, num_osm_landmarks),
+                                        dtype=torch.float32)
+    landmark_mask = torch.zeros((num_panos, max_num_pano_landmarks), dtype=torch.bool)
+
+    for pano_idx, (_, row) in enumerate(pano_metadata.iterrows()):
+        num_pano_lms = len(row.pano_lm_idxs)
+        landmark_similarities[pano_idx, :num_pano_lms] = prior_data.pano_osm_landmark_similarity[row.pano_lm_idxs]
+        landmark_mask[pano_idx, :num_pano_lms] = True
+
+    return Similarities(
+        sat_patch=sat_patch_similarities,
+        landmark=landmark_similarities,
+        landmark_mask=landmark_mask
+    )
 
 
 def _compute_pixel_locs_px(particle_locs_deg: torch.Tensor):
@@ -53,7 +95,7 @@ def _compute_sat_log_likelihood(similarities, sat_geometry, particle_locs_px):
     return torch.full(particle_locs_px.shape[:-1], -torch.inf)
 
 
-def _compute_osm_log_likelihood(similarities, osm_geometry, particle_locs_px):
+def _compute_osm_log_likelihood(similarities, mask, osm_geometry, particle_locs_px):
     return torch.full(particle_locs_px.shape[:-1], -torch.inf)
 
 
@@ -71,16 +113,19 @@ def compute_log_observation_likelihood(prior_data, query, config):
     _check_prior_data(prior_data)
     # Given the panorama ids, get the relevant satellite patch
     # similarities and osm landmark similarities
-    sat_patch_similarities, osm_similarities = _get_similarities(prior_data, query.pano_ids)
+    similarities = _get_similarities(prior_data, query.pano_ids)
 
     # Convert the particle lat/lon locations to pixels
     particle_locs_px = _compute_pixel_locs_px(query.particle_locs_deg)
 
     # Compute the log observation likelihood for each particle
     sat_log_likelihood = _compute_sat_log_likelihood(
-            sat_patch_similarities, prior_data.sat_geometry, particle_locs_px)
+            similarities.sat_patch, prior_data.sat_geometry, particle_locs_px)
     osm_log_likelihood = _compute_osm_log_likelihood(
-            osm_similarities, prior_data.osm_geometry, particle_locs_px)
+            similarities.landmark,
+            similarities.landmark_mask,
+            prior_data.osm_geometry,
+            particle_locs_px)
 
     return sat_log_likelihood, osm_log_likelihood
 
