cc53308aa60991f5e9981d46293460131e129298
Erick Fuentes <efuentes@mit.edu>
2025-11-18 11:26:39 -0500

    refactor STRtree creation out

--- experimental/overhead_matching/swag/evaluation/observation_likelihood_test.py
+++ experimental/overhead_matching/swag/evaluation/observation_likelihood_test.py
@@ -23,215 +23,12 @@ def create_square_patch(center_x, center_y, size=10):
     ])
 
 
+def build_test_spatial_index(sat_geometry):
+    """Helper to build spatial index for tests."""
+    return lol._build_sat_spatial_index(sat_geometry)
+
+
 class ObservationLikelihoodTest(unittest.TestCase):
-    def test_compute_log_observation_likelihood_valid_inputs(self):
-        """Test that valid inputs don't raise exceptions."""
-        osm_geometry = pd.DataFrame([
-            {'osm_id': 0, 'geometry_px': shapely.Point(100, 50), 'osm_embedding_idx': 0},
-            {'osm_id': 1, 'geometry_px': shapely.Point(200, 150), 'osm_embedding_idx': 1},
-        ])
-
-        sat_geometry = pd.DataFrame([
-            {'geometry_px': create_square_patch(100, 50, size=20), 'embedding_idx': 0},
-            {'geometry_px': create_square_patch(200, 150, size=20), 'embedding_idx': 1},
-        ])
-
-        pano_metadata = pd.DataFrame([
-            {'pano_id': 'pano_0', 'pano_lm_idxs': [0, 1]},
-        ])
-
-        pano_sat_similarity = torch.tensor([[0.8, 0.3]])
-        pano_osm_similarity = torch.tensor([[0.9, 0.2], [0.1, 0.7]])
-
-        prior_data = lol.PriorData(
-            osm_geometry=osm_geometry,
-            sat_geometry=sat_geometry,
-            pano_metadata=pano_metadata,
-            pano_sat_similarity=pano_sat_similarity,
-            pano_osm_landmark_similarity=pano_osm_similarity
-        )
-
-        query = lol.QueryData(
-            pano_ids=['pano_0'],
-            particle_locs_deg=torch.tensor([[
-                [37.7749, -122.4194],
-                [40.7128, -74.0060],
-                [51.5074, -0.1278],
-            ]])
-        )
-
-        config = lol.ObservationLikelihoodConfig(
-            obs_likelihood_from_sat_similarity_sigma=0.1,
-            obs_likelihood_from_osm_similarity_sigma=100.0,
-            )
-
-        sat_log_likelihood, osm_log_likelihood = lol.compute_log_observation_likelihood(
-            prior_data, query, config
-        )
-
-        self.assertEqual(sat_log_likelihood.shape, (1, 3))
-        self.assertEqual(osm_log_likelihood.shape, (1, 3))
-
-    def test_mismatched_pano_sat_similarity_shape(self):
-        """Test that pano_sat_similarity with wrong shape raises error."""
-        osm_geometry = pd.DataFrame([
-            {'osm_id': 0, 'geometry_px': shapely.Point(100, 50), 'osm_embedding_idx': 0},
-        ])
-
-        sat_geometry = pd.DataFrame([
-            {'geometry_px': create_square_patch(100, 50, size=20), 'embedding_idx': 0},
-            {'geometry_px': create_square_patch(200, 150, size=20), 'embedding_idx': 1},
-            {'geometry_px': create_square_patch(300, 250, size=20), 'embedding_idx': 2},
-        ])
-
-        pano_metadata = pd.DataFrame([
-            {'pano_id': 'pano_0', 'pano_lm_idxs': [0]},
-        ])
-
-        # Should be (1, 3) but is (1, 2)
-        pano_sat_similarity = torch.tensor([[0.8, 0.3]])
-        pano_osm_similarity = torch.tensor([[0.9]])
-
-        prior_data = lol.PriorData(
-            osm_geometry=osm_geometry,
-            sat_geometry=sat_geometry,
-            pano_metadata=pano_metadata,
-            pano_sat_similarity=pano_sat_similarity,
-            pano_osm_landmark_similarity=pano_osm_similarity
-        )
-
-        query = lol.QueryData(
-            pano_ids=['pano_0'],
-            particle_locs_deg=torch.tensor([[37.7749, -122.4194]])
-        )
-        config = lol.ObservationLikelihoodConfig(
-            obs_likelihood_from_sat_similarity_sigma=0.1,
-            obs_likelihood_from_osm_similarity_sigma=100.0,
-            )
-
-        with self.assertRaises(Exception):
-            lol.compute_log_observation_likelihood(prior_data, query, config)
-
-    def test_missing_required_column(self):
-        """Test that missing osm_embedding_idx column raises error."""
-        osm_geometry = pd.DataFrame([
-            {'osm_id': 0, 'geometry_px': shapely.Point(100, 50)},
-        ])
-
-        sat_geometry = pd.DataFrame([
-            {'geometry_px': create_square_patch(100, 50, size=20), 'embedding_idx': 0},
-        ])
-
-        pano_metadata = pd.DataFrame([
-            {'pano_id': 'pano_0', 'pano_lm_idxs': [0]},
-        ])
-
-        pano_sat_similarity = torch.tensor([[0.8]])
-        pano_osm_similarity = torch.tensor([[0.9]])
-
-        prior_data = lol.PriorData(
-            osm_geometry=osm_geometry,
-            sat_geometry=sat_geometry,
-            pano_metadata=pano_metadata,
-            pano_sat_similarity=pano_sat_similarity,
-            pano_osm_landmark_similarity=pano_osm_similarity
-        )
-
-        query = lol.QueryData(
-            pano_ids=['pano_0'],
-            particle_locs_deg=torch.tensor([[37.7749, -122.4194]])
-        )
-        config = lol.ObservationLikelihoodConfig(
-            obs_likelihood_from_sat_similarity_sigma=0.1,
-            obs_likelihood_from_osm_similarity_sigma=100.0,
-            )
-
-        with self.assertRaises((KeyError, ValueError)):
-            lol.compute_log_observation_likelihood(prior_data, query, config)
-
-    def test_nonexistent_pano_id(self):
-        """Test that requesting non-existent pano_id raises error."""
-        osm_geometry = pd.DataFrame([
-            {'osm_id': 0, 'geometry_px': shapely.Point(100, 50), 'osm_embedding_idx': 0},
-        ])
-
-        sat_geometry = pd.DataFrame([
-            {'geometry_px': create_square_patch(100, 50, size=20), 'embedding_idx': 0},
-        ])
-
-        pano_metadata = pd.DataFrame([
-            {'pano_id': 'pano_0', 'pano_lm_idxs': [0]},
-            {'pano_id': 'pano_1', 'pano_lm_idxs': [0]},
-        ])
-
-        pano_sat_similarity = torch.tensor([[0.8], [0.5]])
-        pano_osm_similarity = torch.tensor([[0.9], [0.7]])
-
-        prior_data = lol.PriorData(
-            osm_geometry=osm_geometry,
-            sat_geometry=sat_geometry,
-            pano_metadata=pano_metadata,
-            pano_sat_similarity=pano_sat_similarity,
-            pano_osm_landmark_similarity=pano_osm_similarity
-        )
-
-        query = lol.QueryData(
-            pano_ids=['pano_99'],
-            particle_locs_deg=torch.tensor([[37.7749, -122.4194]])
-        )
-        config = lol.ObservationLikelihoodConfig(
-            obs_likelihood_from_sat_similarity_sigma=0.1,
-            obs_likelihood_from_osm_similarity_sigma=100.0,
-            )
-
-        with self.assertRaises((AssertionError,)):
-            lol.compute_log_observation_likelihood(prior_data, query, config)
-
-    def test_batch_particles_output_shape(self):
-        """Test that output shape matches particle shape for batch."""
-        osm_geometry = pd.DataFrame([
-            {'osm_id': 0, 'geometry_px': shapely.Point(100, 50), 'osm_embedding_idx': 0},
-        ])
-
-        sat_geometry = pd.DataFrame([
-            {'geometry_px': create_square_patch(100, 50, size=20), 'embedding_idx': 0},
-        ])
-
-        pano_metadata = pd.DataFrame([
-            {'pano_id': 'pano_0', 'pano_lm_idxs': [0]},
-        ])
-
-        pano_sat_similarity = torch.tensor([[0.8]])
-        pano_osm_similarity = torch.tensor([[0.9]])
-
-        prior_data = lol.PriorData(
-            osm_geometry=osm_geometry,
-            sat_geometry=sat_geometry,
-            pano_metadata=pano_metadata,
-            pano_sat_similarity=pano_sat_similarity,
-            pano_osm_landmark_similarity=pano_osm_similarity
-        )
-
-        query = lol.QueryData(
-            pano_ids=['pano_0'],
-            particle_locs_deg=torch.tensor([[
-                [37.7749, -122.4194],
-                [37.7750, -122.4195],
-                [37.7751, -122.4196],
-                [37.7752, -122.4197],
-                [37.7753, -122.4198],
-            ]])
-        )
-        config = lol.ObservationLikelihoodConfig(
-            obs_likelihood_from_sat_similarity_sigma=0.1,
-            obs_likelihood_from_osm_similarity_sigma=100.0,
-            )
-
-        sat_ll, osm_ll = lol.compute_log_observation_likelihood(prior_data, query, config)
-
-        self.assertEqual(sat_ll.shape, (1, 5))
-        self.assertEqual(osm_ll.shape, (1, 5))
-
     def test_get_similarities_single_pano(self):
         """Test extracting similarities for a single panorama."""
         osm_geometry = pd.DataFrame([
@@ -707,12 +504,13 @@ class ObservationLikelihoodTest(unittest.TestCase):
         sat_geometry = pd.DataFrame([
             {'geometry_px': create_square_patch(100, 50, size=20), 'embedding_idx': 0},
         ])
+        sat_tree, patch_centroids = build_test_spatial_index(sat_geometry)
 
         similarities = torch.tensor([[0.8]])
         particle_locs_px = torch.tensor([[100.0, 50.0]])
 
         log_likelihood = lol._compute_sat_log_likelihood(
-            similarities, sat_geometry, particle_locs_px
+            similarities, particle_locs_px, sat_tree, patch_centroids
         )
 
         self.assertEqual(log_likelihood.shape, (1,))
@@ -724,6 +522,7 @@ class ObservationLikelihoodTest(unittest.TestCase):
             {'geometry_px': create_square_patch(100, 50, size=20), 'embedding_idx': 0},
             {'geometry_px': create_square_patch(200, 150, size=20), 'embedding_idx': 1},
         ])
+        sat_tree, patch_centroids = build_test_spatial_index(sat_geometry)
 
         similarities = torch.tensor([
             [0.8, 0.3],
@@ -737,7 +536,7 @@ class ObservationLikelihoodTest(unittest.TestCase):
         ])
 
         log_likelihood = lol._compute_sat_log_likelihood(
-            similarities, sat_geometry, particle_locs_px
+            similarities, particle_locs_px, sat_tree, patch_centroids
         )
 
         self.assertEqual(log_likelihood.shape, (3,))
@@ -747,6 +546,7 @@ class ObservationLikelihoodTest(unittest.TestCase):
         sat_geometry = pd.DataFrame([
             {'geometry_px': create_square_patch(100, 50, size=20), 'embedding_idx': 0},
         ])
+        sat_tree, patch_centroids = build_test_spatial_index(sat_geometry)
 
         similarities = torch.tensor([[0.8], [0.7]])
         particle_locs_px = torch.tensor([
@@ -755,7 +555,7 @@ class ObservationLikelihoodTest(unittest.TestCase):
         ])
 
         log_likelihood = lol._compute_sat_log_likelihood(
-            similarities, sat_geometry, particle_locs_px
+            similarities, particle_locs_px, sat_tree, patch_centroids
         )
 
         self.assertEqual(log_likelihood.shape, (2, 3))
@@ -766,16 +566,17 @@ class ObservationLikelihoodTest(unittest.TestCase):
             {'geometry_px': create_square_patch(100, 50, size=20), 'embedding_idx': 0},
             {'geometry_px': create_square_patch(200, 150, size=20), 'embedding_idx': 1},
         ])
+        sat_tree, patch_centroids = build_test_spatial_index(sat_geometry)
 
         similarities = torch.tensor([[0.9, 0.1]])
         particle_near_high = torch.tensor([[100.0, 50.0]])
         particle_near_low = torch.tensor([[200.0, 150.0]])
 
         ll_near_high = lol._compute_sat_log_likelihood(
-            similarities, sat_geometry, particle_near_high
+            similarities, particle_near_high, sat_tree, patch_centroids
         )
         ll_near_low = lol._compute_sat_log_likelihood(
-            similarities, sat_geometry, particle_near_low
+            similarities, particle_near_low, sat_tree, patch_centroids
         )
 
         self.assertGreater(ll_near_high.item(), ll_near_low.item())
@@ -787,12 +588,13 @@ class ObservationLikelihoodTest(unittest.TestCase):
             {'geometry_px': create_square_patch(200, 150, size=20), 'embedding_idx': 1},
             {'geometry_px': create_square_patch(300, 250, size=20), 'embedding_idx': 2},
         ])
+        sat_tree, patch_centroids = build_test_spatial_index(sat_geometry)
 
         similarities = torch.tensor([[0.8, 0.5, 0.2]])
         particle_locs_px = torch.tensor([[100.0, 50.0]])
 
         log_likelihood = lol._compute_sat_log_likelihood(
-            similarities, sat_geometry, particle_locs_px
+            similarities, particle_locs_px, sat_tree, patch_centroids
         )
 
         self.assertEqual(log_likelihood.shape, (1,))
@@ -804,16 +606,17 @@ class ObservationLikelihoodTest(unittest.TestCase):
         sat_geometry = pd.DataFrame([
             {'geometry_px': create_square_patch(100, 50, size=20), 'embedding_idx': 0},
         ])
+        sat_tree, patch_centroids = build_test_spatial_index(sat_geometry)
 
         similarities = torch.tensor([[0.9]])
         particle_near = torch.tensor([[100.0, 50.0]])
         particle_far = torch.tensor([[1000.0, 1000.0]])
 
         ll_near = lol._compute_sat_log_likelihood(
-            similarities, sat_geometry, particle_near
+            similarities, particle_near, sat_tree, patch_centroids
         )
         ll_far = lol._compute_sat_log_likelihood(
-            similarities, sat_geometry, particle_far
+            similarities, particle_far, sat_tree, patch_centroids
         )
 
         self.assertGreater(ll_near.item(), ll_far.item())
@@ -821,12 +624,13 @@ class ObservationLikelihoodTest(unittest.TestCase):
     def test_compute_sat_log_likelihood_no_patches(self):
         """Test with empty satellite geometry."""
         sat_geometry = pd.DataFrame(columns=['geometry_px', 'embedding_idx'])
+        sat_tree, patch_centroids = build_test_spatial_index(sat_geometry)
 
         similarities = torch.zeros((1, 0))
         particle_locs_px = torch.tensor([[100.0, 50.0]])
 
         log_likelihood = lol._compute_sat_log_likelihood(
-            similarities, sat_geometry, particle_locs_px
+            similarities, particle_locs_px, sat_tree, patch_centroids
         )
 
         self.assertEqual(log_likelihood.shape, (1,))
@@ -836,12 +640,13 @@ class ObservationLikelihoodTest(unittest.TestCase):
         sat_geometry = pd.DataFrame([
             {'geometry_px': create_square_patch(100, 50, size=20), 'embedding_idx': 0},
         ])
+        sat_tree, patch_centroids = build_test_spatial_index(sat_geometry)
 
         similarities = torch.tensor([[0.0]])
         particle_locs_px = torch.tensor([[100.0, 50.0]])
 
         log_likelihood = lol._compute_sat_log_likelihood(
-            similarities, sat_geometry, particle_locs_px
+            similarities, particle_locs_px, sat_tree, patch_centroids
         )
 
         self.assertEqual(log_likelihood.shape, (1,))
@@ -851,12 +656,13 @@ class ObservationLikelihoodTest(unittest.TestCase):
         sat_geometry = pd.DataFrame([
             {'geometry_px': create_square_patch(100, 50, size=20), 'embedding_idx': 0},
         ])
+        sat_tree, patch_centroids = build_test_spatial_index(sat_geometry)
 
         similarities = torch.tensor([[0.8]])
         particle_locs_px = torch.tensor([[100.0, 50.0]])
 
         log_likelihood = lol._compute_sat_log_likelihood(
-            similarities, sat_geometry, particle_locs_px
+            similarities, particle_locs_px, sat_tree, patch_centroids
         )
 
         self.assertIsInstance(log_likelihood, torch.Tensor)
@@ -1085,6 +891,7 @@ class ObservationLikelihoodTest(unittest.TestCase):
             {'geometry_px': create_square_patch(100, 100, size=40), 'embedding_idx': 0},
             {'geometry_px': create_square_patch(110, 110, size=40), 'embedding_idx': 1},
         ])
+        sat_tree, patch_centroids = build_test_spatial_index(sat_geometry)
 
         # Give different similarities to the two patches
         # Panorama has high similarity to patch 0, low similarity to patch 1
@@ -1095,7 +902,7 @@ class ObservationLikelihoodTest(unittest.TestCase):
         particle_in_overlap = torch.tensor([[[105.0, 105.0]]])
 
         ll_overlap = lol._compute_sat_log_likelihood(
-            similarities, sat_geometry, particle_in_overlap
+            similarities, particle_in_overlap, sat_tree, patch_centroids
         )
 
         # Should get a finite likelihood (not -inf) since particle is inside at least one patch
@@ -1106,7 +913,7 @@ class ObservationLikelihoodTest(unittest.TestCase):
         particle_outside = torch.tensor([[[200.0, 200.0]]])
 
         ll_outside = lol._compute_sat_log_likelihood(
-            similarities, sat_geometry, particle_outside
+            similarities, particle_outside, sat_tree, patch_centroids
         )
 
         # Should get -inf likelihood since particle is outside all patches
@@ -1117,7 +924,7 @@ class ObservationLikelihoodTest(unittest.TestCase):
         particle_in_patch0_only = torch.tensor([[[85.0, 85.0]]])
 
         ll_patch0 = lol._compute_sat_log_likelihood(
-            similarities, sat_geometry, particle_in_patch0_only
+            similarities, particle_in_patch0_only, sat_tree, patch_centroids
         )
 
         # Should use patch 0's similarity (0.9), which is higher
@@ -1127,7 +934,7 @@ class ObservationLikelihoodTest(unittest.TestCase):
         particle_in_patch1_only = torch.tensor([[[125.0, 125.0]]])
 
         ll_patch1 = lol._compute_sat_log_likelihood(
-            similarities, sat_geometry, particle_in_patch1_only
+            similarities, particle_in_patch1_only, sat_tree, patch_centroids
         )
 
         # Should use patch 1's similarity (0.2), which is lower
@@ -1146,6 +953,7 @@ class ObservationLikelihoodTest(unittest.TestCase):
             {'geometry_px': create_square_patch(100, 100, size=40), 'embedding_idx': 0},
             {'geometry_px': create_square_patch(110, 110, size=40), 'embedding_idx': 1},
         ])
+        sat_tree, patch_centroids = build_test_spatial_index(sat_geometry)
 
         # Particle at (105, 105) is in the overlap region
         # Distance to patch 0 center (100, 100): sqrt(25 + 25) ≈ 7.07
@@ -1159,14 +967,14 @@ class ObservationLikelihoodTest(unittest.TestCase):
         # Case A: patch 0 has high similarity, patch 1 has low similarity
         similarities_A = torch.tensor([[0.9, 0.2]])
         ll_A = lol._compute_sat_log_likelihood(
-            similarities_A, sat_geometry, particle_in_overlap
+            similarities_A, particle_in_overlap, sat_tree, patch_centroids
         )
 
         # Case B: swap the similarities
         # patch 0 has low similarity, patch 1 has high similarity
         similarities_B = torch.tensor([[0.2, 0.9]])
         ll_B = lol._compute_sat_log_likelihood(
-            similarities_B, sat_geometry, particle_in_overlap
+            similarities_B, particle_in_overlap, sat_tree, patch_centroids
         )
 
         # If the implementation uses nearest patch (patch 0), then:
